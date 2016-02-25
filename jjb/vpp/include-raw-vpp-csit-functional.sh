a41a6e0bee61fc1d6df5be50300ae58b2d59a7ea
Stefan Kobza <skobza@cisco.com>
2016-02-25 14:36:26 +0100

    Add vpp jenkins job to build vpp and run csit tests.
    To start integrating with VPP verify job, start csit tests for each VPP
    verify run. Until artifacts passing is solved between jenkins upstream
    and downstream jobs, build the VPP project again in this job. Then
    check out csit project, copy the deb packages to the cloned dir and
    start the testcase.
    
    Prevent the job non-voting, so that it does not polute VPP reviews with
    erroneous results. We'll switch to normal voting after all potential
    problems are ironed out, and the job is stable.
    
    Change-Id: I9e35e19a1d8b2a64b8e65473fef93e0b537eded2
    Signed-off-by: Stefan Kobza <skobza@cisco.com>

---
+++ jjb/vpp/include-raw-vpp-csit-functional.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -xeu -o pipefail
+
+# Clone csit and start tests
+git clone https://gerrit.fd.io/r/csit
+
+cp build-root/*.deb csit/
+cd csit
+# execute csit bootstrap script if it exists
+if [ -e bootstrap.sh ]
+then
+    # make sure that bootstrap.sh is executable
+    chmod +x bootstrap.sh
+    # run the script
+    ./bootstrap.sh *.deb
+else
+    echo 'ERROR: No bootstrap.sh found'
+    exit 1
+fi
+
+# vim: ts=4 ts=4 sts=4 et :
