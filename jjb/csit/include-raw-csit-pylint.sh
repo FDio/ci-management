2f9fd67211d1529bbed5ec10e566c418ee6fecbe
Stefan Kobza <skobza@cisco.com>
2016-03-04 22:12:36 +0100

    Remove system pylint installation from pylint job.
    Change-Id: Ic4894018ffa10e7742bd798c17bdbe6de7945a0a
    Signed-off-by: Stefan Kobza <skobza@cisco.com>

--- jjb/csit/include-raw-csit-pylint.sh
+++ jjb/csit/include-raw-csit-pylint.sh
@@ -2,8 +2,6 @@
 
 set -x
 
-sudo apt-get install -y --force-yes pylint
-
 # Re-create virtual environment
 rm -rf env || true
 virtualenv env
@@ -11,6 +9,7 @@ virtualenv env
 
 # Install requirements, so all CSIT python dependencies are met
 pip install -r requirements.txt
+pip install pylint==1.5.4
 
 # Run pylint, but hide its' return value until python warnings are cleared
 PYTHONPATH=`pwd` pylint --rcfile=pylint.cfg resources/ > pylint.log || true
