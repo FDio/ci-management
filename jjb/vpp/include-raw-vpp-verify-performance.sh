1f2e0803475f7118dff1a15f1f43c406d2edeab6
pmikus <pmikus@cisco.com>
2016-03-31 11:37:14 +0200

    VPP performance check job
    - vpp-csit-hw-perf job to check performance of proposed patch
    - build, copy, install, start perf tests
    
    Change-Id: I2ae4cee916532c4d57715e2bd739d3109ab7ccf6
    Signed-off-by: pmikus <pmikus@cisco.com>

---
+++ jjb/vpp/include-raw-vpp-verify-performance.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -xeu -o pipefail
+
+# Clone csit and start tests
+git clone --depth 1 https://gerrit.fd.io/r/csit --branch csit-verified
+
+cp build-root/*.deb csit/
+cd csit
+# execute csit bootstrap script if it exists
+if [ ! -e bootstrap-verify-perf.sh ]
+then
+    echo 'ERROR: No bootstrap-verify-perf.sh found'
+    exit 1
+fi
+
+# make sure that bootstrap-verify-perf.sh is executable
+chmod +x bootstrap-verify-perf.sh
+# run the script
+./bootstrap-verify-perf.sh *.deb
+
+# vim: ts=4 ts=4 sts=4 et :
