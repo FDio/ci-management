62afed518c3368a406173d0d92e10992feb9abf9
pmikus <pmikus@cisco.com>
2016-04-14 15:12:37 +0200

    Rename job to performance
    - rename csit-vpp-functional-hw-tb2 job to csit-vpp-perf-hw (as testing performance) with unified trigger for performance testing
    - parameterized job for future extension
    
    Change-Id: I72d1562fad8ad188930f39374f3ebfe8ebb984aa
    Signed-off-by: pmikus <pmikus@cisco.com>

---
+++ jjb/csit/include-raw-csit-vpp-perf-hw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export TEST_TAG="PERFTEST_${TYPE^^}"
+
+# execute csit bootstrap script if it exists
+if [ ! -e bootstrap-hw-tb2.sh ]
+then
+    echo 'ERROR: No bootstrap-hw-tb2.sh found'
+    exit 1
+fi
+
+# make sure that bootstrap-verify-perf.sh is executable
+chmod +x bootstrap-hw-tb2.sh
+# run the script
+./bootstrap-hw-tb2.sh
+
+# vim: ts=4 ts=4 sts=4 et :
