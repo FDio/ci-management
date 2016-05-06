1a01a18e76cd132b1b3a4fc136228d6d00cab58f
Ed Warnicke <eaw@cisco.com>
2016-05-06 10:50:45 -0500

    Fix for java home for honeycomb
    Change-Id: Ib4381c10763251105ca1fc418e70c0e06905ba9a
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/vpp/include-raw-vpp-build.sh
+++ jjb/vpp/include-raw-vpp-build.sh
@@ -11,4 +11,11 @@ else
     echo $CCACHE_DIR does not exist.  This must be a new slave.
 fi
 
+# Make sure we are starting on java-7.  This catches bugs in the
+# vpp build system that can occur on Ubuntu 14.04 when a user may
+# have both java-7 and java-8 installed.
+if [ ${OS} == ubuntu1404 ];then
+    sudo update-java-alternatives /usr/lib/jvm/java-1.7.0-openjdk-amd64
+fi
+
 build-root/vagrant/build.sh
