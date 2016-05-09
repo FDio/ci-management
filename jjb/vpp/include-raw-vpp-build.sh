cf7546ba0195acb070274c92268f3089e9af731d
Ed Warnicke <eaw@cisco.com>
2016-05-09 14:14:06 -0500

    Check to see if a VPP patch needs rebasing
    Change-Id: I9eca0b820a82dac4dbe58759a47f3294c6fcb14b
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/vpp/include-raw-vpp-build.sh
+++ jjb/vpp/include-raw-vpp-build.sh
@@ -11,6 +11,24 @@ else
     echo $CCACHE_DIR does not exist.  This must be a new slave.
 fi
 
+# Check to make sure the patch doesn't need to be rebased
+# Since there was a discontinuity, patches with a
+# parent before 30d41ff need to be rebased
+
+(git log --oneline | grep 30d41ff > /dev/null 2>&1);if [ $? != 0 ]; then REBASE_NEEDED="1";fi
+(git log --oneline | grep fb0815d > /dev/null 2>&1);if [ $? == 0 ]; then VPP_REPO="1";fi
+echo "REBASE_NEEDED: ${REBASE_NEEDED}"
+echo "VPP_REPO: ${VPP_REPO}"
+
+if [ "x${VPP_REPO}" == "x1" ]; then
+    if [ "x${REBASE_NEEDED}" == "x1" ]; then
+        echo "This patch to vpp is based on an old point in the tree that is likely"
+        echo "to fail verify."
+        echo "PLEASE REBASE PATCH ON THE CURRENT HEAD OF THE VPP REPO"
+        exit 1
+    fi
+fi
+
 # Make sure we are starting on java-7.  This catches bugs in the
 # vpp build system that can occur on Ubuntu 14.04 when a user may
 # have both java-7 and java-8 installed.
