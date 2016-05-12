57562b3160a724f5bf73a4ffab75f27bab568d3b
Ed Warnicke <hagbard@gmail.com>
2016-05-12 04:05:01 +0000

    Revert "Changes to accomodate vpp throttle branch."
    This reverts commit 5d1d00330488903ae810de20bfd035fde9cdc25a.
    
    This reverts gerrit: https://gerrit.fd.io/r/#/c/1074/
    
    Change-Id: I5d937a4486ae7426debefdfaf85019508253c3f0
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/vpp/include-raw-vpp-maven-push.sh
+++ jjb/vpp/include-raw-vpp-maven-push.sh
@@ -12,27 +12,15 @@ BASEURL="${NEXUSPROXY}/content/repositories/fd.io."
 BASEREPOID='fdio-'
 declare -A REPO_TARGET
 REPOID_TARGET=(
-    [master:ubuntu1404]="${BASEREPOID}master.ubuntu.trusty.main"
-    [master:ubuntu1604]="${BASEREPOID}master.ubuntu.xenial.main"
-    [master:centos7]="${BASEREPOID}master.centos7"
-    [stable/test:ubuntu1404]="${BASEREPOID}stable.test.ubuntu.trusty.main"
-    [stable/test:ubuntu1604]="${BASEREPOID}stable.test.ubuntu.xenial.main"
-    [stable/test:centos7]="${BASEREPOID}stable.test.centos7"
-    [stable/1606:ubuntu1404]="${BASEREPOID}stable.1606.ubuntu.trusty.main"
-    [stable/1606:ubuntu1604]="${BASEREPOID}stable.1606.ubuntu.xenial.main"
-    [stable/1606:centos7]="${BASEREPOID}stable.1606.centos7"
+    [ubuntu1404]="${BASEREPOID}dev"
+    [ubuntu1604]="${BASEREPOID}ubuntu.xenial.main"
+    [centos7]="${BASEREPOID}yum"
 )
 declare -A REPOURL_TARGET
 REPOURL_TARGET=(
-    [master:ubuntu1404]="${BASEURL}master.ubuntu.trusty.main"
-    [master:ubuntu1604]="${BASEURL}master.ubuntu.xenial.main"
-    [master:centos7]="${BASEURL}master.centos7"
-    [stable/test:ubuntu1404]="${BASEURL}stable.test.ubuntu.trusty.main"
-    [stable/test:ubuntu1604]="${BASEURL}stable.test.ubuntu.xenial.main"
-    [stable/test:centos7]="${BASEURL}stable.test.centos7"
-    [stable/1606:ubuntu1404]="${BASEURL}stable.1606.ubuntu.trusty.main"
-    [stable/1606:ubuntu1604]="${BASEURL}stable.1606.ubuntu.xenial.main"
-    [stable/1606:centos7]="${BASEURL}stable.1606.centos7"
+    [ubuntu1404]="${BASEURL}dev"
+    [ubuntu1604]="${BASEURL}ubuntu.xenial.main"
+    [centos7]="${BASEURL}yum"
 )
 
 function push_file ()
@@ -81,8 +69,8 @@ function push_jar ()
 function push_deb ()
 {
     debfile=$1
-    repoId=${REPOID_TARGET[${GERRIT_BRANCH}:${OS}]}
-    url="${REPOURL_TARGET[${GERRIT_BRANCH}:${OS}]}"
+    repoId=${REPOID_TARGET[${OS}]}
+    url="${REPOURL_TARGET[${OS}]}"
 
     basefile=$(basename -s .deb "$debfile")
     artifactId=$(echo "$basefile" | cut -f 1 -d '_')
@@ -94,8 +82,8 @@ function push_deb ()
 function push_rpm ()
 {
     rpmfile=$1
-    repoId=${REPOID_TARGET[${GERRIT_BRANCH}:${OS}]}
-    url="${REPOURL_TARGET[${GERRIT_BRANCH}:${OS}]}"
+    repoId=${REPOID_TARGET[${OS}]}
+    url="${REPOURL_TARGET[${OS}]}"
 
     if grep -qE '\.s(rc\.)?rpm' <<<"$rpmfile"
     then
