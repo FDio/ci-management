8f6c0d1a3608ed5eec316999601c236061541cd6
C.J. Collier <cjcollier@linuxfoundation.org>
2016-05-17 12:03:02 -0700

    Document the process of re-spinning an image
    * Creat README.txt
    * Creat fdio-respin.sh
    * Creat ../lib/respin-functions.sh
    * Automate the process of fetching latest base images
    * Automate the process of creating new upload snapshots
    * Automate process of re-spinning
    * Correct shasum to sha1sum
    
    Change-Id: Ie70d5f1fdee0e9f06810da6f37bfc710e3e06b8e
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

---
+++ vagrant/basebuild/fdio-respin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright 2016 The Linux Foundation <cjcollier@linuxfoundation.org>
+source ../lib/respin-functions.sh
+
+source ${PVE_PATH}/bin/activate
+
+# Acquire bootstrap images
+#download_deb_image 'Ubuntu' '14.04' 'amd64'
+#download_deb_image 'Ubuntu' '16.04' 'amd64'
+#download_rh_image 'CentOS' '7' 'x86_64'
+
+# Push images to openstack via glance
+#create_deb_image 'Ubuntu' '14.04' 'amd64'
+#create_deb_image 'Ubuntu' '16.04' 'amd64'
+#create_rh_image 'CentOS' '7' 'x86_64'
+
+# Respin images
+respin_deb_image 'Ubuntu' '14.04' 'amd64'
+respin_deb_image 'Ubuntu' '16.04' 'amd64'
+respin_rh_image 'CentOS' '7' 'x86_64'
