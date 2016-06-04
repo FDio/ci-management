cfaf0bf146c2c9e7598faf6c9efb044b8b7dba33
Ed Warnicke <eaw@cisco.com>
2016-06-03 19:22:28 -0500

    Turn on debugging
    * sourcing a debug script if DEBUG is set
    * echo command before executing (set -x)
    * reduce duplicate code
    * give more information about uptime
    
    Change-Id: Ic8409ea3a819f2070eac01ac84e2b3a6fe975592
    Signed-off-by: Ed Warnicke <eaw@cisco.com>
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jjb/tldk/include-raw-build.sh
+++ jjb/tldk/include-raw-build.sh
@@ -1,32 +1,11 @@
 #!/bin/bash
+
 # basic build script example
+DEBUG=1
 set -e -o pipefail
-# do nothing but print the current slave hostname
-hostname
-export CCACHE_DIR=/tmp/ccache
-if [ -d $CCACHE_DIR ];then
-    echo $CCACHE_DIR exists
-    du -sk $CCACHE_DIR
-else
-    echo $CCACHE_DIR does not exist.  This must be a new slave.
-fi
 
-echo "cat /etc/bootstrap.sha"
-if [ -f /etc/bootstrap.sha ];then
-    cat /etc/bootstrap.sha
-else
-    echo "Cannot find /etc/bootstrap.sha"
-fi
-
-echo "cat /etc/bootstrap-functions.sha"
-if [ -f /etc/bootstrap-functions.sha ];then
-    cat /etc/bootstrap-functions.sha
-else
-    echo "Cannot find /etc/bootstrap-functions.sha"
-fi
-
-echo "sha1sum of this script: ${0}"
-sha1sum $0
+SCRIPTDIR=$(realpath "$(dirname $0)/../../")
+[ "${DEBUG}" -gt "0" ] && source "${SCRIPTDIR}/scripts/debug.sh"
 
 make
 
