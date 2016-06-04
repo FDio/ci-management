b3841d34059e36a25ad7e595f13f4a35fb259765
Ed Warnicke <eaw@cisco.com>
2016-06-03 19:22:28 -0500

    Turn on debugging
    * echo command before executing (set -x)
    * reduce duplicate code
    * give more information about uptime
    
    Change-Id: Ic8409ea3a819f2070eac01ac84e2b3a6fe975592
    Signed-off-by: Ed Warnicke <eaw@cisco.com>
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jjb/tldk/include-raw-build.sh
+++ jjb/tldk/include-raw-build.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 # basic build script example
-set -e -o pipefail
+set -xe -o pipefail
 # do nothing but print the current slave hostname
 hostname
 export CCACHE_DIR=/tmp/ccache
 if [ -d $CCACHE_DIR ];then
-    echo $CCACHE_DIR exists
-    du -sk $CCACHE_DIR
+    echo "$CCACHE_DIR size in KB: " $(du -sk $CCACHE_DIR | awk '{print $1}')
 else
-    echo $CCACHE_DIR does not exist.  This must be a new slave.
+    echo "$CCACHE_DIR does not exist. Slave" $(uptime -p)
 fi
 
-echo "cat /etc/bootstrap.sha"
-if [ -f /etc/bootstrap.sha ];then
-    cat /etc/bootstrap.sha
-else
-    echo "Cannot find /etc/bootstrap.sha"
-fi
+PFX=/etc/bootstrap
 
-echo "cat /etc/bootstrap-functions.sha"
-if [ -f /etc/bootstrap-functions.sha ];then
-    cat /etc/bootstrap-functions.sha
-else
-    echo "Cannot find /etc/bootstrap-functions.sha"
-fi
+for FILE in "${PFX}.sha" "${PFX}-functions.sha"
+do
+    if [ -f ${FILE} ]
+    then
+        CMD="cat ${FILE}"
+        echo ${CMD} && eval "${CMD}"
+    else
+        echo "Cannot find ${FILE}"
+    fi
+done
 
 echo "sha1sum of this script: ${0}"
 sha1sum $0
