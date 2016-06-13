6fb351a07f9d1a796579aefe7268ffb33a36b657
Miroslav Miklus <mmiklus@cisco.com>
2016-06-13 16:56:28 +0200

    CSIT jobs - branching
    - introduce per branch jobs: master, stable/1606
    - weekly job to create csit-stable branch
    - archive artifacts for last 10 jobs
    
    Change-Id: Ib3eb7320fad365017d82a5a594123f0c68076151
    Signed-off-by: Miroslav Miklus <mmiklus@cisco.com>

--- jjb/csit/include-raw-csit-vpp-verify-weekly.sh
+++ jjb/csit/include-raw-csit-vpp-verify-weekly.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -xeu -o pipefail
 
-# Clone csit and start tests
-git clone https://gerrit.fd.io/r/csit --branch master
-
-cd csit
-
 # execute csit bootstrap script if it exists
 if [ -e bootstrap-vpp-verify-weekly.sh ]
 then
@@ -14,7 +9,7 @@ then
     # run the script
     ./bootstrap-vpp-verify-weekly.sh
 else
-    echo 'ERROR: No bootstrap-verify-master.sh found'
+    echo 'ERROR: No bootstrap-vpp-verify-weekly.sh found'
     exit 1
 fi
 
