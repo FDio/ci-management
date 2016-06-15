a06fdc3c350db35834368879ce42aa03dfabe6a5
Ed Warnicke <eaw@cisco.com>
2016-06-15 11:45:51 -0500

    DO NOT MERGE - Setup honeycomb to build and push rpms on merge
    This patch depends on the merging of https://gerrit.fd.io/r/#/c/1574/
    which must merge before it.
    
    Please also note, while there is a first step to getting the package
    pushing scripts into a common sharable place, the vpp script
    for pushing has *intentionally* not been changed to use
    it yet because of the release later this week as a matter
    of risk management.
    
    Change-Id: Ifad185d35f582a62bb750ece3c022a24d2153b4e
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ jjb/honeycomb/include-raw-honeycomb-pkg-push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ "${OS}" == "centos7" ]; then
+    # Find the files
+    RPMS=$(find . -type f -iname '*.rpm')
+    SRPMS=$(find . -type f -iname '*.srpm')
+    SRCRPMS=$(find . -type f -name '*.src.rpm')
+    for i in $RPMS $SRPMS $SRCRPMS
+    do
+        push_rpm "$i"
+    done
+fi
