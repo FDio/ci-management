5ddf5fda401c53d16bbaa241448ed094130a442d
C.J. Collier <cjcollier@linuxfoundation.org>
2016-06-17 12:31:42 -0700

    Create script to automate deployment
    * Automate steps taken manually during release 16.06 #24343
    
    * create deploy.sh, using common code from maven_push_functions.sh
    
    * re-factor include-raw-vpp-maven-push.sh to use common code from
      maven_push_functions.sh
    
    * include maven_push_functions.sh for include-raw-vpp-maven-push.sh
      from vpp.yaml (not by sourcing)
    
    Change-Id: I5042f9c9f0eba3641bd6042c7a8b5353edb541f0
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jjb/vpp/include-raw-vpp-maven-push.sh
+++ jjb/vpp/include-raw-vpp-maven-push.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Copyright 2015,2016 The Linux Foundation
+#           2015,2016 Cisco Systems
+
 set -xe -o pipefail
 echo "*******************************************************************"
 echo "* STARTING PUSH OF PACKAGES TO REPOS"
@@ -12,82 +16,6 @@ if [ -z "${MAVEN_SELECTOR}" ]; then
 fi
 
 MVN="${HOME}/tools/hudson.tasks.Maven_MavenInstallation/${MAVEN_SELECTOR}/bin/mvn"
-GROUP_ID="io.fd.${PROJECT}"
-BASEURL="${NEXUSPROXY}/content/repositories/fd.io."
-BASEREPOID='fdio-'
-
-function push_file ()
-{
-    push_file=$1
-    repoId=$2
-    url=$3
-    version=$4
-    artifactId=$5
-    file_type=$6
-    classifier=$7
-
-    if [ "$classifier" ]; then
-        d_classifier="-Dclassifier=$7"
-    fi
-
-    # Disable checks for doublequote to prevent glob / splitting
-    # shellcheck disable=SC2086
-    $MVN org.apache.maven.plugins:maven-deploy-plugin:deploy-file \
-        -Dfile=$push_file -DrepositoryId=$repoId \
-        -Durl=$url -DgroupId=$GROUP_ID \
-        -Dversion=$version -DartifactId=$artifactId \
-        -Dtype=$file_type $d_classifier\
-        -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
-
-    # make sure the script bombs if we fail an upload
-    if [ "$?" != '0' ]; then
-        echo "ERROR: There was an error with the upload"
-        exit 1
-    fi
-}
-
-function push_jar ()
-{
-    jarfile=$1
-    repoId="${BASEREPOID}snapshot"
-    url="${BASEURL}snapshot"
-
-    basefile=$(basename -s .jar "$jarfile")
-    artifactId=$(echo "$basefile" | cut -f 1 -d '-')
-    version=$(echo "$basefile" | cut -f 2 -d '-')
-
-    push_file "$jarfile" "$repoId" "$url" "${version}-SNAPSHOT" "$artifactId" jar
-}
-
-function push_deb ()
-{
-    debfile=$1
-    repoId="fd.io.${REPO_NAME}"
-    url="${BASEURL}${REPO_NAME}"
-
-    basefile=$(basename -s .deb "$debfile")
-    artifactId=$(echo "$basefile" | cut -f 1 -d '_')
-    version=$(echo "$basefile" | cut -f 2- -d '_')
-
-    push_file "$debfile" "$repoId" "$url" "$version" "$artifactId" deb
-}
-
-function push_rpm ()
-{
-    rpmfile=$1
-    repoId="fd.io.${REPO_NAME}"
-    url="${BASEURL}${REPO_NAME}"
-
-    if grep -qE '\.s(rc\.)?rpm' <<<"$rpmfile"
-    then
-        rpmrelease=$(rpm -qp --queryformat="%{release}.src" "$rpmfile")
-    else
-        rpmrelease=$(rpm -qp --queryformat="%{release}.%{arch}" "$rpmfile")
-    fi
-    artifactId=$(rpm -qp --queryformat="%{name}" "$rpmfile")
-    version=$(rpm -qp --queryformat="%{version}" "$rpmfile")
-    push_file "$rpmfile" "$repoId" "$url" "${version}-${rpmrelease}" "$artifactId" rpm
-}
 
 if [ "${OS}" == "ubuntu1404" ]; then
     # Find the files
