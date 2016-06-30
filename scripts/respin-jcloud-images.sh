e176abec35d64311147c317ae6bccef08c285c83
C.J. Collier <cjcollier@linuxfoundation.org>
2016-06-30 06:56:43 -0700

    Improve respin scripts
    * add conditions to only update base image once every two weeks at the
      most
    * automate more manual steps
    
    Change-Id: I434cf2882e5e337ae4b55a4a7acb774a62b528b7
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- scripts/respin-jcloud-images.sh
+++ scripts/respin-jcloud-images.sh
@@ -2,24 +2,43 @@
 
 set -e
 
+SUPPORTED_PLATFORMS=(
+  'Ubuntu 14.04 amd64'
+  'Ubuntu 16.04 amd64'
+  'CentOS 7 x86_64'
+)
+
 CI_MGMT=$(realpath $(dirname $(realpath $0))/..)
 
 # Copyright 2016 The Linux Foundation <cjcollier@linuxfoundation.org>
 source ${CI_MGMT}/vagrant/lib/respin-functions.sh
+source ${PVERC}
 
-source ${PVE_PATH}/bin/activate
+echo nova: $(which nova)
 
-# Acquire bootstrap images
-#download_deb_image 'Ubuntu' '14.04' 'amd64'
-#download_deb_image 'Ubuntu' '16.04' 'amd64'
-#download_rh_image 'CentOS' '7' 'x86_64'
+export NETID=${NETID:-$(nova network-list | awk "/${CPPROJECT}/ {print \$2}")}
 
-# Push images to openstack via glance
-#create_deb_image 'Ubuntu' '14.04' 'amd64'
-#create_deb_image 'Ubuntu' '16.04' 'amd64'
-#create_rh_image 'CentOS' '7' 'x86_64'
+for PLATFORM in "${SUPPORTED_PLATFORMS[@]}"
+do
+    read -ra DVA <<< "${PLATFORM}"
+    DIST="${DVA[0]}"
+    VERSION="${DVA[1]}"
+    ARCH="${DVA[2]}"
+    DTYPE=$(dist_type ${DIST})
 
-# Respin images
-respin_deb_image 'Ubuntu' '14.04' 'amd64'
-respin_deb_image 'Ubuntu' '16.04' 'amd64'
-respin_rh_image 'CentOS' '7' 'x86_64'
+    AGE_JSON=$(latest_src_age ${DIST} ${VERSION} ${ARCH});
+
+    # only fetch new base image if our latest one is more than two weeks old
+    if [ $(echo ${AGE_JSON} | jq .week) -ge "3" ]
+    then
+        # Acquire bootstrap images
+        download_${DTYPE}_image "${DIST}" "${VERSION}" "${ARCH}"
+        
+        # Push images to openstack via glance
+        create_${DTYPE}_image "${DIST}" "${VERSION}" "${ARCH}"
+    fi
+
+    # Respin images
+    cd ${CI_MGMT}/vagrant/basebuild
+    respin_${DTYPE}_image "${DIST}" "${VERSION}" "${ARCH}"
+done
