3db67767f6ba8c322ec24a8bcb04964ffa9e8197
Ed Warnicke <eaw@cisco.com>
2016-07-04 13:06:22 -0500

    DO NOT MERGE (yet) example of vpp-lite jobs
    Change-Id: I9aa16968dac9067a92bc272f1cfa4315c0eec4ad
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ jjb/vpp/include-raw-vpp-light-maven-push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -xe -o pipefail
+echo "*******************************************************************"
+echo "* VPP-LITE DOES NOT PUSH PACKAGES OR JARS TO REPOS"
+echo "*******************************************************************"
