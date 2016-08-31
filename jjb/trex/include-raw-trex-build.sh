5a0b6e2b96ad23b2903b39bf650e39bbffae20fd
Yaroslav Brustinov <ybrustin@cisco.com>
2016-09-01 01:16:22 +0300

    TRex: add playground jobs to check build of TRex/docs
    Change-Id: I606f0dadbbce8b69182adeb5af1b4af08147ed94
    Signed-off-by: Yaroslav Brustinov <ybrustin@cisco.com>

---
+++ jjb/trex/include-raw-trex-build.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -ex
+
+# emulate unique name for now
+GERRIT_NEWREV=hash`date +%s%N`
+WS=${PWD}/$GERRIT_NEWREV
+
+#  temporary don't fail
+set +e
+
+function clean_ws {
+    rm -rf "$WS"
+}
+trap clean_ws EXIT
+
+echo "$WS"
+clean_ws
+mkdir "$WS"
+cd "$WS"
+
+# cloning
+
+git clone https://github.com/cisco-system-traffic-generator/trex-core.git
+git clone https://github.com/cisco-system-traffic-generator/trex-doc.git
+ls -l
+
+# building core
+
+cd trex-core/linux_dpdk
+./b configure
+./b build
+cd -
+
+cd trex-core/linux
+./b configure
+./b build
+cd -
+
+which asciidoc
+which sphinx-build
+which dblatex
+which python
+which python3
+which pip
+
+# building docs
+
+cd trex-doc
+./b configure
+./b build
+cd -
+
+echo Done
+
