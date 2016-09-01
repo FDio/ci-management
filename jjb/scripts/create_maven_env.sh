faecfc82b5460865421be44e5a03d13eeb0e6c3d
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2016-09-01 18:37:18 -0500

    Clean-up environment injection
    Change-Id: I4eca9c3a844ed2f49946c188e7b81ee862e063f7
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

--- jjb/scripts/create_maven_env.sh
+++ jjb/scripts/create_maven_env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "MAVEN_SETTINGS_OPTIONS='-s \$SETTINGS_FILE -gs \$GLOBAL_SETTINGS_FILE'" > maven_env.txt
+echo "MAVEN_SETTINGS_OPTIONS='-s $SETTINGS_FILE -gs $GLOBAL_SETTINGS_FILE'" > maven_env.txt
