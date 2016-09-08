5794541bd966963ba6d4e75754786ed49454f3e9
Ed Warnicke <eaw@cisco.com>
2016-09-08 12:39:32 -0700

    First attempt to build vpp doxygen docs
    Change-Id: I3756dfcd832a4aa8c274abb17b4163f4f53f7d4a
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ jjb/vpp/include-raw-vpp-docs.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e
+[ "$DOCS_REPO_URL" ] || DOCS_REPO_URL="https://nexus.fd.io/content/sites/site/"
+[ "$PROJECT_PATH" ] || PROJECT_PATH=io/fd/vpp
+[ "$DOCS_FILE" ] || DOC_FILE=vpp.docs.zip
+[ "$DOC_DIR" ] || DOC_DIR=build-root/docs/html
+if [ ${BRANCH} == "stable/1609"]; then
+  VERSION=16.09
+else
+  echo "************************************"
+  echo "* ${BRANCH} does not publish docs  *"
+  echo "************************************"
+  exit
+fi
+MVN="/opt/apache/maven/bin/mvn"
+
+make doxygen
+cd ${DOC_DIR}
+zip ${DOC_FILE} .
+cat pom.xml << EOF
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>io.fd.vpp</groupId>
+  <artifactId>docs</artifactId>
+  <version>1.0.0</version>
+  <packaging>pom</packaging>
+  <build>
+    <plugins>
+      <plugin>
+        <groupId>org.apache.maven.plugins</groupId>
+        <artifactId>maven-deploy-plugin</artifactId>
+        <version>2.8.2</version>
+        <configuration>
+          <skip>true</skip>
+        </configuration>
+      </plugin>
+      <plugin>
+        <groupId>org.sonatype.plugins</groupId>
+        <artifactId>maven-upload-plugin</artifactId>
+        <version>0.0.1</version>
+        <executions>
+          <execution>
+            <id>publish-site</id>
+            <phase>deploy</phase>
+            <goals>
+              <goal>upload-file</goal>
+            </goals>
+            <configuration>
+              <serverId>opendaylight-log-archives</serverId>
+              <repositoryUrl>$DOCS_REPO_URL/content-compressed</repositoryUrl>
+              <file>${DOC_FILE}</file>
+              <repositoryPath>${PROJECT_PATH}</repositoryPath>
+            </configuration>
+          </execution>
+        </executions>
+      </plugin>
+    </plugins>
+  </build>
+</project>
+EOF
+${MVN} deploy
+cd -
+
