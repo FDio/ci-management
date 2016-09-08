93506bd3b7c82732bec84611a879644297f7b559
Ed Warnicke <eaw@cisco.com>
2016-09-08 08:51:29 -0700

    Fix to publishing of artifacts
    Change-Id: I5e1c7815a441cec554b6a0c980ef6bf588f9594b
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
+++ jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
@@ -2,7 +2,9 @@
 # basic build script example
 set -e -o pipefail
 
-cd nsh-plugin/build/java/jvpp
-$MVN org.apache.maven.plugins:maven-deploy-plugin:deploy \
-    -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
-cd -
+if [ "${{OS}}" == "ubuntu1404" ]; then
+    cd nsh-plugin/build/java/jvpp
+    $MVN deploy \
+        -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
+    cd -
+fi
