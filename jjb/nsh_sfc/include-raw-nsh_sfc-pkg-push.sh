61667f91fa0b47b139e9b7838d7f6d3c8eb69a23
Ed Warnicke <eaw@cisco.com>
2016-09-08 16:23:20 -0700

    Fix shellcheck errors for include-raw-nsh_sfc-pkg-push.sh
    Change-Id: I5b8e797b66a298d785c16059fe330cddebd93d88
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
+++ jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
@@ -5,6 +5,6 @@ set -e -o pipefail
 if [ "${OS}" == "ubuntu1404" ]; then
     cd nsh-plugin/build/java/jvpp
     $MVN deploy \
-        -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
+        -gs :$GLOBAL_SETTINGS_FILE" -s "$SETTINGS_FILE"
     cd -
 fi
