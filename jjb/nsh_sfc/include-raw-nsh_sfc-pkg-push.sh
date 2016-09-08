e7bb7ba1cb0f0bc040a2cd085f52636843a0e706
Ed Warnicke <eaw@cisco.com>
2016-09-08 16:13:35 -0700

    Fix JAVA env variables for pushing packages
    Change-Id: I93ce84753679cc1f7cf1000896cb5853cd0d5ce2
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
+++ jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # basic build script example
 set -e -o pipefail
-
 if [ "${OS}" == "ubuntu1404" ]; then
+    export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
+    export JAVAC=${JAVA_HOME}/bin/javac
+    export PATH=${JAVA_HOME}/bin/:${PATH}
     cd nsh-plugin/build/java/jvpp
     $MVN deploy \
         -gs $GLOBAL_SETTINGS_FILE -s $SETTINGS_FILE
