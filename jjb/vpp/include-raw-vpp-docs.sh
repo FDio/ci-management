8d525f389c89048713de3bb32ef4b2ef9cee6a98
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-09-09 11:58:06 -0400

    Use correct service url for compressed files
    Also switch fdio-docs to fdio-site since it's named that generically in
    Nexus.
    
    Change-Id: I27ce7b013974d9ed6c0b16cd6fa7356624a7180c
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/vpp/include-raw-vpp-docs.sh
+++ jjb/vpp/include-raw-vpp-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe -o pipefail
-[ "$DOCS_REPO_URL" ] || DOCS_REPO_URL="https://nexus.fd.io/content/sites/site/"
+[ "$DOCS_REPO_URL" ] || DOCS_REPO_URL="https://nexus.fd.io/service/local/repositories/site/"
 [ "$PROJECT_PATH" ] || PROJECT_PATH=io/fd/vpp
 [ "$DOC_FILE" ] || DOC_FILE=vpp.docs.zip
 [ "$DOC_DIR" ] || DOC_DIR=build-root/docs/html
@@ -48,7 +48,7 @@ cat > pom.xml << EOF
               <goal>upload-file</goal>
             </goals>
             <configuration>
-              <serverId>fdio-docs</serverId>
+              <serverId>fdio-site</serverId>
               <repositoryUrl>$DOCS_REPO_URL/content-compressed</repositoryUrl>
               <file>${DOC_FILE}</file>
               <repositoryPath>${PROJECT_PATH}/${VERSION}</repositoryPath>
