c3626dcdf6181a53c73e5264ba78a0b2c9c81545
Ed Warnicke <eaw@cisco.com>
2016-09-08 17:44:16 -0700

    Fix one character mistake in include-raw-vpp-docs.sh
    Change-Id: Id41a74e741d5bd4bdc834373893e20d6d17445cb
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/vpp/include-raw-vpp-docs.sh
+++ jjb/vpp/include-raw-vpp-docs.sh
@@ -19,7 +19,7 @@ sudo apt-get install -y zip
 make doxygen
 cd ${DOC_DIR}
 zip -r ${DOC_FILE} *
-cat pom.xml << EOF
+cat > pom.xml << EOF
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
   <modelVersion>4.0.0</modelVersion>
   <groupId>io.fd.vpp</groupId>
