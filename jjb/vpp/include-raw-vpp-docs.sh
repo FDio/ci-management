6655e382e04a541b0572df2e4b21f59227ad5cbe
Thanh Ha <thanh.ha@linuxfoundation.org>
2016-09-09 12:25:07 -0400

    Remove gratuitous trailing slash
    Nexus does not seem to like the extra slash.
    
    Change-Id: I28a2267a6f4efb3115474f3277c99dbd50784961
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- jjb/vpp/include-raw-vpp-docs.sh
+++ jjb/vpp/include-raw-vpp-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe -o pipefail
-[ "$DOCS_REPO_URL" ] || DOCS_REPO_URL="https://nexus.fd.io/service/local/repositories/site/"
+[ "$DOCS_REPO_URL" ] || DOCS_REPO_URL="https://nexus.fd.io/service/local/repositories/site"
 [ "$PROJECT_PATH" ] || PROJECT_PATH=io/fd/vpp
 [ "$DOC_FILE" ] || DOC_FILE=vpp.docs.zip
 [ "$DOC_DIR" ] || DOC_DIR=build-root/docs/html
