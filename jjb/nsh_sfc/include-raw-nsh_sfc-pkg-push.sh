36985f691b661f5beb1016f79d0a4ea26854fd36
Ed Warnicke <eaw@cisco.com>
2016-09-16 12:25:23 -0700

    Enabled push of rpm/deb packages for nsh_sfc
    Change-Id: Ie3aa47c7075e90f7a1e528a0dff96ab10224676f
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
+++ jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
@@ -2,6 +2,13 @@
 # basic build script example
 set -e -o pipefail
 if [ "${OS}" == "ubuntu1404" ]; then
+    # Find the files
+    DEBS=$(find . -type f -iname '*.deb')
+    for i in $DEBS
+    do
+        push_deb "$i"
+    done
+
     export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
     export JAVAC=${JAVA_HOME}/bin/javac
     export PATH=${JAVA_HOME}/bin/:${PATH}
@@ -9,4 +16,23 @@ if [ "${OS}" == "ubuntu1404" ]; then
     $MVN deploy \
         -gs "$GLOBAL_SETTINGS_FILE" -s "$SETTINGS_FILE"
     cd -
+
+elif [ "${OS}" == "ubuntu1604" ]; then
+
+    # Find the files
+    DEBS=$(find . -type f -iname '*.deb')
+    for i in $DEBS
+    do
+        push_deb "$i"
+    done
+
+elif [ "${OS}" == "centos7" ]; then
+    # Find the files
+    RPMS=$(find . -type f -iname '*.rpm')
+    SRPMS=$(find . -type f -iname '*.srpm')
+    SRCRPMS=$(find . -type f -name '*.src.rpm')
+    for i in $RPMS $SRPMS $SRCRPMS
+    do
+        push_rpm "$i"
+    done
 fi
