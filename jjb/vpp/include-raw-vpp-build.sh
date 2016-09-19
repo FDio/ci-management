5b2941003da81d477e0adbaeafef976c5b9a60b5
C.J. Collier <cjcollier@linuxfoundation.org>
2016-09-19 14:31:06 -0700

    Use compiler cache
    * add push/fetch_ccache functions to maven_push_functions
    - perhaps this file should be re-named to maven_functions?
    
    * before sourcing build script, source maven_push_functions in
    - jjb/nsh_sfc/nsh_sfc.yaml
    - jjb/trex/trex.yaml
    - jjb/tldk/tldk.yaml
    
    * call fetch/push_ccache at beginning/end of
    - jjb/vpp/include-raw-vpp-build.sh
    - jjb/tldk/include-raw-build.sh
    - jjb/trex/include-raw-trex-build.sh
    - jjb/nsh_sfc/include-raw-nsh_sfc-build.sh
    
    Change-Id: I623c41936d13421c7609d2fec81efc1785b33420
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

--- jjb/vpp/include-raw-vpp-build.sh
+++ jjb/vpp/include-raw-vpp-build.sh
@@ -4,12 +4,8 @@ set -xe -o pipefail
 # do nothing but print the current slave hostname
 hostname
 export CCACHE_DIR=/tmp/ccache
-if [ -d $CCACHE_DIR ];then
-    echo $CCACHE_DIR exists
-    du -sk $CCACHE_DIR
-else
-    echo $CCACHE_DIR does not exist.  This must be a new slave.
-fi
+
+fetch_ccache
 
 echo "cat /etc/bootstrap.sha"
 if [ -f /etc/bootstrap.sha ];then
@@ -51,6 +47,10 @@ if [ "x${VPP_REPO}" == "x1" ]; then
         exit 1
     fi
 fi
+
+build-root/vagrant/build.sh
+
+push_ccache
 
 echo "*******************************************************************"
 echo "* VPP BUILD SUCCESSFULLY COMPLETED"
