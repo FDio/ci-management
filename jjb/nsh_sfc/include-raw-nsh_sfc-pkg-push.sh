fbb1cb04db792dfc0766076f990c9b18ed8ae133
Ed Warnicke <eaw@cisco.com>
2016-09-19 10:12:40 -0700

    Add some echos to debug why packages aren't being pushed
    Change-Id: Ida656f01d1df5a943f8b588c5cc0e66dd0fdf5a6
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
+++ jjb/nsh_sfc/include-raw-nsh_sfc-pkg-push.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # basic build script example
 set -e -o pipefail
+echo "Looking for jars/debs/rpms to publish"
 if [ "${OS}" == "ubuntu1404" ]; then
     # Find the files
     DEBS=$(find . -type f -iname '*.deb')
+    echo "Found DEBS=${DEBS}"
     for i in $DEBS
     do
         push_deb "$i"
@@ -21,6 +23,7 @@ elif [ "${OS}" == "ubuntu1604" ]; then
 
     # Find the files
     DEBS=$(find . -type f -iname '*.deb')
+    echo "Found DEBS=${DEBS}"
     for i in $DEBS
     do
         push_deb "$i"
@@ -31,6 +34,9 @@ elif [ "${OS}" == "centos7" ]; then
     RPMS=$(find . -type f -iname '*.rpm')
     SRPMS=$(find . -type f -iname '*.srpm')
     SRCRPMS=$(find . -type f -name '*.src.rpm')
+    echo "Found RPMS=${RPMS}"
+    echo "Found SRPMS=${SRPMS}"
+    echo "Found SRCRPMS=${SRCRPMS}"
     for i in $RPMS $SRPMS $SRCRPMS
     do
         push_rpm "$i"
