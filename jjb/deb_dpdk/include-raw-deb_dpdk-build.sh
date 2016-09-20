c0c571bb0e508008dba69134062501bf42b15173
C.J. Collier <cjcollier@linuxfoundation.org>
2016-09-20 10:21:13 -0700

    Resolve trusty python-sphinx-rtd-theme dependencies
    * python-sphinx-rtd-theme is not in standard trusty repos
    - built 0.1.9-1.1 on/for trusty from
      http://archive.ubuntu.com/ubuntu/pool/main/s/sphinx-rtd-theme/sphinx-rtd-theme_0.1.9-1.dsc
    - published .deb packages in nexus.fd.io's thirdparty repo
    - added code to fetch and install during trusty build
    
    Change-Id: I1ecb1495ad28a26fb477cdb95730bec8a2a85ab6
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- jjb/deb_dpdk/include-raw-deb_dpdk-build.sh
+++ jjb/deb_dpdk/include-raw-deb_dpdk-build.sh
@@ -21,8 +21,27 @@ fi
 echo "sha1sum of this script: ${0}"
 sha1sum $0
 
+if [ $OS == 'ubuntu1404' ]
+then
+    dpkg -l python-sphinx-rtd-theme | grep -q '^ii' || (
+        sudo apt-get install -y fonts-font-awesome fonts-lato libjs-modernizr
+        local NEX_PFX=https://nexus.fd.io/content/repositories/thirdparty/sphinx/rtd/theme
+        local VER=0.1.9-1.1_all
+        local URLS=""
+        local FILES=""
+        for PKG in sphinx-rtd-theme-common python-sphinx-rtd-theme
+        do
+            URLS="${URLS} ${NEX_PFX}/${PKG}/${VER}/${PKG}-${VER}.deb"
+            FILES="${FILES} /tmp/${PKG}-${VER}.deb"
+        done
+        wget -P /tmp ${URLS}
+        sudo dpkg -i ${FILES}
+    )
+fi
+
 MISSING_PKGS=$(dpkg-checkbuilddeps |& perl -pe 's/^.+://g; s/\(.*?\)//g')
 sudo apt-get install -y ${MISSING_PKGS}
+
 debuild -uc -us -j4
 
 echo "*******************************************************************"
