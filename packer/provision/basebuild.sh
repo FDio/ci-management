f1019a98c4b3e2000d14692d40223decedb3408d
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2016-10-13 13:54:18 -0500

    Configure packer.io to replace Vagrant
    Change-Id: Ic87cce4aa674146952efad325a42b095086426d0
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

---
+++ packer/provision/basebuild.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# vim: ts=4 sw=4 sts=4 et tw=72 :
+
+rh_systems() {
+    # Install python dependencies
+    yum install -y python-{devel,virtualenv,setuptools,pip}
+
+    # Build dependencies for Python packages
+    yum install -y openssl-devel mysql-devel gcc
+
+    #Install Documentation packages
+    DOC_PKGS="doxygen graphviz pyparsing python-jinja2"
+    yum install -y install ${DOC_PKGS}
+
+    #Install components to build Ganglia modules
+    yum install -y --enablerepo=epel {libconfuse,ganglia}-devel mock
+
+    #Install debuginfo packages
+    debuginfo-install -y glibc-2.17-106.el7_2.4.x86_64 openssl-libs-1.0.1e-51.el7_2.4.x86_64 zlib-1.2.7-15.el7.x86_64
+
+    # Packer builds happen from the centos flavor images
+    PACKERDIR=$(mktemp -d)
+    # disable double quote checking
+    # shellcheck disable=SC2086
+    cd $PACKERDIR
+    wget https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip
+    unzip packer_0.10.1_linux_amd64.zip -d /usr/local/bin/
+    # rename packer to avoid conflicts with cracklib
+    mv /usr/local/bin/packer /usr/local/bin/packer.io
+
+    # cleanup from the installation
+    # disable double quote checking
+    # shellcheck disable=SC2086
+    rm -rf $PACKERDIR
+    # cleanup from previous install process
+    if [ -d /tmp/packer ]
+    then
+        rm -rf /tmp/packer
+    fi
+}
+
+ubuntu_systems() {
+    # Install python dependencies
+    apt install -y python-{dev,virtualenv,setuptools,pip}
+
+    # Build dependencies for Python packages
+    apt install -y libssl-dev libmysqlclient-dev gcc
+}
+
+all_systems() {
+    echo 'Record the bootstrap checksum'
+    echo "$0"
+    sha1sum "$0" > /etc/basebuild.sha
+
+    echo 'Configure keep alive to prevent timeout during testing'
+    local SSH_CFG=/etc/ssh/ssh_config
+    echo "TCPKeepAlive        true" | sudo tee -a ${SSH_CFG} >/dev/null 2>&1
+    echo "ServerAliveCountMax 30"   | sudo tee -a ${SSH_CFG} >/dev/null 2>&1
+    echo "ServerAliveInterval 10"   | sudo tee -a ${SSH_CFG} >/dev/null 2>&1
+
+}
+
+echo "---> Detecting OS"
+ORIGIN=$(facter operatingsystem | tr '[:upper:]' '[:lower:]')
+
+case "${ORIGIN}" in
+    fedora|centos|redhat)
+        echo "---> RH type system detected"
+        rh_systems
+    ;;
+    ubuntu)
+        echo "---> Ubuntu system detected"
+        ubuntu_systems
+    ;;
+    *)
+        echo "---> Unknown operating system"
+    ;;
+esac
+
+# execute steps for all systems
+all_systems
