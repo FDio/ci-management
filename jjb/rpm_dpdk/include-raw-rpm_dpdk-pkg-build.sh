896c1ee2aec450e94e132227c1053498bda34ddd
Thomas F Herbert <therbert@redhat.com>
2016-10-16 19:52:46 -0400

    Initial commit of support for rpm_dpdk project. Fix script name and removed Maven Definition missing required for Centos 7 builds.
    Change-Id: I6b7f4fab9c1b2d634e5576cd21977f0455a18cd1
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

---
+++ jjb/rpm_dpdk/include-raw-rpm_dpdk-pkg-build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# basic build script example
+set -e -o pipefail
+# do nothing but print the current slave hostname
+hostname
+export CCACHE_DIR=/tmp/ccache
+if [ -d $CCACHE_DIR ];then
+    echo $CCACHE_DIR exists
+    du -sk $CCACHE_DIR
+else
+    echo $CCACHE_DIR does not exist.  This must be a new slave.
+fi
+
+echo "cat /etc/bootstrap.sha"
+if [ -f /etc/bootstrap.sha ];then
+    cat /etc/bootstrap.sha
+else
+    echo "Cannot find /etc/bootstrap.sha"
+fi
+
+echo "cat /etc/bootstrap-functions.sha"
+if [ -f /etc/bootstrap-functions.sha ];then
+    cat /etc/bootstrap-functions.sha
+else
+    echo "Cannot find /etc/bootstrap-functions.sha"
+fi
+
+echo "sha1sum of this script: ${0}"
+sha1sum $0
+
+build/build.sh
+
+echo "*******************************************************************"
+echo "* rpm_dpdk BUILD SUCCESSFULLY COMPLETED"
+echo "*******************************************************************"
