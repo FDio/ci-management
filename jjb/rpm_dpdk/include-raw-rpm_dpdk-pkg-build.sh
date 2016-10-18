3623f5ac7497c369b7a7be650bc9cb3fcd0c2d4c
Thomas F Herbert <therbert@redhat.com>
2016-10-18 14:20:42 -0400

    Remove references to ccache.
    Change-Id: Ie3578599ec8493efb748fcf3602ed4fde22e77dc
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

--- jjb/rpm_dpdk/include-raw-rpm_dpdk-pkg-build.sh
+++ jjb/rpm_dpdk/include-raw-rpm_dpdk-pkg-build.sh
@@ -3,13 +3,6 @@
 set -e -o pipefail
 # do nothing but print the current slave hostname
 hostname
-export CCACHE_DIR=/tmp/ccache
-if [ -d $CCACHE_DIR ];then
-    echo $CCACHE_DIR exists
-    du -sk $CCACHE_DIR
-else
-    echo $CCACHE_DIR does not exist.  This must be a new slave.
-fi
 
 echo "cat /etc/bootstrap.sha"
 if [ -f /etc/bootstrap.sha ];then
