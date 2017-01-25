34dd1c16476a9e2ced33e35b02f4452c87b69e57
selias <samelias@cisco.com>
2017-01-25 17:15:01 +0100

    Csit-Honeycomb job for 17.01
     - relates to https://gerrit.fd.io/r/4933
    
    Change-Id: I78e0a28d529b3308d0acd6a11a45ff49b24a8919
    Signed-off-by: selias <samelias@cisco.com>

--- jjb/hc2vpp/include-raw-hc2vpp-csit-integration.sh
+++ jjb/hc2vpp/include-raw-hc2vpp-csit-integration.sh
@@ -2,7 +2,10 @@
 set -xeu -o pipefail
 
 # Clone csit and start tests
-git clone https://gerrit.fd.io/r/csit --branch master
+if [ ${STREAM} == 'master' ]; then
+    git clone https://gerrit.fd.io/r/csit --branch master
+else
+        git clone https://gerrit.fd.io/r/csit --branch 'rls'${STREAM}
 
 # If the git clone fails, complain clearly and exit
 if [ $? != 0 ]; then
@@ -20,7 +23,11 @@ else
     # make sure that bootstrap.sh is executable
     chmod +x bootstrap-hc2vpp-integration.sh
     # run the script
-    ./bootstrap-hc2vpp-integration.sh
+    if [ ${STREAM} == 'master' ]; then
+        ./bootstrap-hc2vpp-integration.sh ${STREAM}
+    else
+        ./bootstrap-hc2vpp-integration.sh 'stable.'${STREAM}
+    fi
 fi
 
 # vim: ts=4 ts=4 sts=4 et :
--- jjb/hc2vpp/include-raw-hc2vpp-csit-verify-prebuild.sh
+++ jjb/hc2vpp/include-raw-hc2vpp-csit-verify-prebuild.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 set -xeu -o pipefail
 
-current_dir = `pwd`
+current_dir=`pwd`
 cd ${WORKSPACE}
 
-# Clone csit and run package download script
-git clone https://gerrit.fd.io/r/csit --branch master
+# Get CSIT branch from which to test from
+# running build-root/scripts/csit-test-branch
+if [ -x csit-test-branch ]; then
+    CSIT_BRANCH=`csit-test-branch`;
+fi
+
+# Clone csit and start tests
+git clone https://gerrit.fd.io/r/csit --branch ${CSIT_BRANCH}
 
 # If the git clone fails, complain clearly and exit
 if [ $? != 0 ]; then
-    echo "Failed to run: git clone https://gerrit.fd.io/r/csit --branch master"
+    echo "Failed to run: git clone https://gerrit.fd.io/r/csit --branch ${CSIT_BRANCH}"
     exit
 fi
 
-./csit/resources/tools/download_hc_build_pkgs.sh
+if [ ${STREAM} == 'master' ]; then
+    ./csit/resources/tools/download_hc_build_pkgs.sh ${STREAM}
+else
+    ./csit/resources/tools/download_hc_build_pkgs.sh 'stable.'${STREAM}
+fi
 
 cd ${current_dir}
