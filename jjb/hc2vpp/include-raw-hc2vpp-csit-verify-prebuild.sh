de80838b716b6e93c017ceba561ed5306c720c6d
selias <samelias@cisco.com>
2017-01-25 17:15:01 +0100

    Csit-Honeycomb job for 17.01
     - relates to https://gerrit.fd.io/r/4933
    
    Change-Id: I78e0a28d529b3308d0acd6a11a45ff49b24a8919
    Signed-off-by: selias <samelias@cisco.com>

--- jjb/hc2vpp/include-raw-hc2vpp-csit-verify-prebuild.sh
+++ jjb/hc2vpp/include-raw-hc2vpp-csit-verify-prebuild.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 set -xeu -o pipefail
 
-current_dir = `pwd`
+current_dir=`pwd`
 cd ${WORKSPACE}
 
-# Clone csit and run package download script
-git clone https://gerrit.fd.io/r/csit --branch master
+# Get CSIT branch from which to test from
+# running build-root/scripts/csit-test-branch
+if [ -x csit-test-branch ]; then
+    CSIT_BRANCH=`csit-test-branch`;
+fi
+
+# Clone csit and start tests
+git clone https://gerrit.fd.io/r/csit --branch ${CSIT_BRANCH}
 
 # If the git clone fails, complain clearly and exit
 if [ $? != 0 ]; then
-    echo "Failed to run: git clone https://gerrit.fd.io/r/csit --branch master"
+    echo "Failed to run: git clone https://gerrit.fd.io/r/csit --branch ${CSIT_BRANCH}"
     exit
 fi
 
-./csit/resources/tools/download_hc_build_pkgs.sh
+./csit/resources/tools/download_hc_build_pkgs.sh ${STREAM}
 
 cd ${current_dir}
