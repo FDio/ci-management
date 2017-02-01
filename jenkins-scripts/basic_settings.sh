6c9cd202f0366274b630713f87eaf9d0e078d525
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2017-02-01 15:27:10 -0600

    Remove Ubuntu mirror
    Reverting https://gerrit.fd.io/r/#/c/4988/ due to broken builds.  We'll
    submit a new change to configure the mirror once it's been properly
    tested.
    
    Change-Id: Ice894d9c1c48b5a11f45e5e80523dd1ac6e410a1
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

--- jenkins-scripts/basic_settings.sh
+++ jenkins-scripts/basic_settings.sh
@@ -17,9 +17,6 @@ case "$(facter operatingsystem)" in
 
     # attach to the fd.io.dev apt repo
     echo 'deb http://nexus.fd.io/content/repositories/fd.io.dev/ ./' >> /etc/apt/sources.list
-
-    # Configure Ubuntu mirror
-    perl -pi -e 'unless(m{(security|fd\.io)}){ s{://[^/]+/}{://ca.archive.ubuntu.com/ubuntu/ } }' /etc/apt/sources.list
     ;;
   *)
     # Do nothing on other distros for now
