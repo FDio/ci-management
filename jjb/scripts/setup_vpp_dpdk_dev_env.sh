9fc108d19c27a003698ff01c2c52842a89261f39
Ed Warnicke <eaw@cisco.com>
2017-03-01 12:13:32 -0700

    Fix vpp merge jobs
    This patch switches to installing the vpp-dpkg-dev whenever
    possible rather than building it.  This means no duplicative
    pushing of vpp-dpkg-dev, which should fix the merge jobs.
    It also should mean faster vpp builds since we generally
    won't need to build DPDK either.
    
    Change-Id: I602fb09a847bb4b0d54c5844dc4c79a27393580d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/scripts/setup_vpp_dpdk_dev_env.sh
+++ jjb/scripts/setup_vpp_dpdk_dev_env.sh
@@ -20,25 +20,24 @@ echo DISTRIB_CODENAME: $DISTRIB_CODENAME
 echo DISTRIB_DESCRIPTION: $DISTRIB_DESCRIPTION
 
 function setup {
-    if [ -z ${REPO_NAME} ]; then
-        REPO_URL="${NEXUSPROXY}/content/repositories/fd.io.${REPO_NAME}"
-        echo "REPO_URL: ${REPO_URL}"
-        # Setup by installing vpp-dev and vpp-lib
-        if [ $DISTRIB_ID == "Ubuntu" ]; then
-            echo "deb ${REPO_URL} ./" | sudo tee /etc/apt/sources.list.d/99fd.io.list
-            sudo apt-get update
-            sudo apt-get -y --force-yes install vpp-dev vpp-lib
-        elif [[ $DISTRIB_ID == "CentOS" ]]; then
-            sudo cat << EOF > fdio-master.repo
+    echo "INSTALLING VPP-DPKG-DEV from apt/yum repo"
+    REPO_URL="${NEXUSPROXY}/content/repositories/fd.io.${REPO_NAME}"
+    echo "REPO_URL: ${REPO_URL}"
+    # Setup by installing vpp-dev and vpp-lib
+    if [ $DISTRIB_ID == "Ubuntu" ]; then
+        echo "deb ${REPO_URL} ./" | sudo tee /etc/apt/sources.list.d/99fd.io.list
+        sudo apt-get update || true
+        sudo apt-get -y --force-yes install vpp-dpdk-dev || true
+    elif [[ $DISTRIB_ID == "CentOS" ]]; then
+        sudo cat << EOF > fdio-master.repo
 [fdio-master]
 name=fd.io master branch latest merge
 baseurl=${REPO_URL}
 enabled=1
 gpgcheck=0
 EOF
-            sudo mv fdio-master.repo /etc/yum.repos.d/fdio-master.repo
-            sudo yum -y install vpp-devel vpp-lib
-        fi
+        sudo mv fdio-master.repo /etc/yum.repos.d/fdio-master.repo
+        sudo yum -y install vpp-dpdk-dev || true
     fi
 }
 
