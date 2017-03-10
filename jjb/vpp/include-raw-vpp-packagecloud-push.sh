fb7ef559753b17fc33b536f7828cef11923e555f
Ed Warnicke <eaw@cisco.com>
2017-03-10 11:23:19 -0700

    Adding distinguishers for debian/centos releases
    Change-Id: I0b04dcb4b12d69b83c71d96f22717e4d14bbca37
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/vpp/include-raw-vpp-packagecloud-push.sh
+++ jjb/vpp/include-raw-vpp-packagecloud-push.sh
@@ -4,13 +4,13 @@ if [ "${OS}" == "ubuntu1404" ]; then
     DEBS=$(find . -type f -iname '*.deb')
     for i in $DEBS
     do
-        package_cloud push fdio/test "$i"
+        package_cloud push fdio/test/ubuntu/trusty/main "$i"
     done
 elif [ "${OS}" == "ubuntu1604" ]; then
     DEBS=$(find . -type f -iname '*.deb')
     for i in $DEBS
     do
-        package_cloud push fdio/test "$i"
+        package_cloud push fdio/test/ubuntu/xenial/main "$i"
     done
 elif [ "${OS}" == "centos7" ]; then
     # Find the files
@@ -19,6 +19,6 @@ elif [ "${OS}" == "centos7" ]; then
     SRCRPMS=$(find . -type f -name '*.src.rpm')
     for i in $RPMS $SRPMS $SRCRPMS
     do
-        package_cloud push fdio/test "$i"
+        package_cloud push fdio/test/centos7 "$i"
     done
 fi
