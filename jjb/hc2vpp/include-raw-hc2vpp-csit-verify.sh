056928ef4743c00469e86bf0b572504358600853
Marek Gradzki <mgradzki@cisco.com>
2017-03-21 09:26:47 +0100

    Disable CSIT functional tests for ubuntu14
    Change-Id: I2f870d39a52c5c8b77dcd980ed017884d194007b
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

--- jjb/hc2vpp/include-raw-hc2vpp-csit-verify.sh
+++ jjb/hc2vpp/include-raw-hc2vpp-csit-verify.sh
@@ -2,16 +2,8 @@
 set -xeu -o pipefail
 
 # create HC .deb packages
-if [ "${OS}" == "ubuntu1404" ]; then
-
-    ./packaging/deb/trusty/debuild.sh
-    cp ./packaging/deb/trusty/*.deb ${WORKSPACE}/csit
-
-elif [ "${OS}" == "ubuntu1604" ]; then
-
-    ./packaging/deb/xenial/debuild.sh
-    cp ./packaging/deb/xenial/*.deb ${WORKSPACE}/csit
-fi
+./packaging/deb/xenial/debuild.sh
+cp ./packaging/deb/xenial/*.deb ${WORKSPACE}/csit
 
 cd ${WORKSPACE}/csit
 # execute csit bootstrap script if it exists
