c4b3d771f5ae7cf796e5aa7401ab3e3a8fa674ba
Ed Warnicke <eaw@cisco.com>
2017-03-27 14:45:48 -0700

    Try to push rpm/deb to package cloud in a single transaction
    Change-Id: I8086e4b615757ecc0b37c0bcd4eb020eba3a4c9f
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/vpp/include-raw-vpp-packagecloud-push.sh
+++ jjb/vpp/include-raw-vpp-packagecloud-push.sh
@@ -2,23 +2,14 @@
 
 if [ "${OS}" == "ubuntu1404" ]; then
     DEBS=$(find . -type f -iname '*.deb')
-    for i in $DEBS
-    do
-        package_cloud push fdio/test/ubuntu/trusty/main "$i"
-    done
+    package_cloud push fdio/test/ubuntu/trusty/main ${DEBS}
 elif [ "${OS}" == "ubuntu1604" ]; then
     DEBS=$(find . -type f -iname '*.deb')
-    for i in $DEBS
-    do
-        package_cloud push fdio/test/ubuntu/xenial/main "$i"
-    done
+    package_cloud push fdio/test/ubuntu/xenial/main ${DEBS}
 elif [ "${OS}" == "centos7" ]; then
     # Find the files
     RPMS=$(find . -type f -iname '*.rpm')
     SRPMS=$(find . -type f -iname '*.srpm')
     SRCRPMS=$(find . -type f -name '*.src.rpm')
-    for i in $RPMS $SRPMS $SRCRPMS
-    do
-        package_cloud push fdio/test/el/7/os/x86_64/ "$i"
-    done
+    package_cloud push fdio/test/el/7/os/x86_64/ ${RPMS} ${SRPMS} ${SRCRPMS}
 fi
