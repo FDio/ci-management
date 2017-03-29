6b6b929c9db9bafdd0b2845cfd7e962aeb32c25e
selias <samelias@cisco.com>
2017-03-29 13:15:36 +0200

    Use single VPP version dependency in published hc2vpp packages
     - HC2VPP-122 - create packages with specific version dependency
     - *todo* CSIT-544 - modify jobs to download the specific VPP version required
    
    Change-Id: I7a274fcbb4a1659bde3018de5460b35029993ac7
    Signed-off-by: selias <samelias@cisco.com>

---
+++ jjb/hc2vpp/include-raw-hc2vpp-integration-prebuild.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -ex
+
+STREAM=$1
+OS=$2
+
+# Download the latest VPP java API package and its dependencies
+URL="https://nexus.fd.io/service/local/artifact/maven/content"
+VERSION="RELEASE"
+GROUP="io.fd.vpp"
+ARTIFACTS="vpp vpp-lib vpp-plugins vpp-api-java"
+
+if [ "${OS}" == "ubuntu1404" ]; then
+    OS="ubuntu.trusty.main"
+    PACKAGE="deb deb.md5"
+    CLASS="deb"
+elif [ "${OS}" == "ubuntu1604" ]; then
+    OS="ubuntu.xenial.main"
+    PACKAGE="deb deb.md5"
+    CLASS="deb"
+elif [ "${OS}" == "centos7" ]; then
+    OS="centos7"
+    PACKAGE="rpm rpm.md5"
+    CLASS="rpm"
+fi
+
+REPO="fd.io.${STREAM}.${OS}"
+
+for ART in ${ARTIFACTS}; do
+    for PAC in ${PACKAGE}; do
+        curl "${URL}?r=${REPO}&g=${GROUP}&a=${ART}&p=${PAC}&v=${VERSION}&c=${CLASS}" -O -J || exit
+    done
+done
+
+for FILE in *.deb; do
+    echo " "${FILE} >> ${FILE}.md5
+done
+for FILE in *.rpm; do
+    echo " "${FILE} >> ${FILE}.md5
+done
+for MD5FILE in *.md5; do
+    md5sum -c ${MD5FILE} || exit
+done
+
+# install vpp-api-java and dependencies, this extracts .jar files into usr/share/java
+sudo dpkg -i vpp*
+rm vpp*
+
+# install jvpp jars into maven repo, so that maven picks them up when building hc2vpp
+current_dir=`pwd`
+cd /usr/share/java
+
+for item in jvpp*.jar; do
+    # Example filename: jvpp-registry-17.01-20161206.125556-1.jar
+    # ArtifactId = jvpp-registry
+    # Version = 17.01
+    basefile=$(basename -s .jar "$item")
+    artifactId=$(echo "$basefile" | cut -d '-' -f 1-2)
+    version=$(echo "$basefile" | cut -d '-' -f 3)
+    mvn install:install-file -Dfile=${item} -DgroupId=io.fd.vpp -DartifactId=${artifactId} -Dversion=${version} -Dpackaging=jar -Dmaven.repo.local=/tmp/r -Dorg.ops4j.pax.url.mvn.localRepository=/tmp/r
+done
+
+cd current_dir
--- jjb/hc2vpp/include-raw-hc2vpp-pkg-push.sh
+++ jjb/hc2vpp/include-raw-hc2vpp-pkg-push.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+# determine JVPP version used in maven build
+if [ "${OS}" == "centos7" ]; then
+    VERSION=`yum list installed vpp-api-java | grep vpp-api-java | awk '{ printf $2; }'`
+    # write a file that will echo VPP dependencies
+    echo -n 'echo' > vpp_dependencies
+    echo " \"vpp = ${VERSION}, vpp-plugins = ${VERSION}\"" >> vpp_dependencies
+    chmod +x vpp_dependencies
+    # overwrite default dependencies file
+    mv vpp_dependencies packaging/rpm/
+else
+    VERSION=`apt list --installed | grep vpp-api-java | awk '{ printf $2; }'`
+    # write a file that will echo VPP dependencies
+    echo -n 'echo' > vpp_dependencies
+    echo " \"vpp (= ${VERSION}), vpp-plugins (= ${VERSION})\"" >> vpp_dependencies
+    chmod +x vpp_dependencies
+    # overwrite default dependencies file
+    mv vpp_dependencies packaging/deb/common/
+fi
+
 if [ "${OS}" == "centos7" ]; then
 
     # Build the rpms
