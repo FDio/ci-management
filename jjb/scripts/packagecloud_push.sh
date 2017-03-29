5fbe770c61ba1e2a645f706a44bbe334f473c59f
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2017-03-29 13:08:25 -0500

    Configure VPP to publish to packagecloud.io
    Added a packagecloud_push script to push artifacts to the
    packagecloud.io repository
    
    Added the stream-parameter and packagecloud_push script to the VPP merge
    job to allow artifacts to be published to packagecloud repositories in
    addition to Nexus repositories
    
    Change-Id: I9076ee35a6d330579ba72aa19a455b3d7a22e7f5
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

---
+++ jjb/scripts/packagecloud_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# PCIO_CO is a Jenkins Global Environment variable
+
+FACTER_OS=$(/usr/bin/facter operatingsystem)
+
+case "$FACTER_OS" in
+  Ubuntu)
+    FACTER_LSBNAME=$(/usr/bin/facter lsbdistcodename)
+    DEBS=$(find . -type f -iname '*.deb')
+    package_cloud push "${PCIO_CO}/${STREAM}/ubuntu/${FACTER_LSBNAME}/main/" ${DEBS}
+  ;;
+  CentOS)
+    FACTER_OSMAJREL=$(/usr/bin/facter operatingsystemmajrelease)
+    FACTER_ARCH=$(/usr/bin/facter architecture)
+    RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
+    package_cloud push "${PCIO_CO}/${STREAM}/el/${FACTER_OSMAJREL}/os/${FACTER_ARCH}/" ${RPMS}
+  ;;
+esac
