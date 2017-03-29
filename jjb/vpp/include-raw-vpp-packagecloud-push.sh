5c49501a6428ed955aea5c3601a4386c05779ca1
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2017-03-29 13:08:25 -0500

    Adding VPP merge job to publish to packagecloud.io
    This job will publish VPP artifacts to a packagecloud.io repository in
    addition to the Nexus apt/yum repositories.  This change is in
    preparation for automated releases.
    
    In addition to creating a new job, this change is also reconfiguring the
    push script to be reusable.
    
    Change-Id: I9076ee35a6d330579ba72aa19a455b3d7a22e7f5
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

--- jjb/vpp/include-raw-vpp-packagecloud-push.sh
+++ jjb/vpp/include-raw-vpp-packagecloud-push.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-if [ "${OS}" == "ubuntu1404" ]; then
-    DEBS=$(find . -type f -iname '*.deb')
-    package_cloud push fdio/test/ubuntu/trusty/main ${DEBS}
-elif [ "${OS}" == "ubuntu1604" ]; then
-    DEBS=$(find . -type f -iname '*.deb')
-    package_cloud push fdio/test/ubuntu/xenial/main ${DEBS}
-elif [ "${OS}" == "centos7" ]; then
-    # Find the files
-    RPMS=$(find . -type f -iname '*.rpm')
-    SRPMS=$(find . -type f -iname '*.srpm')
-    SRCRPMS=$(find . -type f -name '*.src.rpm')
-    package_cloud push fdio/test/el/7/os/x86_64/ ${RPMS} ${SRPMS} ${SRCRPMS}
-fi
+STREAM='{$STREAM}'
+FACTER_OS='{$FACTER_OS}'
+FACTER_OSVER='{$FACTER_OSVER}'
+
+case "$FACTER_OS" in
+    Ubuntu)
+        if [ "$FACTER_OSVER" == "14.04" ]
+        then
+            DEBS=$(find . -type f -iname '*.deb')
+            package_cloud push '${PCIO_CO}/${STREAM}/ubuntu/trusty/main/' ${DEBS}
+        elif [ "$FACTER_OSVER" == "16.04" ]
+        then
+            DEBS=$(find . -type f -iname '*.deb')
+            package_cloud push '${PCIO_CO}/${STREAM}/ubuntu/xenial/main/' ${DEBS}
+        else
+            echo "---> OS Release is not supported"
+        fi
+    ;;
+    CentOS)
+        if [ "$FACTER_OSVER" == "7" ]
+        then
+            RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
+            package_cloud push '${PCIO_CO}/${STREAM}/el/7/os/x86_64/' ${RPMS}
+        else
+            echo "---> OS Release is not supported"
+        fi
+    ;;
+esac 
