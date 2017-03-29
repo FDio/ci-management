9a3e69900079dfde9a4901a69935680ff93a217b
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2017-03-29 13:08:25 -0500

    Adding VPP merge job to publish to packagecloud.io
    This job will publish VPP artifacts to a packagecloud.io repository in
    addition to the Nexus apt/yum repositories.  This change is in
    preparation for automated releases.
    
    In addition to creating a new job, this change is also reconfiguring the
    push script to be reusable.
    
    Change-Id: I9076ee35a6d330579ba72aa19a455b3d7a22e7f5
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

---
+++ jjb/scripts/packagecloud_push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "${OS}" == "ubuntu1404" ]; then
+    DEBS=$(find . -type f -iname '*.deb')
+    package_cloud push ${PCIO_CO}/${stream}/ubuntu/trusty/main ${DEBS}
+elif [ "${OS}" == "ubuntu1604" ]; then
+    DEBS=$(find . -type f -iname '*.deb')
+    package_cloud push ${PCIO_CO}/${stream}/ubuntu/xenial/main ${DEBS}
+elif [ "${OS}" == "centos7" ]; then
+    # Find the files
+    RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
+    package_cloud push ${PCIO_CO}/${stream}/el/7/os/x86_64/ ${RPMS}
