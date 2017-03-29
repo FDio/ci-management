63a83ad19f009a93e7e705ed4afd1b233c495fa1
selias <samelias@cisco.com>
2017-03-29 13:15:36 +0200

    HC2VPP-122 Use single VPP version dependency in published hc2vpp packages
     - download newest vpp-api-java to be used in hc2vpp build
     - publish hc2vpp packages with dependency on the version used
    
    Change-Id: I7a274fcbb4a1659bde3018de5460b35029993ac7
    Signed-off-by: selias <samelias@cisco.com>

---
+++ jjb/hc2vpp/include-raw-hc2vpp-integration-prebuild.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -ex
+
+# Download the latest VPP java API package
+URL="https://nexus.fd.io/service/local/artifact/maven/content"
+VERSION="RELEASE"
+GROUP="io.fd.vpp"
+ARTIFACTS="vpp-api-java"
+
+if [ "${OS}" == "ubuntu1404" ]; then
+    OS_PART="ubuntu.trusty.main"
+    PACKAGE="deb deb.md5"
+    CLASS="deb"
+elif [ "${OS}" == "ubuntu1604" ]; then
+    OS_PART="ubuntu.xenial.main"
+    PACKAGE="deb deb.md5"
+    CLASS="deb"
+elif [ "${OS}" == "centos7" ]; then
+    OS_PART="centos7"
+    PACKAGE="rpm rpm.md5"
+    CLASS=""
+fi
+
+if [ "${STREAM}" == "master" ]; then
+    STREAM_PART="master"
+else
+    STREAM_PART="stable.${STREAM}"
+fi
+
+REPO="fd.io.${STREAM_PART}.${OS_PART}"
+
+for ART in ${ARTIFACTS}; do
+    for PAC in ${PACKAGE}; do
+        curl "${URL}?r=${REPO}&g=${GROUP}&a=${ART}&p=${PAC}&v=${VERSION}&c=${CLASS}" -O -J || exit
+    done
+done
+
+# verify downloaded package
+if [ "${OS}" == "centos7" ]; then
+    FILES=*.rpm
+else
+    FILES=*.deb
+fi
+
+for FILE in ${FILES}; do
+    echo " "${FILE} >> ${FILE}.md5
+done
+for MD5FILE in *.md5; do
+    md5sum -c ${MD5FILE} || exit
+    rm ${MD5FILE}
+done
+
+# install vpp-api-java, this extracts jvpp .jar files into usr/share/java
+if [ "${OS}" == "centos7" ]; then
+    sudo rpm --nodeps --install vpp-api-java*
+else
+    sudo dpkg --ignore-depends=vpp --install vpp-api-java*
+fi
+rm vpp-api-java*
+
+# install jvpp jars into maven repo, so that maven picks them up when building hc2vpp
+current_dir=`pwd`
+cd /usr/share/java
+
+for item in jvpp*.jar; do
+    # Example filename: jvpp-registry-17.01-20161206.125556-1.jar
+    # ArtifactId = jvpp-registry
+    # Version = 17.01
+    basefile=$(basename -s .jar "$item")
+    artifactId=$(echo "$basefile" | cut -d '-' -f 1-2)
+    version=$(echo "$basefile" | cut -d '-' -f 3)
+    mvn install:install-file -Dfile=${item} -DgroupId=io.fd.vpp -DartifactId=${artifactId} -Dversion=${version} -Dpackaging=jar -Dmaven.repo.local=/tmp/r -Dorg.ops4j.pax.url.mvn.localRepository=/tmp/r
+done
+
+cd ${current_dir}
--- jjb/hc2vpp/include-raw-hc2vpp-pkg-push.sh
+++ jjb/hc2vpp/include-raw-hc2vpp-pkg-push.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+set -x
+
+# determine VPP Java API version used in maven build
+if [ "${OS}" == "centos7" ]; then
+    VERSION=`yum list installed vpp-api-java | grep vpp-api-java | awk '{ printf $2; }'`
+    # write a file that will echo VPP dependencies
+    echo -n 'echo' > vpp_dependencies
+    echo " \"vpp = ${VERSION}, vpp-plugins = ${VERSION}\"" >> vpp_dependencies
+    chmod +x vpp_dependencies
+    # overwrite default dependencies file
+    mv vpp_dependencies packaging/rpm/
+else
+    VERSION=`apt list --installed | grep vpp-api-java | awk '{ printf $2; }'`
+    # write a file that will echo VPP dependencies
+    echo -n 'echo' > vpp_dependencies
+    echo " \"vpp (= ${VERSION}), vpp-plugins (= ${VERSION})\"" >> vpp_dependencies
+    chmod +x vpp_dependencies
+    # overwrite default dependencies file
+    mv vpp_dependencies packaging/deb/common/
+fi
+
 if [ "${OS}" == "centos7" ]; then
 
     # Build the rpms
@@ -8,6 +29,7 @@ if [ "${OS}" == "centos7" ]; then
     RPMS=$(find . -type f -iname '*.rpm')
     SRPMS=$(find . -type f -iname '*.srpm')
     SRCRPMS=$(find . -type f -name '*.src.rpm')
+    # publish hc2vpp packages
     for i in $RPMS $SRPMS $SRCRPMS
     do
         push_rpm "$i"
@@ -19,6 +41,7 @@ elif [ "${OS}" == "ubuntu1404" ]; then
 
     # Find the files
     DEBS=$(find . -type f -iname '*.deb')
+    # publish hc2vpp packages
     for i in $DEBS
     do
         push_deb "$i"
@@ -30,6 +53,7 @@ elif [ "${OS}" == "ubuntu1604" ]; then
 
     # Find the files
     DEBS=$(find . -type f -iname '*.deb')
+    # publish hc2vpp packages
     for i in $DEBS
     do
         push_deb "$i"
