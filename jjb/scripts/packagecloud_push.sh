5d5ba3552af1934d0a8e4cc991e4cda7cd752016
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2017-03-29 13:08:25 -0500

    Adding VPP merge job to publish to packagecloud.io
    This job will publish VPP artifacts to a packagecloud.io repository in
    addition to the Nexus apt/yum repositories.  This change is in
    preparation for automated releases.
    
    In addition to creating a new job, this change is also reconfiguring the
    push script to be reusable.
    
    Change-Id: I9076ee35a6d330579ba72aa19a455b3d7a22e7f5
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

---
+++ jjb/scripts/packagecloud_push.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+STREAM='{$stream}'
+FACTER_OS='{$FACTER_OS}'
+FACTER_OSVER='{$FACTER_OSVER}'
+
+case "$FACTER_OS" in
+    Ubuntu)
+        if [ "$FACTER_OSVER" == "14.04" ]
+        then
+            DEBS=$(find . -type f -iname '*.deb')
+            package_cloud push '${PCIO_CO}/${STREAM}/ubuntu/trusty/main/' ${DEBS}
+        elif [ "$FACTER_OSVER" == "16.04" ]
+        then
+            DEBS=$(find . -type f -iname '*.deb')
+            package_cloud push '${PCIO_CO}/$STREAM}/ubuntu/xenial/main/' ${DEBS}
+        else
+            echo "---> OS Release push of packages to package cloud not supported"
+        fi
+    ;;
+    CentOS)
+        if [ "$FACTER_OSVER" == "7" ]
+        then
+            RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
+            package_cloud push '${PCIO_CO}/$STREAM}/el/7/os/x86_64/' ${RPMS}
+        else
+            echo "---> OS Release push of packages to package cloud not supported"
+        fi
+    ;;
+esac
