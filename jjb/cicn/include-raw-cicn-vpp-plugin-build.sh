3c2958be4e6fed64638bb4e5777cd43efe0d02bc
Mauro Sardara <msardara+fdio@cisco.com>
2017-03-30 11:37:48 +0200

    Trigger daily build of CICN packages
    Change-Id: If9653487e726a2cbe87d59958e1a3eff804c2f50
    Signed-off-by: Mauro Sardara <msardara+fdio@cisco.com>

--- jjb/cicn/include-raw-cicn-vpp-plugin-build.sh
+++ jjb/cicn/include-raw-cicn-vpp-plugin-build.sh
@@ -3,7 +3,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-PACKAGE_NAME="LIBCCNX_COMMON"
-PACKAGE_DEPS="LIBCCNX_COMMON_DEPS"
-cd libccnx-common
+PACKAGE_NAME="VPP_PLUGIN"
+PACKAGE_DEPS="VPP_PLUGIN_DEPS"
+
+cd cicn-plugin
+
 build_package $PACKAGE_NAME $PACKAGE_DEPS
