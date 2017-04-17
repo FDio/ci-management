a84fc17443864f4c02bde817e184ab1a7d7e50a2
Ed Warnicke <eaw@cisco.com>
2017-04-17 13:48:06 -0700

    Make packagecloud push conditional on existence of ~/.packagecloud
    Change-Id: I34d88bfd6f4c55c00d354aae13536a8e3eb8e613
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- jjb/scripts/packagecloud_push.sh
+++ jjb/scripts/packagecloud_push.sh
@@ -3,17 +3,18 @@
 # PCIO_CO is a Jenkins Global Environment variable
 
 FACTER_OS=$(/usr/bin/facter operatingsystem)
-
-case "$FACTER_OS" in
-  Ubuntu)
-    FACTER_LSBNAME=$(/usr/bin/facter lsbdistcodename)
-    DEBS=$(find . -type f -iname '*.deb')
-    package_cloud push "${PCIO_CO}/${STREAM}/ubuntu/${FACTER_LSBNAME}/main/" ${DEBS}
-  ;;
-  CentOS)
-    FACTER_OSMAJREL=$(/usr/bin/facter operatingsystemmajrelease)
-    FACTER_ARCH=$(/usr/bin/facter architecture)
-    RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
-    package_cloud push "${PCIO_CO}/${STREAM}/el/${FACTER_OSMAJREL}/os/${FACTER_ARCH}/" ${RPMS}
-  ;;
-esac
+if [ -f ~/.packagecloud ]; then
+    case "$FACTER_OS" in
+      Ubuntu)
+        FACTER_LSBNAME=$(/usr/bin/facter lsbdistcodename)
+        DEBS=$(find . -type f -iname '*.deb')
+        package_cloud push "${PCIO_CO}/${STREAM}/ubuntu/${FACTER_LSBNAME}/main/" ${DEBS}
+      ;;
+      CentOS)
+        FACTER_OSMAJREL=$(/usr/bin/facter operatingsystemmajrelease)
+        FACTER_ARCH=$(/usr/bin/facter architecture)
+        RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
+        package_cloud push "${PCIO_CO}/${STREAM}/el/${FACTER_OSMAJREL}/os/${FACTER_ARCH}/" ${RPMS}
+      ;;
+    esac
+fi
