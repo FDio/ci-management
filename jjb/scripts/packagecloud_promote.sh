d165cbfe87f396f85174d54b7d9a30fc5fc4099c
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2017-04-17 15:01:36 -0500

    Configure Jenkins packagecloud promote job
    Add manual VPP job to promote artifacts from packagecloud.io staging
    repository to packagecloud.io release repository
    
    Add packagecloud_api Jenkins managed file
    
    Add packagecloud_promote script
    
    Change-Id: If1ef1162f82201ea41b685eb45d0f09c0c7d1ce9
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

---
+++ jjb/scripts/packagecloud_promote.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+stage_repo="https://packagecloud.io/api/v1/repos/fdio/staging"
+curl --netrc-file /home/jenkins/packagecloud_api $stage_repo/packages.json \
+    | python -mjson.tool >filenames.txt
+grep 'promote_url' filenames.txt > packages.txt
+sed -i 's|[",:]||g' packages.txt
+sed -i 's/promote_url//g' packages.txt
+
+while read packages; do
+echo $packages
+curl --netrc-file /home/jenkins/packagecloud_api -v -o -X POST -F \ 
+destination=fdio/release/ https://packagecloud.io$packages
+
+done <packages.txt
