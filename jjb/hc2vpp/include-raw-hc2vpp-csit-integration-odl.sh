707bf79a763eeb7b7eb60c663e35ddd40686ead3
selias <samelias@cisco.com>
2017-05-15 10:58:28 +0200

    CSIT-577 Add Honeycomb test jobs with ODL
    Tests use ODL client to translate Restconf requests into Netconf.
    
    ODL client used is located on VIRL in /nfs/common and accessed through
    CSIT scripts added in https://gerrit.fd.io/r/6663
    
    Change-Id: I3f6041e61d0ccce0b70433053b8845594a9d6e4e
    Signed-off-by: selias <samelias@cisco.com>

---
+++ jjb/hc2vpp/include-raw-hc2vpp-csit-integration-odl.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -xeu -o pipefail
+
+# Get CSIT branch
+if [ -f csit-test-branch ]; then
+    chmod +x csit-test-branch
+    CSIT_BRANCH=`./csit-test-branch`
+else
+    CSIT_BRANCH='master'
+fi
+
+# Clone csit
+git clone https://gerrit.fd.io/r/csit --branch ${CSIT_BRANCH}
+
+# If the git clone fails, complain clearly and exit
+if [ $? != 0 ]; then
+    echo "Failed to run: git clone https://gerrit.fd.io/r/csit --branch master"
+    exit
+fi
+
+cd csit
+# execute csit bootstrap script if it exists
+if [ ! -e bootstrap-hc2vpp-integration.sh ]
+then
+    echo 'ERROR: No bootstrap-hc2vpp-integration.sh found'
+    exit 1
+else
+    # make sure that bootstrap.sh is executable
+    chmod +x bootstrap-hc2vpp-integration.sh
+    # run the script
+    if [ ${STREAM} == 'master' ]; then
+        ./bootstrap-hc2vpp-integration-odl.sh ${STREAM} ${OS}
+    else
+        ./bootstrap-hc2vpp-integration-odl.sh 'stable.'${STREAM} ${OS}
+    fi
+fi
+
+# vim: ts=4 ts=4 sts=4 et :
