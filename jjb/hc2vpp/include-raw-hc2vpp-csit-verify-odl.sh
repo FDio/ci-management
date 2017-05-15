0bb24308b58db536098d91c41adef23de45aeebf
selias <samelias@cisco.com>
2017-05-15 10:58:28 +0200

    CSIT-577 Add Honeycomb test jobs with ODL
    Tests use ODL client to translate Restconf requests into Netconf.
    
    ODL client used is located on VIRL in /nfs/common and accessed through
    CSIT scripts added in https://gerrit.fd.io/r/6663
    
    Change-Id: I3f6041e61d0ccce0b70433053b8845594a9d6e4e
    Signed-off-by: selias <samelias@cisco.com>

---
+++ jjb/hc2vpp/include-raw-hc2vpp-csit-verify-odl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -xeu -o pipefail
+
+# create HC .deb packages
+./packaging/deb/xenial/debuild.sh
+cp ./packaging/deb/xenial/*.deb ${WORKSPACE}/csit
+
+cd ${WORKSPACE}/csit
+# execute csit bootstrap script if it exists
+if [ ! -e bootstrap-hc2vpp-verify.sh ]
+then
+    echo 'ERROR: No bootstrap-hc2vpp-verify.sh found'
+    exit 1
+else
+    # make sure that bootstrap.sh is executable
+    chmod +x bootstrap-hc2vpp-verify.sh
+    # run the script
+    ./bootstrap-hc2vpp-verify-odl.sh ${OS} ${ODL}
+fi
+
+# vim: ts=4 ts=4 sts=4 et :
