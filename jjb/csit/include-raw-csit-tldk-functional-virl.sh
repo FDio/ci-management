df6ee789026a5c6d8976404132fb218bd837e088
Fangyin Hu <fangyinx.hu@intel.com>
2017-07-14 03:56:00 -0700

    Add the TLDK functional test Jenkis job into CSIT.
    Change-Id: I3cd01d3a17025b597fd4ad5863231e2c4597fcf9
    Signed-off-by: Fangyin Hu <fangyinx.hu@intel.com>

---
+++ jjb/csit/include-raw-csit-tldk-functional-virl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -xeu -o pipefail
+
+# Clone tldk and start tests
+git clone https://gerrit.fd.io/r/tldk
+
+# If the git clone fails, complain clearly and exit
+if [ $? != 0 ]; then
+    echo "Failed to run: git clone https://gerrit.fd.io/r/tldk"
+    exit 1
+fi
+
+# execute tldk bootstrap script if it exists
+if [ -e bootstrap-TLDK.sh ]
+then
+    # make sure that bootstrap-TLDK.sh is executable
+    chmod +x bootstrap-TLDK.sh
+    # run the script
+    ./bootstrap-TLDK.sh
+else
+    echo 'ERROR: No bootstrap-TLDK.sh found'
+    exit 1
+fi
+
+# vim: ts=4 ts=4 sts=4 et :
