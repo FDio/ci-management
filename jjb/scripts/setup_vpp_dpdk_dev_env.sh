2b31c8bc9e8337d8b29f0f741360f99766f82fe8
Marco Varlese <marco.varlese@suse.com>
2017-10-13 10:18:25 +0200

    openSUSE support and some rework to identify OS
    Rework to identify OS:
    Same approach followed by Makefiles and building scripts in the VPP
    repository. It is much cleaner and easier to simply use the environment
    variables in /etc/os-release
    
    openSUSE support:
    Some of the code added to the scripts can be removed once a separate
    patch from Vanessa is merged. The purpose here is to save the changes
    made since they will be lost otherwise.
    
    Change-Id: I06a041bbc5f5b7e3fa3510db43acf7d1462ec24c
    Signed-off-by: Marco Varlese <marco.varlese@suse.com>

--- jjb/scripts/setup_vpp_dpdk_dev_env.sh
+++ jjb/scripts/setup_vpp_dpdk_dev_env.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 set -e -o pipefail
 
-# Figure out what system we are running on
-if [ -f /etc/lsb-release ];then
-    . /etc/lsb-release
-elif [ -f /etc/redhat-release ];then
-    sudo yum install -y redhat-lsb
-    DISTRIB_ID=`lsb_release -si`
-    DISTRIB_RELEASE=`lsb_release -sr`
-    DISTRIB_CODENAME=`lsb_release -sc`
-    DISTRIB_DESCRIPTION=`lsb_release -sd`
-fi
-echo DISTRIB_ID: $DISTRIB_ID
-echo DISTRIB_RELEASE: $DISTRIB_RELEASE
-echo DISTRIB_CODENAME: $DISTRIB_CODENAME
-echo DISTRIB_DESCRIPTION: $DISTRIB_DESCRIPTION
+OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
 
 function setup {
     if ! [ -z ${REPO_NAME} ]; then
@@ -22,12 +10,12 @@ function setup {
         REPO_URL="${NEXUSPROXY}/content/repositories/fd.io.${REPO_NAME}"
         echo "REPO_URL: ${REPO_URL}"
         # Setup by installing vpp-dev and vpp-lib
-        if [ $DISTRIB_ID == "Ubuntu" ]; then
+        if [ "$DISTRIB_ID" == "ubuntu" ]; then
             echo "deb ${REPO_URL} ./" | sudo tee /etc/apt/sources.list.d/99fd.io.list
             sudo apt-get update || true
             sudo apt-get -y --force-yes install vpp-dpdk-dev || true
             sudo apt-get -y --force-yes install vpp-dpdk-dkms || true
-        elif [[ $DISTRIB_ID == "CentOS" ]]; then
+        elif [ "$DISTRIB_ID" == "centos" ]; then
             sudo cat << EOF > fdio-master.repo
 [fdio-master]
 name=fd.io master branch latest merge
--- jjb/vpp/include-raw-vpp-checkstyle.sh
+++ jjb/vpp/include-raw-vpp-checkstyle.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# THIS BLOCK CAN BE REMOVED ONCE THE PATCH https://gerrit.fd.io/r/#/c/8280/ is merged
+# BLOCK BEGIN
+OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+
+echo OS_ID: $OS_ID
+echo OS_VERSION_ID: $OS_VERSION_ID
+
+if [ "$OS_ID" == "opensuse" ]; then
+	echo "##############################################################################"
+	echo "# Removing the cloud repo added in some previous steps with the wrong command "
+	sudo zypper rr openSUSE-Leap-Cloud-Tools
+	echo "# Installing clang package"
+	sudo zypper install -y libclang llvm-clang
+	echo " # Installing NASM 2.13"
+	sudo zypper install -y https://download.opensuse.org/tumbleweed/repo/oss/suse/x86_64/nasm-2.13.01-2.1.x86_64.rpm
+	echo "Adding the correct Cloud Repo"
+	sudo zypper --non-interactive --gpg-auto-import-keys --no-gpgcheck ar --refresh -n CloudRepo \
+		http://download.opensuse.org/repositories/Cloud:/Tools/openSUSE_Leap_42.3/Cloud:Tools.repo
+fi
+# BLOCK END
+
 if [ -f build-root/scripts/checkstyle.sh ];then
     build-root/scripts/checkstyle.sh
 else
