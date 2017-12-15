26a34dbcb02f87e3fed941db025b2b722d89bba5
Marco Varlese <marco.varlese@suse.com>
2017-12-15 12:02:30 +0100

    Address 'indent' version disceprancy among distros
    openSUSE runs version 2.2.10 of indent which differs from what CentOS
    and Ubuntu are running. That causes some verification issues since the
    supported 'style' changed in the newer version of indent.
    
    This patch fixes that.
    
    Change-Id: Id81832a6c7cefc53a963a15c3df956c08523df0a
    Signed-off-by: Marco Varlese <marco.varlese@suse.com>

--- jjb/vpp/include-raw-vpp-checkstyle.sh
+++ jjb/vpp/include-raw-vpp-checkstyle.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+
+if [ "$OS_ID" == "opensuse" ];then
+	echo "openSUSE runs indent 2.2.10 which differs from other distros (2.2.11)"
+	exit 0
+fi
+
 if [ -f build-root/scripts/checkstyle.sh ];then
     build-root/scripts/checkstyle.sh
 else
