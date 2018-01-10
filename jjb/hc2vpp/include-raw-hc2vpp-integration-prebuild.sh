f078c7b34e4a2fa60cf03f826a75a0165be10b2a
Marek Gradzki <mgradzki@cisco.com>
2018-01-10 08:24:06 +0100

    Adapt to hc2vpp refactor
    Change-Id: Ie7c3bc5b61e743e389c23284be4aa474415f0032
    Signed-off-by: Marek Gradzki <mgradzki@cisco.com>

--- jjb/hc2vpp/include-raw-hc2vpp-integration-prebuild.sh
+++ jjb/hc2vpp/include-raw-hc2vpp-integration-prebuild.sh
@@ -65,7 +65,7 @@ fi
 rm vpp-api-java*
 
 # install jvpp jars into maven repo, so that maven picks them up when building hc2vpp
-version=`./jvpp-version`
+version=`./jvpp/version`
 
 current_dir=`pwd`
 cd /usr/share/java
