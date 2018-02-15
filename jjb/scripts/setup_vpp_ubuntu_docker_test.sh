4be174ef50e1e4095213771acccce917779ee6ce
Ed Kern <ejk@cisco.com>
2018-02-15 09:01:22 -0700

    VPP: add pkg and pip list for csit builds
    Change-Id: Ib41bdffa79e6ff0551ec8fd4d96cbd2da5c4b052
    Signed-off-by: Ed Kern <ejk@cisco.com>

--- jjb/scripts/setup_vpp_ubuntu_docker_test.sh
+++ jjb/scripts/setup_vpp_ubuntu_docker_test.sh
@@ -9,6 +9,25 @@
 ##############################################################################
 set -e -o pipefail
 
+OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+
 if ! [ -z ${DOCKER_TEST} ] ; then
 		mount -o remount /dev/shm -o size=512M || true
+        echo "/dev/shm remounted"
 fi
+
+if [ "${OS_ID}" == "ubuntu" ]; then
+    dpkg-query -W -f='${binary:Package}\t${Version}\n'
+    pip list
+elif [ "${OS_ID}" == "centos" ]; then
+    yum list installed
+    pip list
+elif [ "${OS_ID}" == "opensuse" ]; then
+    yum list installed
+    pip list
+fi
+if [ "x${IS_CSIT_VPP_JOB}" == "xTrue" ]; then
+	(cd dpdk ; apt-get download vpp-dpdk-dkms > /dev/null 2>&1) || true
+    ls -l dpdk/*.deb || true
+    echo "csit vpp-dpdk-dkms package download"
+fi     
