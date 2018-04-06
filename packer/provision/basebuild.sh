08099216324fac395ada753b54f0f62380d28af0
Marco Varlese <marco.varlese@suse.com>
2018-04-06 10:25:48 +0200

    Indent package for opensuse Leap 42.3
    openSUSE Leap 42.3 has an older version of the indent package compared
    to other distributions in Jenkins. In order to avoid misalignment, let's
    install version 2.2.11 to align with others.
    
    Change-Id: I1473425d2cb3d503bb98e008d8fa896fdd9173eb
    Signed-off-by: Marco Varlese <marco.varlese@suse.com>

--- packer/provision/basebuild.sh
+++ packer/provision/basebuild.sh
@@ -257,6 +257,12 @@ opensuse_systems() {
     # minimum so installing from TW repo
     zypper install -y \
         https://download.opensuse.org/tumbleweed/repo/oss/suse/x86_64/nasm-2.13.01-2.2.x86_64.rpm
+
+    # Leap 42.3 does not have a recent INDENT version
+    # Other distros in Jenkins run 2.2.11 so let's align Leap 42.3 too to avoid
+    # build checks differences and false-positives
+    zypper install -y \
+        https://download.opensuse.org/repositories/devel:/tools/openSUSE_Leap_42.3/x86_64/indent-2.2.11-67.d_t.3.x86_64.rpm
 }
 
 all_systems() {
