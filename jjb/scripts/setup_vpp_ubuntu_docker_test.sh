e039d0d932443955ef8ca89d15e4e951aba08f0c
Ed Kern <ejk@cisco.com>
2018-06-19 14:31:50 -0600

    VPP: list out preinstalled packages
    Few echos and ls to list out preinstalled or cached
    deps on ubuntu images.
    
    Change-Id: I4ea5dfdafec66c29b94ce116f25d0a23c0a6a870
    Signed-off-by: Ed Kern <ejk@cisco.com>

--- jjb/scripts/setup_vpp_ubuntu_docker_test.sh
+++ jjb/scripts/setup_vpp_ubuntu_docker_test.sh
@@ -21,7 +21,19 @@ grep search /etc/resolv.conf  || true
 
 if [ "${OS_ID}" == "ubuntu" ]; then
     dpkg-query -W -f='${binary:Package}\t${Version}\n' || true
+    echo "************************************************************************"
+    echo "pip list:"
     pip list || true
+    echo "************************************************************************"
+    echo "Contents of /var/cache/vpp/python/virtualenv/lib/python2.7/site-packages:"
+    ls -lth /var/cache/vpp/python/virtualenv/lib/python2.7/site-packages || true
+    echo "************************************************************************"
+    echo "Contents of br Downloads:"
+    ls -lth /w/Downloads || true
+    echo "************************************************************************"
+    echo "Contents of /w/dpdk for test folks:"
+    echo "************************************************************************"
+    ls -lth /w/dpdk || true
 elif [ "${OS_ID}" == "centos" ]; then
     yum list installed || true
     pip list || true
