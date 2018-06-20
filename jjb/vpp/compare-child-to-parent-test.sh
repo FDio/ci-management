97900fd68bf544cae2d391ae9fbe6c7dca1139a4
Vratko Polak <vrpolak@cisco.com>
2018-06-20 12:10:12 +0200

    DO_NOT_MERGE: WIP: Perf parent comparison
    Change-Id: I8643b4ba352122c51289db923f0d0ebe597d08e4
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ jjb/vpp/compare-child-to-parent-test.sh
@@ -0,0 +1,13 @@
+cd $WORKSPACE
+
+rm -f csit/results.txt
+grep -o "'Maximum Receive Rate Results .*\]'" csit/output.xml | grep -o '\[.*\]' >> results.txt
+
+echo "PARENT:"
+cat csit/results.txt
+
+echo "NEW:"
+cat csit_/results.txt
+
+echo "FIXME: IMPLEMENT THE REST!"
+exit 1
