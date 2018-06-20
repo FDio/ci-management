739e5f6f85890b0b58c959067c51a8280434937e
Vratko Polak <vrpolak@cisco.com>
2018-06-20 16:24:59 +0200

    DO_NOT_MERGE: CSIT-1134: Perf parent comparison
    Change-Id: I8643b4ba352122c51289db923f0d0ebe597d08e4
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ jjb/vpp/compare-child-to-parent-test.sh
@@ -0,0 +1,15 @@
+cd $WORKSPACE
+
+bash csit/resources/tools/jenkins/patch_vote/parse.sh
+
+echo "PARENT:"
+cat csit/results.txt
+
+echo "NEW:"
+cat csit_/results.txt
+
+virtualenv --system-site-packages "env"
+source "env/bin/activate"
+pip install -r csit/resources/tools/jenkins/patch_vote/requirements.txt
+python csit/resources/tools/jenkins/patch_vote/compare.py
+# The exit code affects the vote result.
