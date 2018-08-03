c3af73757e44fb479eda24d5fe1b0e41178027cf
Vratko Polak <vrpolak@cisco.com>
2018-08-03 15:09:53 +0200

    DO_NOT_MERGE: Download builds for per-patch
    This is to speed-up per-patch test reliability verification.
    
    Change-Id: Iab634692c52ae8f178bf6eaa03c6d877760c4893
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ jjb/vpp/download-builds.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -exu -o pipefail
+
+cd "$WORKSPACE"
+rm -rf build-root build_parent build_new archive
+wget -N --progress=dot:giga "https://jenkins.fd.io/sandbox/job/vpp-csit-verify-hw-perf-master-2n-skx/23/artifact/*zip*/archive.zip"
+unzip archive.zip
+mv archive/build_parent ./
+mv archive/build_new ./
+cp -r build_new build-root
+# Create symlinks so that if job fails on robot test, results can be archived.
+ln -s csit csit_new
