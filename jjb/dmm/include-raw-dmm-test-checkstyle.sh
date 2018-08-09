d695b59cc7017264f1f089a4b5ad0efc207fafde
sharath <sharathkumarboyanapally@gmail.com>
2018-08-09 14:41:56 +0530

    enhance dmm checksytle job
    Change-Id: Ia6307a87cf1d95410d77336283b016f2c9eeef5a
    Signed-off-by: sharath <sharathkumarboyanapally@gmail.com>

--- jjb/dmm/include-raw-dmm-test-checkstyle.sh
+++ jjb/dmm/include-raw-dmm-test-checkstyle.sh
@@ -12,12 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-cd build/
-cmake ..
-make checkstyle
-if [ $? -eq 0 ]; then
-    echo "DMM checkstyle is SUCCESS"
+if grep 'checkstyle' CMakeLists.txt > /dev/null
+then
+    cd build/
+    cmake ..
+    make checkstyle
+    if [ $? -eq 0 ]; then
+        echo "DMM checkstyle is SUCCESS"
+    else
+        echo "DMM checkstyle has FAILED"
+        exit 1
+    fi
 else
-    echo "DMM checkstyle has FAILED"
+    echo "Can't find checkstyle target in CMakeLists.txt- skipping test checkstyle"
     exit 1
 fi
