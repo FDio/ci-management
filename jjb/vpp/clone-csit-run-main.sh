4acceee279f9b1b805485e36b098a854fa029a97
Vratko Polak <vrpolak@cisco.com>
2018-08-13 15:40:41 +0200

    CSIT-1264: Job for per-patch VPP perf testing
    Most logic is in CSIT repository.
    
    + Hardcode topology type of vpp-csit-verify-perf-{stream}- job.
    + Introduce gerrit related parameters to enable Sandbox testing.
    + Add the parameters to vpp-csit and csit-vpp perf verify jobs.
    
    Change-Id: I8643b4ba352122c51289db923f0d0ebe597d08e4
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

---
+++ jjb/vpp/clone-csit-run-main.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
+
+# Clone CSIT git repository and proceed with entry script located there.
+#
+# Variables read:
+# - WORKSPACE - Jenkins workspace to create csit subdirectory in.
+# Directories updated:
+# - ${WORKSPACE}/csit - Created, holding a checked out CSIT repository.
+# - Multiple other side effects by entry script(s), see CSIT repository.
+
+cd "${WORKSPACE}"
+git clone https://gerrit.fd.io/r/csit --depth 1 --no-single-branch
+csit_entry_dir="${WORKSPACE}/csit/resources/libraries/bash/entry"
+source "${csit_entry_dir}/with_oper_for_vpp.sh" "per_patch_perf.sh"
