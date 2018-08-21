0562585dadd9e47f6869d6015d864d9964983e62
Ed Kern <ejk@cisco.com>
2018-08-21 16:09:43 -0600

    VPP: expand arm builds to 2G shm
    At request of test im expanding default shm
    on arm builds to 2G from 512M
    
    Change-Id: Ib0283fba5228083d578c66f9b382e5d4c06e72f3
    Signed-off-by: Ed Kern <ejk@cisco.com>

---
+++ jjb/scripts/setup_vpp_ubuntu_arm_test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+##############################################################################
+# Copyright (c) 2018 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+set -e -o pipefail
+
+OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+
+if ! [ -z ${DOCKER_TEST} ] ; then
+		mount -o remount /dev/shm -o size=2G || true
+        echo "/dev/shm remounted"
+fi
+
+##container server node detection
+grep search /etc/resolv.conf  || true
+
+if [ "${OS_ID}" == "ubuntu" ]; then
+    dpkg-query -W -f='${binary:Package}\t${Version}\n' || true
+    echo "************************************************************************"
+    echo "pip list:"
+    pip list || true
+    echo "************************************************************************"
+    echo "Contents of /var/cache/vpp/python/virtualenv/lib/python2.7/site-packages:"
+    ls -lth /var/cache/vpp/python/virtualenv/lib/python2.7/site-packages || true
+    echo "************************************************************************"
+    echo "Contents of br Downloads:"
+    ls -lth /w/Downloads || true
+    echo "************************************************************************"
+    echo "Contents of /w/dpdk for test folks:"
+    echo "************************************************************************"
+    ls -lth /w/dpdk || true
+elif [ "${OS_ID}" == "centos" ]; then
+    yum list installed || true
+    pip list || true
+elif [ "${OS_ID}" == "opensuse" ]; then
+    yum list installed || true
+    pip list || true
+fi
+
+##This will remove any previously installed dpdk for old branch builds
+
+if [ "${GERRIT_BRANCH}" != "master" ]; then
+    if [ "${OS_ID}" == "ubuntu" ]; then
+        apt-get -y remove vpp-dpdk-dev || true
+        apt-get -y remove vpp-dpdk-dkms || true
+    elif [ "${OS_ID}" == "centos" ]; then
+        yum -y erase vpp-dpdk-devel || true
+        yum clean all || true
+    elif [ "${OS_ID}" == "opensuse" ]; then
+        yum -y erase vpp-dpdk-devel || true
+    fi
+fi
