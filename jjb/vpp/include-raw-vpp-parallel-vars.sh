4fa505fe12c175adcd6faa9885588597656bcc39
juraj.linkes <juraj.linkes@pantheon.tech>
2018-08-30 12:11:31 +0200

    Flags for parallel test run and arm verify update
    Added a script where we can look at the label (flavor) of the jenkins
    job and set the number of cores used in build/test for that label.
    
    When setting the number of cores, the label along with the corresponding
    hosts must ensure (by properly configuring them in nomad) that the
    resources used by jobs on the host won't exceed host's resources.
    
    Change-Id: I138c8ad797df84ebf9a926074a1a7cb5037bd681
    Signed-off-by: juraj.linkes <juraj.linkes@pantheon.tech>

---
+++ jjb/vpp/include-raw-vpp-parallel-vars.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+CORES=4
+if [[ -z ${NODE_NAME+x} || -z ${NODE_LABELS+x} ]]
+then
+    echo "NODE_NAME or NODE_LABELS is not set, using defaults for parallel build/test"
+else
+    for NODE_LABEL in $NODE_LABELS
+    do
+        # NODE_LABELS is jenkins-SOMEHEX and the label we're looking for
+        # NODE_NAME is jenkins-SOMEHEX
+        if [[ $NODE_NAME != $NODE_LABEL ]]
+        then
+            # Found label such as ubuntu1604arm-us
+            break
+        fi
+    done
+
+    if [[ $NODE_LABEL == 'ubuntu1604arm-us' ]]
+    then
+        CORES=16
+    fi
+fi
+
+echo "Using MAKE_PARALLEL_FLAGS='-j $CORES' TEST_JOBS=$CORES for parallel build/test"
+
+export MAKE_PARALLEL_FLAGS="-j $CORES"
+TEST_JOBS=$CORES
