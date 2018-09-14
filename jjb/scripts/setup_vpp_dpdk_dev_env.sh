a39c82a9f11cfbb848fd33dbdc135fbf95571a59
Ed Warnicke <hagbard@gmail.com>
2018-09-14 18:26:47 -0500

    Also remove spurious apt repos...
    Change-Id: I9b47c377c793e64ca1ac69c945f7399486083f21
    Signed-off-by: Ed Warnicke <hagbard@gmail.com>

--- jjb/scripts/setup_vpp_dpdk_dev_env.sh
+++ jjb/scripts/setup_vpp_dpdk_dev_env.sh
@@ -7,20 +7,27 @@ OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\
 function setup {
     if ! [ -z ${REPO_NAME} ]; then
         echo "INSTALLING VPP-DPKG-DEV from apt/yum repo"
-        REPO_URL="${NEXUSPROXY}/content/repositories/fd.io.${REPO_NAME}"
+        REPO_URL="https://packagecloud.io/fdio/${STREAM}"
         echo "REPO_URL: ${REPO_URL}"
         # Setup by installing vpp-dev and vpp-lib
         if [ "$OS_ID" == "ubuntu" ]; then
+            if [ -f /etc/apt/sources.list.d/99fd.io.list ];then
+                echo "Deleting: /etc/apt/sources.list.d/99fd.io.list"
+                rm /etc/apt/sources.list.d/99fd.io.list
+            fi
             curl -s https://packagecloud.io/install/repositories/fdio/${STREAM}/script.deb.sh | sudo bash
             sudo apt-get -y --force-yes install vpp-dpdk-dev || true
             sudo apt-get -y --force-yes install vpp-dpdk-dkms || true
         elif [ "$OS_ID" == "centos" ]; then
             if [ -f /etc/yum.repos.d/fdio-master.repo ]; then
+                echo "Deleting: /etc/yum.repos.d/fdio-master.repo"
                 rm /etc/yum.repos.d/fdio-master.repo
             fi
             curl -s https://packagecloud.io/install/repositories/fdio/${STREAM}/script.rpm.sh | sudo bash
             sudo yum -y install vpp-dpdk-devel || true
         elif [ "$OS_ID" == "opensuse" ]; then
+            REPO_URL="${NEXUSPROXY}/content/repositories/fd.io.${REPO_NAME}"
+            echo "REPO_URL: ${REPO_URL}"
             sudo cat << EOF > fdio-master.repo
 [fdio-master]
 name=fd.io master branch latest merge
