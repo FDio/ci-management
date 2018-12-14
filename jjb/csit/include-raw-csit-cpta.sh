2329d28cb680b4259ca5d4e514a3c06f2d028bf0
Vratko Polak <vrpolak@cisco.com>
2018-12-14 14:59:02 +0100

    Support secondary dir for trending job
    https://gerrit.fd.io/r/16413 has introduced
    secondary trending webpage tree located outside presentation/.
    
    This change makes the job upload also data from presentation_new/
    but only if such data exists.
    
    Also, put variables consistently to double-quotes.
    
    Change-Id: I3c31245be2280927abc57bd919589bd58572555c
    Signed-off-by: Vratko Polak <vrpolak@cisco.com>

--- jjb/csit/include-raw-csit-cpta.sh
+++ jjb/csit/include-raw-csit-cpta.sh
@@ -2,23 +2,29 @@
 
 set -xe -o pipefail
 
-[ "$DOCS_REPO_URL" ] || DOCS_REPO_URL="https://nexus.fd.io/content/sites/site"
-[ "$PROJECT_PATH" ] || PROJECT_PATH=io/fd/csit
-[ "$DOC_DIR" ] || DOC_DIR=resources/tools/presentation
-[ "$BUILD_DIR" ] || BUILD_DIR=${DOC_DIR}/_build
-[ "$SITE_DIR" ] || SITE_DIR=build-root/docs/deploy-site
-[ "$RESOURCES_DIR" ] || RESOURCES_DIR=${SITE_DIR}/src/site/resources/trending
-[ "$MVN" ] || MVN="/opt/apache/maven/bin/mvn"
+[ "${DOCS_REPO_URL}" ] || DOCS_REPO_URL="https://nexus.fd.io/content/sites/site"
+[ "${PROJECT_PATH}" ] || PROJECT_PATH="io/fd/csit"
+[ "${DOC_DIR}" ] || DOC_DIR="resources/tools/presentation"
+[ "${BUILD_DIR}" ] || BUILD_DIR="${DOC_DIR}/_build"
+[ "${SECONDARY_BUILD_DIR}" ] || SECONDARY_BUILD_DIR="${DOC_DIR}_new/_build"
+[ "${SITE_DIR}" ] || SITE_DIR="build-root/docs/deploy-site"
+[ "${RESOURCES_DIR}" ] || RESOURCES_DIR="${SITE_DIR}/src/site/resources/trending"
+[ "${SECONDARY_RESOURCES_DIR}" ] || SECONDARY_RESOURCES_DIR="${RESOURCES_DIR}/new"
+[ "${MVN}" ] || MVN="/opt/apache/maven/bin/mvn"
 
-cd ${DOC_DIR}
+cd "${DOC_DIR}"
 chmod +x ./run_cpta.sh
 STATUS=$(./run_cpta.sh | tail -1)
 
-cd ${WORKSPACE}
+cd "${WORKSPACE}"
 
-mkdir -p ${RESOURCES_DIR}
-mv -f ${BUILD_DIR}/* ${RESOURCES_DIR}
-cd ${SITE_DIR}
+mkdir -p "${RESOURCES_DIR}"
+mv -f ${BUILD_DIR}/* "${RESOURCES_DIR}"
+if [ -d "${SECONDARY_BUILD_DIR}" ]; then
+    mkdir -p "${SECONDARY_RESOURCES_DIR}"
+    mv -f "${SECONDARY_BUILD_DIR}"/* "${SECONDARY_RESOURCES_DIR}"
+fi
+cd "${SITE_DIR}"
 
 cat > pom.xml << EOF
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
@@ -52,7 +58,7 @@ ${MVN} site:site site:deploy -gs "${GLOBAL_SETTINGS_FILE}" -s "${SETTINGS_FILE}"
 
 cd -
 
-if [ ${STATUS} == "PASS" ]; then
+if [ "${STATUS}" == "PASS" ]; then
     exit 0
 else
     exit 1
