ad682cc1bcc04b8ea27f1e535c45240702769a7c
Michal Cmarada <mcmarada@cisco.com>
2019-02-11 13:47:44 +0100

    JVPP-22: introduce jvpp-verify job
    Change-Id: Ie762e087046224b458eda9184b741ce2707edccb
    Signed-off-by: Michal Cmarada <mcmarada@cisco.com>

---
+++ jjb/jvpp/include-raw-jvpp-build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# basic build script example
+set -xe -o pipefail
+
+OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+
+echo OS_ID: ${OS_ID}
+echo OS_VERSION_ID: ${OS_VERSION_ID}
+
+# do nothing but print the current slave hostname
+hostname
+export CCACHE_DIR=/tmp/ccache
+if [[ -d ${CCACHE_DIR} ]];then
+    echo ${CCACHE_DIR} exists
+    du -sk ${CCACHE_DIR}
+else
+    echo ${CCACHE_DIR} does not exist.  This must be a new slave.
+fi
+
+echo "cat /etc/bootstrap.sha"
+if [[ -f /etc/bootstrap.sha ]];then
+    cat /etc/bootstrap.sha
+else
+    echo "Cannot find cat /etc/bootstrap.sha"
+fi
+
+echo "cat /etc/bootstrap-functions.sha"
+if [[ -f /etc/bootstrap-functions.sha ]];then
+    cat /etc/bootstrap-functions.sha
+else
+    echo "Cannot find cat /etc/bootstrap-functions.sha"
+fi
+
+echo "sha1sum of this script: ${0}"
+sha1sum $0
+
+echo "CC=${CC}"
+
+echo "Building using \"make package\""
+# ensure that we build from scratch
+./clean.sh
+[[ "x${DRYRUN}" == "xTrue" ]] || cd Requirements && make UNATTENDED=yes install-dep && cd ..
+[[ "x${DRYRUN}" == "xTrue" ]] || pwd
+if [[ "${OS_ID}" == "centos" ]]; then
+    [[ "x${DRYRUN}" == "xTrue" ]] || cmake3 .
+else
+    [[ "x${DRYRUN}" == "xTrue" ]] || cmake .
+fi
+
+[[ "x${DRYRUN}" == "xTrue" ]] || make package
+# This will build deb or rpm JVPP package based on OS. Built packages are located in build-root/packages/
+
+echo "*******************************************************************"
+echo "* JVPP BUILD SUCCESSFULLY COMPLETED"
+echo "*******************************************************************"
---
+++ jjb/scripts/setup_jvpp_dev_env.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e -o pipefail
+
+# Figure out what system we are running on
+if [[ -f /etc/lsb-release ]];then
+    . /etc/lsb-release
+elif [[ -f /etc/redhat-release ]];then
+    sudo yum install -y redhat-lsb
+    DISTRIB_ID=`lsb_release -si`
+    DISTRIB_RELEASE=`lsb_release -sr`
+    DISTRIB_CODENAME=`lsb_release -sc`
+    DISTRIB_DESCRIPTION=`lsb_release -sd`
+fi
+echo DISTRIB_ID: $DISTRIB_ID
+echo DISTRIB_RELEASE: $DISTRIB_RELEASE
+echo DISTRIB_CODENAME: $DISTRIB_CODENAME
+echo DISTRIB_DESCRIPTION: $DISTRIB_DESCRIPTION
+
+function setup {
+    if ! [[ -z ${REPO_NAME} ]]; then
+        echo "INSTALLING VPP-DPKG-DEV from apt/yum repo"
+        REPO_URL="https://packagecloud.io/fdio/${STREAM}"
+        echo "REPO_URL: ${REPO_URL}"
+        # Setup by installing vpp-dev and vpp-lib
+        if [[ "$DISTRIB_ID" == "Ubuntu" ]]; then
+            if [[ -f /etc/apt/sources.list.d/99fd.io.list ]];then
+                echo "Deleting: /etc/apt/sources.list.d/99fd.io.list"
+                sudo rm /etc/apt/sources.list.d/99fd.io.list
+            fi
+            curl -s https://packagecloud.io/install/repositories/fdio/${STREAM}/script.deb.sh | sudo bash
+            sudo apt-get -y --force-yes install libvppinfra libvppinfra-dev vpp vpp-dev vpp-plugin-core || true
+        elif [[ "$DISTRIB_ID" == "CentOS" ]]; then
+            if [[ -f /etc/yum.repos.d/fdio-master.repo ]]; then
+                echo "Deleting: /etc/yum.repos.d/fdio-master.repo"
+                sudo rm /etc/yum.repos.d/fdio-master.repo
+            fi
+            curl -s https://packagecloud.io/install/repositories/fdio/${STREAM}/script.rpm.sh | sudo bash
+            sudo yum -y install vpp-devel vpp-lib vpp-plugins || true
+        fi
+    fi
+}
+
+setup
