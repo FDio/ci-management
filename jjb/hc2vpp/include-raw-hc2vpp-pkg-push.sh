36c66743acc244cf23f65194d34a96519cf6732c
Michal Cmarada <mcmarada@cisco.com>
2019-02-11 09:38:22 +0100

    introduce jvpp-verify job
    Change-Id: Ie762e087046224b458eda9184b741ce2707edccb
    Signed-off-by: Michal Cmarada <mcmarada@cisco.com>

--- jjb/hc2vpp/include-raw-hc2vpp-pkg-push.sh
+++ jjb/hc2vpp/include-raw-hc2vpp-pkg-push.sh
@@ -25,7 +25,7 @@ if [ "${STREAM}" == "master" ]; then
 
         # Write a file that will echo VPP dependencies
         echo -n 'echo' > vpp_dependencies
-        echo " \"vpp (= ${VERSION}), vpp-plugins (= ${VERSION})\"" >> vpp_dependencies
+        echo " \"vpp (= ${VERSION}), vpp-plugin-core (= ${VERSION})\"" >> vpp_dependencies
         chmod +x vpp_dependencies
 
         # Overwrite default dependencies file
