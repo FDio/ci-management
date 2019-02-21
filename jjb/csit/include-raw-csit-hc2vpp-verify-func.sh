f71eec1f96d8aab5da15ad079bea80c954b39f95
Michal Cmarada <mcmarada@cisco.com>
2019-02-21 13:29:02 +0100

    HC2VPP-372: Update hc2vpp integration and hc2vpp/csit jobs
     - add support for ubuntu bionic
     - update jobs to support package cloud download script
       (csit patch: https://gerrit.fd.io/r/#/c/17779/)
     - cleanup
    
    Change-Id: I60d8af98c98612ff82ca782274d317a1726cfc7e
    Signed-off-by: Michal Cmarada <mcmarada@cisco.com>

--- jjb/csit/include-raw-csit-hc2vpp-verify-func.sh
+++ jjb/csit/include-raw-csit-hc2vpp-verify-func.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # execute csit bootstrap script if it exists
-if [ ! -e bootstrap-hc2vpp-integration.sh ]
+if [[ ! -e bootstrap-hc2vpp-integration.sh ]]
 then
     echo 'ERROR: No bootstrap-hc2vpp-integration.sh found'
     exit 1
@@ -9,11 +9,7 @@ else
     # make sure that bootstrap.sh is executable
     chmod +x bootstrap-hc2vpp-integration.sh
     # run the script
-    if [ ${STREAM} == 'master' ]; then
-        ./bootstrap-hc2vpp-integration.sh ${STREAM} ${OS}
-    else
-        ./bootstrap-hc2vpp-integration.sh 'stable.'${STREAM} ${OS}
-    fi
+    ./bootstrap-hc2vpp-integration.sh ${STREAM} ${OS}
 fi
 
 # vim: ts=4 ts=4 sts=4 et :
