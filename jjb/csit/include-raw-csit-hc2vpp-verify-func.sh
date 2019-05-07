e6b4d7dc211c154582a2aa04d6610258d0c9b7e3
Michal Cmarada <mcmarada@cisco.com>
2019-05-07 11:39:21 +0200

    add param for csit-hc2vpp-verify for release testing
    To be able to test released packages we need to add
    new parameter to csit-hc2vpp-verify-func job.
    
    Usage: In commit message for release branch add "honeycomb" parameter
    with desired version after trigger keyword.
    Example: verify-hc2vpp-func honeycomb=19.04-release
    
    To test regular RC versions use trigger keyword
    without "honeycomb" param.
    Example: verify-hc2vpp-func
    
    Change-Id: I67c9a856e031bb2cec6b18312d86c2fcd342d2ee
    Signed-off-by: Michal Cmarada <mcmarada@cisco.com>

--- jjb/csit/include-raw-csit-hc2vpp-verify-func.sh
+++ jjb/csit/include-raw-csit-hc2vpp-verify-func.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# Parse optional arguments from gerrit comment trigger
+for i in ${GERRIT_EVENT_COMMENT_TEXT}; do
+    case ${i} in
+        *honeycomb=*)
+            hc_version=`echo "${i}" | cut -d = -f2-`
+        ;;
+        *)
+        ;;
+    esac
+done
+
+# If HC variable is set, check honeycomb version.
+if [[ -n "${hc_version}" ]]; then
+    if [[ "${hc_version}" == *"-release" ]]; then
+        # we are going to test release build. All release
+        # packages should be already present in release repo
+        STREAM="release"
+        echo "STREAM set to: ${STREAM}"
+    fi
+fi
+
 # execute csit bootstrap script if it exists
 if [[ ! -e bootstrap-hc2vpp-integration.sh ]]
 then
