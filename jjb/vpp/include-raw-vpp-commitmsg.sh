95ffade249a3ab996cf6840c5db3c1e053eba7a0
Ed Kern <ejk@cisco.com>
2019-06-12 16:21:54 -0600

    VPP: added commit msg checker
    Adding commit msg checker to checkstyle job
    
    Change-Id: I5c151524f8d3f96e1392f1eede38eb825536dfc8
    Signed-off-by: Ed Kern <ejk@cisco.com>

---
+++ jjb/vpp/include-raw-vpp-commitmsg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f extras/scripts/check_commit_msg.sh ];then
+	echo "Running extras/scripts/check_commit_msg.sh"
+    extras/scripts/check_commit_msg.sh
+else
+    echo "Cannot find cat extras/scripts/check_commit_msg.sh - skipping commit message check"
+fi
