e1296a8c7acca82a4f2eafe5e4c2639d9bfce6c8
Peter Mikus <pmikus@cisco.com>
2019-08-08 09:27:03 +0000

    CSIT: Use entry scripts by default in perf
    Change-Id: I5b41dd9f2b1952be90bac41ecbb25c910e1deb42
    Signed-off-by: Peter Mikus <pmikus@cisco.com>

--- jjb/csit/include-raw-csit-perf-verify.sh
+++ jjb/csit/include-raw-csit-perf-verify.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -xeu -o pipefail
+# Copyright (c) 2019 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -exuo pipefail
 
 if [[ ${GERRIT_EVENT_TYPE} == 'comment-added' ]]; then
     TRIGGER=`echo ${GERRIT_EVENT_COMMENT_TEXT} \
@@ -8,23 +21,8 @@ if [[ ${GERRIT_EVENT_TYPE} == 'comment-added' ]]; then
 else
     TRIGGER=''
 fi
-
-# grep to see where minion is running
-grep search /etc/resolv.conf  || true
-
 # Export test tags as string.
 export TEST_TAG_STRING=${TRIGGER#$"perftest"}
 
-# execute csit bootstrap script if it exists
-if [ ! -e bootstrap-verify-perf.sh ]
-then
-    echo 'ERROR: No bootstrap-verify-perf.sh found'
-    exit 1
-fi
-
-# make sure that bootstrap-verify-perf.sh is executable
-chmod +x bootstrap-verify-perf.sh
-# run the script
-./bootstrap-verify-perf.sh
-
-# vim: ts=4 ts=4 sts=4 et :
+csit_entry_dir="${WORKSPACE}/resources/libraries/bash/entry"
+source "${csit_entry_dir}/bootstrap_verify_perf.sh"
