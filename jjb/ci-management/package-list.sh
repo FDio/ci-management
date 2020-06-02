9717e58a9f6dc38bd6633d55cb1dcadb4f99c148
Mauro Sardara <msardara@cisco.com>
2020-06-02 19:16:29 +0200

    Test commands for release repo cleanup.
    Signed-off-by: Mauro Sardara <msardara@cisco.com>
    Change-Id: I71bee2717bbcc3021fd85020f2059839ea5a930c
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ jjb/ci-management/package-list.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+export PACKAGE_LIST_COMMON="libhicnctrl \
+libhicnctrl-memif \
+hicn-collectd-plugins \
+hicn-apps \
+hicn-light \
+hicn-apps-memif \
+libhicntransport-memif \
+libhicn \
+libhicntransport \
+hicn-plugin \
+facemgr \
+hicn-utils-memif \
+hicn-utils \
+hicn-sysrepo-plugin \
+hicn-extra-plugin \
+libparc \
+libparc-doc \
+longbow \
+longbow-doc"
+
+export PACKAGE_LIST_UBUNTU="libhicnctrl-dev \
+libhicn-ctrl-dev \
+libhicnctrl-memif-dev \
+libhicntransport-memif-dev \
+libhicn-dev \
+libhicntransport-dev \
+hicn-plugin-dev \
+libdash \
+libdash-dev \
+libdash-doc \
+libparc-dev \
+libmemif-dev \
+longbow-dev"
+
+export PACKAGE_LIST_CENTOS="libhicnctrl-devel \
+libhicnctrl-memif-devel \
+libhicntransport-memif-devel \
+libhicn-devel \
+libhicntransport-devel \
+hicn-plugin-devel \
+libparc-devel \
+libmemif-devel \
+longbow-devel"
+
+export VERSION_WHITELIST="19.01-227 \
+19.08-289 \
+20.01-73 \
+20.01-114"
