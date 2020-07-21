0cdbc30de66f6b471676a5060d07de38f45ad1b5
Dave Wallace <dwallacelf@gmail.com>
2020-07-21 15:32:12 -0700

    Automate generation of docker builder images.
    - Add bash scripts to generate builder image Dockerfile
      that invokes the scripts to iterate over project
      branches installing OS packages and python
      modules.
    
    Change-Id: I9b3b355b593e2f982f287023c662cc4a4f35a734
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

---
+++ docker/scripts/lib_csit.sh
@@ -0,0 +1,141 @@
+# lib_csit.sh - Docker build script CSIT library.
+#               For import only.
+
+# Don't import more than once.
+if [ -n "$(alias lib_csit_imported 2> /dev/null)" ] ; then
+  return 0
+fi
+alias lib_csit_imported=true
+
+export CIMAN_DOCKER_SCRIPTS="${CIMAN_DOCKER_SCRIPTS:-$(dirname $BASH_SOURCE)}"
+. $CIMAN_DOCKER_SCRIPTS/lib_common.sh
+. $CIMAN_DOCKER_SCRIPTS/lib_apt.sh
+. $CIMAN_DOCKER_SCRIPTS/lib_yum.sh
+
+csit_checkout_branch_for_vpp() {
+  local vpp_branch=$1
+  local csit_dir="$DOCKER_CSIT_DIR"
+  local csit_bash_function_dir="$csit_dir/resources/libraries/bash/function"
+
+  # import checkout_csit_for_vpp() if not defined
+  set +e && [ -z "$(declare -f checkout_csit_for_vpp)" ] \
+    && source $csit_bash_function_dir/branch.sh
+  CSIT_DIR=$(csit_dir) checkout_csit_for_vpp $vpp_branch
+
+  csit_branch="$(git branch | grep -e '^*' | mawk '{print $2}')"
+}
+
+csit_install_packages() {
+  local branch="$1"
+  local branchname="$(echo $branch | sed -e 's,/,_,')"
+  local csit_dir="$DOCKER_CSIT_DIR"
+  local csit_ansible_dir="$csit_dir/resources/tools/testbed-setup/ansible"
+  local bld_log="$DOCKER_BUILD_LOG_DIR/$FDIOTOOLS_IMAGENAME"
+  bld_log="${bld_log}-$branchname-csit_install_packages-bld.log"
+
+  git clean -qfdx
+
+  # Install PyYAML required by csit_get_packages_from_ansible_config.py
+  #
+  # Note: Conditional install due to Bug 1696324 -
+  #       Update to python3.6 breaks PyYAML dependencies
+  # Status:       CLOSED CANTFIX
+  # https://bugzilla.redhat.com/show_bug.cgi?id=1696324
+  if [ "${OS_ID}-${OS_ID_VERSION}" = "centos-8" ] ; then
+      yum install -y python3-pyyaml
+  else
+      python3 -m pip install pyyaml
+  fi
+
+  # Not in double quotes to let bash remove newline characters
+  local yaml_files=$(grep -r packages_by $csit_ansible_dir | cut -d: -f1 | sort -u | grep -ve calibration -e kernel -e mellanox -e nomad)
+  packages=$(csit_get_packages_from_ansible_config.py --$OS_ID --$OS_ARCH $yaml_files)
+  if [ -n "$packages" ] ; then
+    case $OS_ID in
+      ubuntu*)
+        apt_install_packages $packages
+        ;;
+      debian*)
+        apt_install_packages $packages
+        ;;
+      centos*)
+        yum_install_packages $packages
+        ;;
+      *)
+        echo "Unsupported OS ($OS_ID): CSIT packages NOT INSTALLED!"
+        ;;
+    esac
+  fi
+}
+
+csit_pip_cache() {
+  local branch="$1"
+  local VENV_OPTS=""
+  # ensure PS1 is defined (used by virtualenv activate script)
+  PS1=${PS1:-"#"}
+  local csit_dir="$DOCKER_CSIT_DIR"
+  local csit_bash_function_dir="$csit_dir/resources/libraries/bash/function"
+
+  if [ -f "$csit_dir/VPP_REPO_URL" ] \
+         && [ -f "$csit_dir/requirements.txt" ]; then
+
+    local branchname="$(echo $branch | sed -e 's,/,_,')"
+    local bld_log="$DOCKER_BUILD_LOG_DIR"
+    bld_log="${bld_log}/$FDIOTOOLS_IMAGENAME-$branchname-csit_pip_cache-bld.log"
+
+    export PYTHONPATH=$csit_dir
+    git clean -qfdx
+
+    description="Install CSIT python packages from $branch branch"
+    echo_log "    Starting  $description..."
+    [ -n "$(declare -f deactivate)" ] && deactivate
+    local PIP=pip
+    local setup_framework=$csit_dir/resources/libraries/python/SetupFramework.py
+    if [ -n "$(grep pip3 $setup_framework)" ]; then
+      PIP=pip3
+      VENV_OPTS="-p python3"
+    fi
+    rm -rf $PYTHONPATH/env
+    virtualenv $VENV_OPTS $PYTHONPATH/env
+    . $PYTHONPATH/env/bin/activate
+    if [ "$OS_ARCH" = "aarch64" ] ; then
+      local numpy_ver="$(grep numpy $PYTHONPATH/requirements.txt)"
+      [ -n "$numpy_ver" ] && $PIP install --upgrade $numpy_ver 2>&1 \
+      | tee -a $bld_log
+    fi
+    $PIP install --upgrade -r $PYTHONPATH/requirements.txt 2>&1 \
+      | tee -a $bld_log
+    $PIP install --upgrade -r $PYTHONPATH/tox-requirements.txt 2>&1 \
+      | tee -a $bld_log
+    if [ "$OS_ARCH" = "x86_64" ] ; then
+      local PRESENTATION_DIR="$PYTHONPATH/resources/tools/presentation"
+      if [ -n "$(grep -r python3 $PRESENTATION_DIR)" ] && [ "$PIP" != "pip3" ] ; then
+        PIP=pip3
+        deactivate
+        virtualenv -p python3 $PYTHONPATH/env
+        . $PYTHONPATH/env/bin/activate
+      fi
+      $PIP install --upgrade -r $PRESENTATION_DIR/requirements.txt 2>&1 \
+        | tee -a $bld_log
+    fi
+
+    # Virtualenv version is pinned in common.sh in newer csit branches
+    # xargs removes leading/trailing spaces
+    install_virtualenv="$(grep 'virtualenv' $csit_bash_function_dir/common.sh | grep pip | grep install | cut -d'|' -f1 | xargs)"
+    $install_virtualenv
+
+    deactivate
+    rm -rf $PYTHONPATH/env
+    git checkout -q -- .
+    echo_log "    Completed $description!"
+  else
+    echo_log "ERROR: Missing or invalid CSIT_DIR: '$csit_dir'!"
+    return 1
+  fi
+}
+
+if [ ! -d "$DOCKER_CSIT_DIR" ] ; then
+  echo_log "Cloning CSIT into $DOCKER_CSIT_DIR..."
+  git clone -q https://gerrit.fd.io/r/csit $DOCKER_CSIT_DIR
+fi
+clean_git_repo $DOCKER_CSIT_DIR
--- jjb/scripts/setup_vpp_dpdk_dev_env.sh
+++ jjb/scripts/setup_vpp_dpdk_dev_env.sh
@@ -6,35 +6,37 @@ OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\
 
 function setup {
     if ! [ -z ${REPO_NAME} ]; then
-        echo "INSTALLING VPP-DPKG-DEV from apt/yum repo"
+        echo "Installing vpp-ext-deps..."
         REPO_URL="https://packagecloud.io/fdio/${STREAM}"
         echo "REPO_URL: ${REPO_URL}"
         # Setup by installing vpp-dev and vpp-lib
         if [ "$OS_ID" == "ubuntu" ]; then
-        	if ! [ "${STREAM}" == "master" ]; then
-        		echo "tree not master deleting packagecloud repo pointer"
-        		sudo rm  -f /etc/apt/sources.list.d/fdio_master.list
-        		curl -s https://packagecloud.io/install/repositories/fdio/${STREAM}/script.deb.sh | sudo bash
-        	fi
-            if [ -f /etc/apt/sources.list.d/99fd.io.list ];then
-                echo "Deleting: /etc/apt/sources.list.d/99fd.io.list"
-                sudo rm /etc/apt/sources.list.d/99fd.io.list
+            if [ "${STREAM}" != "master" ]; then
+                echo "{stream} is not master: deleting packagecloud apt sources list"
+                sudo rm  -f /etc/apt/sources.list.d/fdio_master.list
+                curl -s https://packagecloud.io/install/repositories/fdio/${STREAM}/script.deb.sh | sudo bash
             fi
             sudo apt-get update -qq || true
-            sudo apt-get -y --force-yes install vpp-dpdk-dev || true
-            sudo apt-get -y --force-yes install vpp-dpdk-dkms || true
-            sudo apt-get -y --force-yes install vpp-ext-deps || true
+            curr_vpp_ext_deps="/root/Downloads/$(basename $(apt-cache show vpp-ext-deps | grep Filename | head -1 | cut -d' ' -f2))"
+            if [ -f "$curr_vpp_ext_deps" ] ; then
+                echo "Installing cached vpp-ext-deps pkg: $curr_vpp_ext_deps"
+                sudo dpkg -i $curr_vpp_ext_deps
+            else
+                echo "Installing vpp-ext-deps from packagecloud.io"
+                local force_opts="--allow-downgrades --allow-remove-essential"
+                force_opts="$force_opts --allow-change-held-packages"
+                sudo apt-get -y $force_opts install vpp-ext-deps || true
+            fi
         elif [ "$OS_ID" == "centos" ]; then
             if [ -f /etc/yum.repos.d/fdio-master.repo ]; then
                 echo "Deleting: /etc/yum.repos.d/fdio-master.repo"
                 sudo rm /etc/yum.repos.d/fdio-master.repo
             fi
-            if ! [ "${STREAM}" == "master" ]; then
+            if [ "${STREAM}" != "master" ]; then
                 echo "tree not master deleting packagecloud repo pointer"
                 sudo rm  -f /etc/yum.repos.d/fdio_master.repo
                 curl -s https://packagecloud.io/install/repositories/fdio/${STREAM}/script.rpm.sh | sudo bash
             fi
-            sudo yum -y install vpp-dpdk-devel || true
             sudo yum -y install vpp-ext-deps || true
         elif [ "$OS_ID" == "opensuse" ]; then
             REPO_URL="${NEXUSPROXY}/content/repositories/fd.io.${REPO_NAME}"
@@ -60,7 +62,6 @@ enabled=1
 gpgcheck=0
 EOF
             sudo mv fdio-master.repo /etc/yum/repos.d/fdio-master.repo
-            sudo yum -y install vpp-dpdk-devel || true
             sudo yum -y install vpp-ext-deps || true
         fi
     fi
