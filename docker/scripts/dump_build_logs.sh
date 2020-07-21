5eeb746cffd2cc4d40c4dae56368ce36c373b570
Dave Wallace <dwallacelf@gmail.com>
2020-07-21 15:32:12 -0700

    Automate generation of docker builder images.
    - Add bash scripts to generate builder image Dockerfile
      that invokes the scripts to iterate over project
      branches installing OS packages and python
      modules.
    
    Change-Id: I9b3b355b593e2f982f287023c662cc4a4f35a734
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

---
+++ docker/scripts/dump_build_logs.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+
+# Copyright (c) 2020 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euxo pipefail
+
+export CIMAN_DOCKER_SCRIPTS=${CIMAN_DOCKER_SCRIPTS:-"$(dirname $BASH_SOURCE)"}
+export CIMAN_ROOT=${CIMAN_ROOT:-"$(dirname $(dirname $CIMAN_DOCKER_SCRIPTS))"}
+. $CIMAN_DOCKER_SCRIPTS/lib_common.sh
+
+must_be_run_as_root
+must_be_run_in_docker_build
+
+dump_build_logs() {
+    local set_opts=$-
+    grep -q e <<< $set_opts && set +e # disable exit on errors
+
+    # Find errors
+    local found="$(grep -nisH error $DOCKER_BUILD_LOG_DIR/*-bld.log)"
+    if [ -n "$found" ] ; then
+        echo -e "\nErrors found in build log files:\n$found\n"
+    else
+        echo -e "\nNo errors found in build logs\n"
+    fi
+
+    # Find warnings
+    found="$(grep -nisH warning $DOCKER_BUILD_LOG_DIR/*-bld.log)"
+    if [ -n "$found" ] ; then
+        echo -e "\nWarnings found in build log files:\n$found\n"
+    else
+        echo -e "\nNo warnings found in build logs\n"
+    fi
+
+    grep -q e <<< $set_opts && set -e # re-enable exit on errors
+}
+
+dump_cache_files() {
+    local cache_files_log="$DOCKER_BUILD_LOG_DIR/cached_files.json"
+    tree -a --timefmt "+%Y-%m-%d %H:%M:%S" --prune /root
+    tree -afJ --timefmt "+%Y-%m-%d %H:%M:%S" --prune -o $cache_files_log /root
+}
+
+dump_cache_files
+dump_build_logs
+dump_echo_log
