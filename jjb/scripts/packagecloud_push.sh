928d0f859776b4585c5cb78ce930b3729e7fdd3c
Dave Wallace <dwallacelf@gmail.com>
2020-10-01 20:38:47 +0000

    Clean up vpp jenkins scripts & docker dir
    - Remove docker/vpp Dockerfile definitions and
      colateral files for image now being build by
      https://gerrit.fd.io/r/c/ci-management/+/28022
    - Remove opensuse support which has been elided
      from the vpp build in
      https://gerrit.fd.io/r/c/vpp/+/28999
    - Remove unused scripts.
    - Output script name marker for all jjb scripts
      following LFIT practices.
    - Remove old/unused output from scripts.
    - Add logic to use vpp-ext-deps download cache
      in the executor images to speed up build jobs.
    - Add copyright notifcations to modified files
      without an existing copyright.
    - Make all vpp/docs jobs use '-{stream}-{os}-{executor}'
      suffix in their names.
    - Consolidate executor logging into
      setup_executor_env.sh and call it first for all jobs.
    
    Change-Id: I4ffb67ed476659f0127ca8c84f20a305d06149bf
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- jjb/scripts/packagecloud_push.sh
+++ jjb/scripts/packagecloud_push.sh
@@ -1,39 +1,59 @@
 #!/bin/bash
-# PCIO_CO is a Jenkins Global Environment variable
 
-set -x
+# Copyright (c) 2020 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "---> jjb/scripts/packagecloud_push.sh"
+
+set -euxo pipefail
 
 echo "STARTING PACKAGECLOUD PUSH"
 
 sleep 10
 
-if [ -f /usr/bin/zypper ]; then
-    FACTER_OS="openSUSE"
-else
-    FACTER_OS=$(/usr/bin/facter operatingsystem)
-fi
+FACTER_OS=$(/usr/bin/facter operatingsystem)
+push_cmd=""
 
+# PCIO_CO and SILO are Jenkins Global Environment variables defined in
+# .../ci-management/jenkins-config/global-vars-*.sh
 if [ -f ~/.packagecloud ]; then
     case "$FACTER_OS" in
-      Ubuntu)
-        FACTER_LSBNAME=$(/usr/bin/facter lsbdistcodename)
-        DEBS=$(find . -type f -iname '*.deb')
-        package_cloud push "${PCIO_CO}/${STREAM}/ubuntu/${FACTER_LSBNAME}/main/" ${DEBS}
-      ;;
-      CentOS)
-        FACTER_OSMAJREL=$(/usr/bin/facter operatingsystemmajrelease)
-        FACTER_ARCH=$(/usr/bin/facter architecture)
-        RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
-        package_cloud push "${PCIO_CO}/${STREAM}/el/${FACTER_OSMAJREL}/os/${FACTER_ARCH}/" ${RPMS}
-      ;;
-      openSUSE)
-        # Use /etc/os-release on openSUSE to get $VERSION
-        . /etc/os-release
-        RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm' | grep -v 'vpp-ext-deps')
-        VPP_EXT_RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm' | grep 'vpp-ext-deps')
-        package_cloud push "${PCIO_CO}/${STREAM}/opensuse/${VERSION}/" ${RPMS}
-        # This file may have already been uploaded. Don't error out if it exists.
-        package_cloud push "${PCIO_CO}/${STREAM}/opensuse/${VERSION}/" ${VPP_EXT_RPMS} --skip-errors
-      ;;
+        Ubuntu)
+            FACTER_LSBNAME=$(/usr/bin/facter lsbdistcodename)
+            DEBS=$(find . -type f -iname '*.deb')
+            push_cmd="package_cloud push ${PCIO_CO}/${STREAM}/ubuntu/${FACTER_LSBNAME}/main/ ${DEBS}"
+            ;;
+        CentOS)
+            FACTER_OSMAJREL=$(/usr/bin/facter operatingsystemmajrelease)
+            FACTER_ARCH=$(/usr/bin/facter architecture)
+            RPMS=$(find . -type f -iregex '.*/.*\.\(s\)?rpm')
+            push_cmd="package_cloud push ${PCIO_CO}/${STREAM}/el/${FACTER_OSMAJREL}/os/${FACTER_ARCH}/ ${RPMS}"
+            ;;
+        *)
+            echo "ERROR: Unsupported OS '$FACTER_OS'"
+            echo "PACKAGECLOUD PUSH FAILED!"
+            exit 1
+            ;;
     esac
+    if [ "${SILO,,}" = "sandbox" ] ; then
+        echo "SANDBOX: skipping '$push_cmd'"
+    else
+        $push_cmd
+    fi
+else
+    echo "ERROR: Missing '~/.packagecloud' for user '$(id)'"
+    echo "PACKAGECLOUD PUSH FAILED!"
+    exit 1
 fi
+
+echo "PACKAGECLOUD PUSH COMPLETE"
