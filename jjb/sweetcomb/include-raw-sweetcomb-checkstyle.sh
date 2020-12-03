1c01372a3ddefe84ea755c17347e4ca760af9c1b
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2020-12-03 15:38:16 -0600

    Remove Sweetcomb jobs
    The Sweetcomb project has been archived. This patch is to remove
    existing jobs.
    
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
    Change-Id: Ic9d5d0d351767481c07ac27c1765160ed635228a

--- jjb/sweetcomb/include-raw-sweetcomb-checkstyle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -f build-root/scripts/checkstyle.sh ];then
-    build-root/scripts/checkstyle.sh
-else
-    echo "Cannot find cat build-root/scripts/checkstyle.sh - skipping checkstyle"
-fi
