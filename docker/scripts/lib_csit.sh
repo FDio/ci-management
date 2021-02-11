7aec65e257592fbbc0e79188c44113d2d80f829e
Dave Wallace <dwallacelf@gmail.com>
2021-02-11 16:44:59 -0500

    Use Ubuntu-20.04 for vpp-csit-* job executor images
    - refactor vpp-csit job names to conform to the vpp
      job nomenclature
    - add ubuntu-20.04 for CSIT image builder install
      requirements
    - fix executor class verification
    - add option to build_executor_docker_image.sh to
      dump Dockerfile without building image(s)
    - refactor Dockerfile generation to support
      executor classes
    - add support for csit_shim and csit_dut docker
      image generation from csit ansible repo
    - refactor docker installation
    - refactor locale support
    - refactor package cloud installation
    
    Change-Id: I5f4ffc587c6b8c43cb874dfd8c615ce56d3b97a4
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- docker/scripts/lib_csit.sh
+++ docker/scripts/lib_csit.sh
@@ -1,7 +1,7 @@
 # lib_csit.sh - Docker build script CSIT library.
 #               For import only.
 
-# Copyright (c) 2020 Cisco and/or its affiliates.
+# Copyright (c) 2021 Cisco and/or its affiliates.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
@@ -26,6 +26,25 @@ export CIMAN_DOCKER_SCRIPTS="${CIMAN_DOCKER_SCRIPTS:-$(dirname $BASH_SOURCE)}"
 . $CIMAN_DOCKER_SCRIPTS/lib_yum.sh
 . $CIMAN_DOCKER_SCRIPTS/lib_dnf.sh
 
+CSIT_SUPPORTED_EXECUTOR_CLASSES="builder csit csit_dut"
+csit_supported_executor_class() {
+    if ! grep -q "${1:-}" <<< $CSIT_SUPPORTED_EXECUTOR_CLASSES ; then
+        return 1
+    fi
+    return 0
+}
+
+csit_supported_os() {
+    case "$1" in
+        # TODO: Remove ubuntu-18.04 once CSIT has completed transition
+        #       to ubuntu-20.04
+        ubuntu-18.04) return 0 ;;
+        ubuntu-20.04) return 0 ;;
+                   *) ;;
+    esac
+    return 1
+}
+
 csit_checkout_branch_for_vpp() {
     local vpp_branch=$1
     local csit_dir="$DOCKER_CSIT_DIR"
@@ -34,7 +53,7 @@ csit_checkout_branch_for_vpp() {
     # import checkout_csit_for_vpp() if not defined
     set +e && [ -z "$(declare -f checkout_csit_for_vpp)" ] \
         && source $csit_bash_function_dir/branch.sh
-    CSIT_DIR=$(csit_dir) checkout_csit_for_vpp $vpp_branch
+    CSIT_DIR=$csit_dir checkout_csit_for_vpp $vpp_branch
 
     csit_branch="$(git branch | grep -e '^*' | mawk '{print $2}')"
 }
@@ -61,34 +80,38 @@ csit_install_packages() {
         python3 -m pip install pyyaml
     fi
 
-    # NOTE: the CSIT baseline os is implicitly pinned to ubuntu 18.04
-    #       so only gather packages from ansible for that OS.
-    if [ "$OS_NAME" = "ubuntu-18.04" ] ; then
-        # Not in double quotes to let bash remove newline characters
-        local exclude_roles="-e calibration -e kernel -e mellanox -e nomad"
-        [ "$OS_ARCH" = "aarch64" ] && exclude_roles="$exclude_roles -e iperf"
-        local yaml_files=$(grep -r packages_by $csit_ansible_dir | cut -d: -f1 | sort -u | grep -v $exclude_roles)
-        packages=$(dbld_csit_find_ansible_packages.py --$OS_ID --$OS_ARCH $yaml_files)
+    local exclude_roles="-e calibration -e kernel -e mellanox -e nomad -e consul"
+    [ "$OS_ARCH" = "aarch64" ] && exclude_roles="$exclude_roles -e iperf"
 
-        if [ -n "$packages" ] ; then
-            case "$OS_NAME" in
-                ubuntu*)
-                    apt_install_packages $packages
-                    ;;
-                debian*)
-                    apt_install_packages $packages
-                    ;;
-                centos-7)
-                    yum_install_packages $packages
-                    ;;
-                centos-8)
-                    dnf_install_packages $packages
-                    ;;
-                *)
-                    echo "Unsupported OS ($OS_ID): CSIT packages NOT INSTALLED!"
-                    ;;
-            esac
-        fi
+    # Not in double quotes to let bash remove newline characters
+    local yaml_files=$(grep -r packages_by $csit_ansible_dir | cut -d: -f1 | sort -u | grep -v $exclude_roles)
+    packages=$(dbld_csit_find_ansible_packages.py --$OS_ID --$OS_ARCH $yaml_files)
+
+    # TODO: Fix Ubuntu-18.04 specific package names that fail on Ubuntu-20.04
+    #       (remove when CSIT is updated)
+    if [ "$OS_NAME" = "ubuntu-20.04" ] ; then
+        packages="${packages/libmbedcrypto1/libmbedcrypto3}"
+        packages="${packages/libmbedtls10/libmbedtls12}"
+        packages="$(echo ${packages//python\-/python3\-} | tr ' ' '\n' | sort -u | xargs)"
+    fi
+    if [ -n "$packages" ] ; then
+        case "$OS_NAME" in
+            ubuntu*)
+                apt_install_packages $packages
+                ;;
+            debian*)
+                apt_install_packages $packages
+                ;;
+            centos-7)
+                yum_install_packages $packages
+                ;;
+            centos-8)
+                dnf_install_packages $packages
+                ;;
+            *)
+                echo "Unsupported OS ($OS_ID): CSIT packages NOT INSTALLED!"
+                ;;
+        esac
     fi
 }
 
@@ -164,4 +187,164 @@ docker_build_setup_csit() {
         git clone -q https://gerrit.fd.io/r/csit $DOCKER_CSIT_DIR
     fi
     clean_git_repo $DOCKER_CSIT_DIR
+}
+
+csit_dut_generate_docker_build_files() {
+    local build_files_dir="$DOCKER_BUILD_DIR/files"
+
+    cat <<EOF >$build_files_dir/suporvisord.conf
+[unix_http_server]
+file = /tmp/supervisor.sock
+chmod = 0777
+
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+
+[supervisorctl]
+serverurl = unix:///tmp/supervisor.sock
+
+[supervisord]
+pidfile = /tmp/supervisord.pid
+identifier = supervisor
+directory = /tmp
+logfile = /tmp/supervisord.log
+loglevel = debug
+nodaemon = false
+
+[program:vpp]
+command = /usr/bin/vpp -c /etc/vpp/startup.conf
+autostart = false
+autorestart = true
+redirect_stderr = true
+priority = 1
+EOF
+}
+
+csit_shim_generate_docker_build_files() {
+    local build_files_dir="$DOCKER_BUILD_DIR/files"
+    local badkey='AAAAB3NzaC1yc2EAAAADAQABAAABAQCyUNd/iRk5Ajw4ZBB0gXyjzecEzQHh/MctgvHGJjasqJDkwYyZBrunUorOZ3n82W8VGdd5+eNINCWOM/ERjuaHjnutfade+ocPgZRdk+kEgTvetDVNWIgBd0PMVcnp57jJfx7CZVqTNgGeVQ8OJ2RbJGeOb/EKApQI74IPkAfc0PSieSw5gC0eqEOHb39Awgp0ycrzsUHF/OEicfCmo+6vvrMGenDe7frKUoTKYMWs7l3DOyFC8NaOxhGD3J1Ne5u3A/r4w6mN1HVI0rFwIcoms+t0B4lb2ODWKZiZikQdn8/eqwsmbSEZZsWN3FkshgjPS83+dNqVwB6pPY5Yqte7'
+
+    mkdir -p $build_files_dir
+    echo "ssh-rsa $badkey ejk@bhima.local" >$build_files_dir/badkeypub
+
+    cat <<EOF >$build_files_dir/sshconfig
+Host 172.17.0.*
+	StrictHostKeyChecking no
+	UserKnownHostsFile=/dev/null
+EOF
+    cat <<EOF >$build_files_dir/wrapdocker
+#!/bin/bash
+
+# Ensure that all nodes in /dev/mapper correspond to mapped devices currently loaded by the device-mapper kernel driver
+dmsetup mknodes
+
+# First, make sure that cgroups are mounted correctly.
+CGROUP=/sys/fs/cgroup
+: {LOG:=stdio}
+
+[ -d \$CGROUP ] ||
+    mkdir \$CGROUP
+
+mountpoint -q \$CGROUP ||
+    mount -n -t tmpfs -o uid=0,gid=0,mode=0755 cgroup \$CGROUP || {
+        echo "Could not make a tmpfs mount. Did you use --privileged?"
+        exit 1
+    }
+
+if [ -d /sys/kernel/security ] && ! mountpoint -q /sys/kernel/security
+then
+    mount -t securityfs none /sys/kernel/security || {
+        echo "Could not mount /sys/kernel/security."
+        echo "AppArmor detection and --privileged mode might break."
+    }
+fi
+
+# Mount the cgroup hierarchies exactly as they are in the parent system.
+for SUBSYS in \$(cut -d: -f2 /proc/1/cgroup)
+do
+        [ -d \$CGROUP/\$SUBSYS ] || mkdir \$CGROUP/\$SUBSYS
+        mountpoint -q \$CGROUP/\$SUBSYS ||
+                mount -n -t cgroup -o \$SUBSYS cgroup \$CGROUP/\$SUBSYS
+
+        # The two following sections address a bug which manifests itself
+        # by a cryptic "lxc-start: no ns_cgroup option specified" when
+        # trying to start containers withina container.
+        # The bug seems to appear when the cgroup hierarchies are not
+        # mounted on the exact same directories in the host, and in the
+        # container.
+
+        # Named, control-less cgroups are mounted with "-o name=foo"
+        # (and appear as such under /proc/<pid>/cgroup) but are usually
+        # mounted on a directory named "foo" (without the "name=" prefix).
+        # Systemd and OpenRC (and possibly others) both create such a
+        # cgroup. To avoid the aforementioned bug, we symlink "foo" to
+        # "name=foo". This shouldn't have any adverse effect.
+        echo \$SUBSYS | grep -q ^name= && {
+                NAME=\$(echo \$SUBSYS | sed s/^name=//)
+                ln -s \$SUBSYS \$CGROUP/\$NAME
+        }
+
+        # Likewise, on at least one system, it has been reported that
+        # systemd would mount the CPU and CPU accounting controllers
+        # (respectively "cpu" and "cpuacct") with "-o cpuacct,cpu"
+        # but on a directory called "cpu,cpuacct" (note the inversion
+        # in the order of the groups). This tries to work around it.
+        [ \$SUBSYS = cpuacct,cpu ] && ln -s \$SUBSYS \$CGROUP/cpu,cpuacct
+done
+
+# Note: as I write those lines, the LXC userland tools cannot setup
+# a "sub-container" properly if the "devices" cgroup is not in its
+# own hierarchy. Let's detect this and issue a warning.
+grep -q :devices: /proc/1/cgroup ||
+    echo "WARNING: the 'devices' cgroup should be in its own hierarchy."
+grep -qw devices /proc/1/cgroup ||
+    echo "WARNING: it looks like the 'devices' cgroup is not mounted."
+
+# Now, close extraneous file descriptors.
+pushd /proc/self/fd >/dev/null
+for FD in *
+do
+    case "\$FD" in
+    # Keep stdin/stdout/stderr
+    [012])
+        ;;
+    # Nuke everything else
+    *)
+        eval exec "\$FD>&-"
+        ;;
+    esac
+done
+popd >/dev/null
+
+
+# If a pidfile is still around (for example after a container restart),
+# delete it so that docker can start.
+rm -rf /var/run/docker.pid
+
+# If we were given a PORT environment variable, start as a simple daemon;
+# otherwise, spawn a shell as well
+if [ "\$PORT" ]
+then
+    exec dockerd -H 0.0.0.0:\$PORT -H unix:///var/run/docker.sock \
+        \$DOCKER_DAEMON_ARGS
+else
+    if [ "\$LOG" == "file" ]
+    then
+        dockerd \$DOCKER_DAEMON_ARGS &>/var/log/docker.log &
+    else
+        dockerd \$DOCKER_DAEMON_ARGS &
+    fi
+    (( timeout = 60 + SECONDS ))
+    until docker info >/dev/null 2>&1
+    do
+        if (( SECONDS >= timeout )); then
+            echo 'Timed out trying to connect to internal docker host.' >&2
+            break
+        fi
+        sleep 1
+    done
+    [[ \$1 ]] && exec "\$@"
+    exec bash --login
+fi
+EOF
 }
