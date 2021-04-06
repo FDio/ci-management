3189f7d890306a0ba2f1abe4644511c390e51218
Dave Wallace <dwallacelf@gmail.com>
2021-04-06 17:08:21 -0400

    Use fdio-infra-publish for nomad executor based jobs
    - Separate post build executor info dump into its
      own script file.
    - Remove backup_upload_archives.sh from
      fdio-infra-shiplogs which is now only used for
      openstack executors.
    
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>
    Change-Id: I564201b8c578e6541152e6af74537d1d007ba1a7

---
+++ jjb/scripts/post_build_executor_info.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Copyright (c) 2021 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "---> jjb/scripts/post_build_executor_info.sh"
+
+# Output executor runtime attributes [again] in case the job fails prior to
+# running setup_executor_env.sh
+long_line="************************************************************************"
+OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_ARCH=$(uname -m)
+echo "$long_line"
+echo "Executor Runtime Attributes:"
+echo "OS: $OS_ID-$OS_VERSION_ID"
+echo "Arch: $OS_ARCH"
+echo "Nomad Hostname: $(grep search /etc/resolv.conf | cut -d' ' -f2 | head -1)"
+echo "Container ID: $(hostname)"
+
+if [ -n "$(which ccache)" ] ; then
+    echo "$long_line"
+    [ -n "${CCACHE_DISABLE:-}" ] && echo "CCACHE_DISABLE = '$CCACHE_DISABLE'"
+    [ -n "${CCACHE_DIR:-}" ] && echo "CCACHE_DIR = '$CCACHE_DIR'"
+    echo "ccache statistics:"
+    ccache -s
+fi
+
+echo "$long_line"
