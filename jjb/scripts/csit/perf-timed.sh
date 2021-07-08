fc0610b66b7caab0efe9a36afc0976e736c41d9e
pmikus <pmikus@cisco.com>
2021-07-08 12:34:33 +0000

    Global: Rework archive artifacts
    This patch is unifying location of artifacts archived
    with logs, removes archive-artifacts-parameter macro
    and ARCHVIVE_ARTIFACTS env var from all project and
    template definitions.
    
    All project specific artifacts to be uploaded with the
    log files are copied to $WORKSPACE/archives.
    
    Unfortunately enough the NEXUS storage and S3 is
    mutual exclusive as per global-jjb and global releng
    so they have been separated into different macros. The
    next step once this is merged will be to remove NEXUS
    entirely via JCasC.
    
    + Remove archive-artifacts from all yaml files.
    + Add fdio-infra-ship-backup-logs macro
    + Remove unused jjb/include-raw-deploy-archives.sh
    + CSIT:
      - copy job artifacts to $WORKSPACE/archives
    + HC2VPP
      - remove CSIT
    + TLDK:
      - remove CSIT
    
    Signed-off-by: pmikus <pmikus@cisco.com>
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>
    Change-Id: Iada020cf269714c34f9ce32d764d991827e3b003

--- jjb/scripts/csit/perf-timed.sh
+++ jjb/scripts/csit/perf-timed.sh
@@ -40,3 +40,4 @@ fi
 popd
 csit_entry_dir="${WORKSPACE}/csit/resources/libraries/bash/entry"
 source "${csit_entry_dir}/with_oper_for_vpp.sh" "bootstrap_verify_perf.sh"
+cp -R "${WORKSPACE}/csit/archives" "${WORKSPACE}/archives" || true
