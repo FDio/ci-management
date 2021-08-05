FDio/ci-management

commit 37a55a05cd6ac7ca15bbb99f5ddc9ca61874d5f8

Global: Rework archive artifacts
This patch removes archive-artifacts-parameter macro
and ARCHVIVE_ARTIFACTS env var from csit and vpp project.

All project specific artifacts to be uploaded with the
log files SHOULD BE copied to $WORKSPACE/archives.

The next step once this is merged will be to remove NEXUS
entirely via JCasC.

+ Remove archive-artifacts from all csit/vpp yaml files.
+ Add fdio-infra-ship-backup-logs macro
+ Remove unused jjb/include-raw-deploy-archives.sh
+ CSIT:
  - copy job artifacts to $WORKSPACE/archives
+ HC2VPP
  - remove CSIT
+ TLDK:
  - remove CSIT

Signed-off-by: pmikus <pmikus@cisco.com>
Signed-off-by: Dave Wallace <dwallacelf@gmail.com>
Change-Id: Iada020cf269714c34f9ce32d764d991827e3b003

jjb/scripts/vpp/csit-device.sh
@@ -38,3 +38,4 @@ fi
 popd
 csit_entry_dir="${WORKSPACE}/csit/resources/libraries/bash/entry"
 source "${csit_entry_dir}/with_oper_for_vpp.sh" "per_patch_device.sh"
+cp -R "${WORKSPACE}/csit_current/archives/"* "${WORKSPACE}/archives/" || true

jjb/scripts/vpp/csit-perf.sh
@@ -38,3 +38,5 @@ fi
 popd
 csit_entry_dir="${WORKSPACE}/csit/resources/libraries/bash/entry"
 source "${csit_entry_dir}/with_oper_for_vpp.sh" "per_patch_perf.sh"
+cp -R "${WORKSPACE}/csit_current/"* "${WORKSPACE}/archives/" || true
+cp -R "${WORKSPACE}/csit_parent/"* "${WORKSPACE}/archives/" || true

jjb/csit/csit-perf.yaml
@@ -355,7 +355,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: true
-    archive-artifacts: 'archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -376,10 +375,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
       # This is a manually-triggered verify job, part of API coverage.
       # CRC checking is needed to ensure vpp crc job breaks only when intended.
       # Can be removed when this job is no longer needed for full API coverage.
@@ -412,11 +407,6 @@
           - ../scripts/csit/perf-verify.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -447,7 +437,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: false
-    archive-artifacts: 'csit/archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -465,10 +454,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     wrappers:
       - fdio-infra-wrappers-non-activity-timeout:
@@ -489,11 +474,6 @@
           - ../scripts/csit/perf-timed.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -524,7 +504,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: false
-    archive-artifacts: 'csit/archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -542,10 +521,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     wrappers:
       - fdio-infra-wrappers-non-activity-timeout:
@@ -566,11 +541,6 @@
           - ../scripts/csit/perf-timed.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -600,7 +570,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: false
-    archive-artifacts: 'csit/archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -614,10 +583,6 @@
           project: '{project}'
       - gerrit-parameter:
           branch: '{branch}'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     wrappers:
       - fdio-infra-wrappers-non-activity-timeout:
@@ -638,11 +603,6 @@
           - ../scripts/csit/perf-timed.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -689,7 +649,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: true
-    archive-artifacts: 'archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -707,10 +666,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - gerrit-trigger-scm:
@@ -738,11 +693,6 @@
           - ../scripts/csit/perf-verify.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -773,7 +723,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: false
-    archive-artifacts: 'csit/archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -791,10 +740,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     wrappers:
       - fdio-infra-wrappers-non-activity-timeout:
@@ -815,11 +760,6 @@
           - ../scripts/csit/perf-timed.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -864,7 +804,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: true
-    archive-artifacts: 'archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -885,10 +824,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - gerrit-trigger-scm:
@@ -916,11 +851,6 @@
           - ../scripts/csit/perf-verify.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -965,7 +895,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: true
-    archive-artifacts: 'archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -986,10 +915,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - gerrit-trigger-scm:
@@ -1017,11 +942,6 @@
           - ../scripts/csit/perf-verify.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -1066,7 +986,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: true
-    archive-artifacts: 'archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -1087,10 +1006,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - gerrit-trigger-scm:
@@ -1118,11 +1033,6 @@
           - ../scripts/csit/perf-verify.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish
 
 - job-template:
@@ -1167,7 +1077,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: true
-    archive-artifacts: 'archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -1188,10 +1097,6 @@
           csit-perf-trial-duration: "{csit-perf-trial-duration}"
       - csit-perf-trial-multiplicity-parameter:
           csit-perf-trial-multiplicity: "{csit-perf-trial-multiplicity}"
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - gerrit-trigger-scm:
@@ -1219,9 +1124,4 @@
           - ../scripts/csit/perf-verify.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
-
       - fdio-infra-publish

jjb/csit/csit-tox.yaml
@@ -25,8 +25,6 @@
     stream:
       - master:
           branch: 'master'
-      - '2009':
-          branch: 'rls2009'
       - '2009_lts':
           branch: 'rls2009_lts'
       - '2101':
@@ -40,7 +38,6 @@
     project-type: freestyle
     node: 'builder-{os}-prod-{executor-arch}'
     concurrent: true
-    archive-artifacts: '*.log'
     latest-only: false
 
     build-discarder:
@@ -55,10 +52,6 @@
       - gerrit-parameter:
           branch: '{branch}'
       - gerrit-refspec-parameter
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - gerrit-trigger-scm:
@@ -112,12 +105,4 @@
           - ../scripts/csit/tox.sh
 
     publishers:
-      # TODO: Remove this when a voting pylint difference checker
-      # with nice enough output is implemented.
-      - violations:
-          pylint:
-            min: 10
-            max: 250
-            unstable: 250
-            pattern: 'pylint.log'
       - fdio-infra-publish

jjb/csit/csit-vpp-device.yaml
@@ -80,7 +80,6 @@
     project-type: freestyle
     node: 'csit-builder-{os}-prod-{executor-arch}'
     concurrent: true
-    archive-artifacts: 'archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -101,10 +100,6 @@
       - gerrit-refspec-parameter
       - gerrit-event-type-parameter
       - gerrit-event-comment-text-parameter
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
       # This is an automatic verify job, part of API coverage.
       # CRC checking is needed to ensure vpp crc job breaks only when intended.
       # Unlikely to be removed, as this job is primary source of API coverage.
@@ -136,10 +131,6 @@
           - ../scripts/csit/device-verify.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
       - fdio-infra-publish
 
 - job-template:
@@ -148,7 +139,6 @@
     project-type: freestyle
     node: 'csit-builder-{os}-prod-{executor-arch}'
     concurrent: false
-    archive-artifacts: 'csit/archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -166,10 +156,6 @@
           make-parallel-jobs: '{make-parallel-jobs}'
       - os-parameter:
           os: '{os}'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     wrappers:
       - fdio-infra-wrappers-non-activity-timeout:
@@ -195,10 +181,6 @@
           - ../scripts/csit/device-semiweekly.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
       - fdio-infra-publish
 
 - job-template:
@@ -207,7 +189,6 @@
     project-type: freestyle
     node: 'csit-builder-{os}-prod-{executor-arch}'
     concurrent: false
-    archive-artifacts: 'archive/*.*'
     latest-only: false
 
     build-discarder:
@@ -225,10 +206,6 @@
           make-parallel-jobs: '{make-parallel-jobs}'
       - os-parameter:
           os: '{os}'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - git-scm:
@@ -256,8 +233,4 @@
           - ../scripts/csit/device-verify.sh
 
     publishers:
-      - robot:
-          output-path: 'archives'
-          other-files:
-            - '*.*'
       - fdio-infra-publish

jjb/csit/csit.yaml
@@ -68,10 +68,6 @@
           branch: '{branch}'
       - maven-exec:
           maven-version: 'mvn33-new'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - gerrit-trigger-scm:
@@ -149,10 +145,6 @@
           branch: '{branch}'
       - maven-exec:
           maven-version: 'mvn33-new'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
 
     scm:
       - gerrit-trigger-scm:

jjb/global-macros.yaml
@@ -667,7 +667,7 @@
               build-steps:
                 - shell: !include-raw: scripts/post_build_executor_info.sh
                 - shell: !include-raw: scripts/post_build_deploy_archives.sh
-                - shell: !include-raw: scripts/backup_upload_archives.sh
+                - fdio-infra-ship-backup-logs
                 - fdio-infra-ship-logs
           mark-unstable-if-failed: true
       - workspace-cleanup:
@@ -725,6 +725,18 @@
           num-to-keep: '{build-num-to-keep}'
 
 - builder:
+    name: fdio-infra-ship-backup-logs
+    builders:
+      - config-file-provider:
+          files:
+            - file-id: "jenkins-s3-log-ship"
+              variable: $HOME/.aws/credentials
+      - shell: !include-raw:
+          - scripts/logs_publish.sh
+      - shell: !include-raw:
+          - ../global-jjb/shell/logs-clear-credentials.sh
+
+- builder:
     name: fdio-infra-ship-logs
     builders:
       # Ensure no pre-existing .netrc files are overriding logs config
@@ -750,7 +762,7 @@
       - shell: !include-raw:
           - ../global-jjb/shell/logs-clear-credentials.sh
       - description-setter:
-          regexp: "(^(Nexus|S3) build logs: .*)"
+          regexp: "(^Nexus build logs: .*)"
 
 - builder:
     name: packer-validate

jjb/hc2vpp/hc2vpp-csit.yaml
@@ -1,421 +1,0 @@
----
-- project:
-    name: hc2vpp-csit
-    description: 'jenkins jobs to test Hc2vpp features.'
-    jobs:
-      - 'hc2vpp-csit-integration-{stream}-{os}'
-      - 'hc2vpp-csit-verify-{stream}-{os}'
-      - 'hc2vpp-csit-integration-odl-{odl}-{stream}-{os}'
-      - 'hc2vpp-csit-verify-odl-{odl}-{stream}-{os}'
-    project: 'hc2vpp'
-    build-artifact-num-to-keep: 10
-    stream:
-      - master:
-          branch: 'master'
-          repo-stream-part: 'master'
-      - '1904':
-          branch: 'stable/1904'
-          repo-stream-part: 'stable.1904'
-
-    os:
-      - ubuntu1804:
-          repo-os-part: 'ubuntu.bionic.main'
-
-    odl: 'neon'
-
-- project:
-    name: csit-hc2vpp-perf
-    description: 'jenkins jobs to test Hc2vpp performance.'
-    jobs:
-      - 'hc2vpp-csit-perf-{stream}-{os}'
-    project: 'hc2vpp'
-    build-artifact-num-to-keep: 10
-    stream:
-      - master:
-          branch: 'master'
-          repo-stream-part: 'master'
-
-    os:
-      - ubuntu1804:
-          repo-os-part: 'ubuntu.bionic.main'
-
-    odl: 'neon'
-
-- job-template:
-    name: 'hc2vpp-csit-integration-{stream}-{os}'
-
-    project-type: freestyle
-    node: '{os}-builder-4c-4g'
-    concurrent: true
-    archive-artifacts: '**/csit/archive/*.*'
-    latest-only: false
-
-
-    build-discarder:
-      daysToKeep: '{build-days-to-keep}'
-      numToKeep: '{build-num-to-keep}'
-      artifactDaysToKeep: '{build-artifact-days-to-keep}'
-      artifactNumToKeep: '{build-artifact-num-to-keep}'
-
-    parameters:
-      - project-parameter:
-          project: '{project}'
-      - os-parameter:
-          os: '{os}'
-      - stream-parameter:
-          stream: '{stream}'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
-
-    scm:
-      - git-scm:
-          credentials-id: 'jenkins-gerrit-credentials'
-          branch: '{branch}'
-
-    wrappers:
-      - fdio-infra-wrappers:
-          build-timeout: '{build-timeout}'
-
-    triggers:
-      - reverse:
-          jobs: 'hc2vpp-integration-{stream}-{os}'
-          result: 'success'
-
-    builders:
-      - shell:
-          !include-raw-escape: include-raw-hc2vpp-csit-integration.sh
-
-    publishers:
-      - email-notification:
-          email-prefix: '[hc2vpp]'
-
-      - robot-report:
-          output-path: 'csit/archive'
-
-      - lf-infra-publish
-
-- job-template:
-    name: 'hc2vpp-csit-verify-{stream}-{os}'
-
-    project-type: maven
-    node: '{os}-builder-4c-4g'
-    jdk: openjdk8-{os}
-    concurrent: true
-    archive-artifacts: '**/csit/archive/*.*'
-    latest-only: false
-
-    build-discarder:
-      daysToKeep: '{build-days-to-keep}'
-      numToKeep: '{build-num-to-keep}'
-      artifactDaysToKeep: '{build-artifact-days-to-keep}'
-      artifactNumToKeep: '{build-artifact-num-to-keep}'
-
-    parameters:
-      - project-parameter:
-          project: '{project}'
-      - gerrit-parameter:
-          branch: '{branch}'
-      - os-parameter:
-          os: '{os}'
-      - maven-project-parameter:
-          maven: 'mvn36'
-      - maven-exec:
-          maven-version: 'mvn36'
-      - stream-parameter:
-          stream: '{stream}'
-      - odl-parameter:
-          odl: '{odl}'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
-
-    scm:
-      - gerrit-trigger-scm:
-          credentials-id: 'jenkins-gerrit-credentials'
-          refspec: '$GERRIT_REFSPEC'
-          choosing-strategy: 'gerrit'
-
-    wrappers:
-      - fdio-infra-wrappers:
-          build-timeout: '{build-timeout}'
-
-    triggers:
-      - gerrit:
-          server-name: 'Primary'
-          trigger-on:
-            - comment-added-contains-event:
-                comment-contains-value: 'verify-csit'
-          projects:
-            - project-compare-type: 'ANT'
-              project-pattern: '{project}'
-              branches:
-                - branch-compare-type: 'ANT'
-                  branch-pattern: '**/{branch}'
-          skip-vote:
-            successful: true
-            failed: true
-            unstable: true
-            notbuilt: true
-
-    prebuilders:
-      - provide-maven-settings:
-          settings-file: 'honeycomb-settings'
-          global-settings-file: 'global-settings'
-      - shell:
-          !include-raw-escape: include-raw-hc2vpp-csit-verify-prebuild.sh
-
-    maven:
-      maven-name: 'mvn36'
-      root-pom: 'pom.xml'
-      # yamllint disable-line rule:line-length
-      goals: 'clean install -V -Dmaven.repo.local=/tmp/r -Dorg.ops4j.pax.url.mvn.localRepository=/tmp/r -DskipTests -Dcheckstyle.skip=true'
-      maven-opts: '-Xmx2048m -Dmaven.compile.fork=true'
-      settings: 'hc2vpp-settings'
-      settings-type: cfp
-      global-settings: 'global-settings'
-      global-settings-type: cfp
-
-    postbuilders:
-      - shell:
-          !include-raw-escape: include-raw-hc2vpp-csit-verify.sh
-
-    publishers:
-      - email-notification:
-          email-prefix: '[hc2vpp]'
-
-      - robot-report:
-          output-path: 'csit/archive'
-
-      - lf-infra-publish
-
-- job-template:
-    name: 'hc2vpp-csit-integration-odl-{odl}-{stream}-{os}'
-
-    project-type: freestyle
-    node: '{os}-builder-4c-4g'
-    concurrent: true
-    archive-artifacts: '**/csit/archive/*.*'
-    latest-only: false
-
-
-    build-discarder:
-      daysToKeep: '{build-days-to-keep}'
-      numToKeep: '{build-num-to-keep}'
-      artifactDaysToKeep: '{build-artifact-days-to-keep}'
-      artifactNumToKeep: '{build-artifact-num-to-keep}'
-
-    parameters:
-      - project-parameter:
-          project: '{project}'
-      - os-parameter:
-          os: '{os}'
-      - stream-parameter:
-          stream: '{stream}'
-      - odl-parameter:
-          odl: '{odl}'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
-
-    scm:
-      - git-scm:
-          credentials-id: 'jenkins-gerrit-credentials'
-          branch: '{branch}'
-
-    wrappers:
-      - fdio-infra-wrappers:
-          build-timeout: '{build-timeout}'
-
-    triggers:
-      - reverse:
-          jobs: 'hc2vpp-integration-{stream}-{os}'
-          result: 'success'
-
-    builders:
-      - shell:
-          !include-raw-escape: include-raw-hc2vpp-csit-integration-odl.sh
-
-    publishers:
-      - email-notification:
-          email-prefix: '[hc2vpp]'
-
-      - robot-report:
-          output-path: 'csit/archive'
-
-      - lf-infra-publish
-
-- job-template:
-    name: 'hc2vpp-csit-verify-odl-{odl}-{stream}-{os}'
-
-    project-type: maven
-    node: '{os}-builder-4c-4g'
-    jdk: openjdk8-{os}
-    concurrent: true
-    archive-artifacts: '**/csit/archive/*.*'
-    latest-only: false
-
-    build-discarder:
-      daysToKeep: '{build-days-to-keep}'
-      numToKeep: '{build-num-to-keep}'
-      artifactDaysToKeep: '{build-artifact-days-to-keep}'
-      artifactNumToKeep: '{build-artifact-num-to-keep}'
-    parameters:
-      - project-parameter:
-          project: '{project}'
-      - gerrit-parameter:
-          branch: '{branch}'
-      - os-parameter:
-          os: '{os}'
-      - maven-project-parameter:
-          maven: 'mvn36'
-      - maven-exec:
-          maven-version: 'mvn36'
-      - stream-parameter:
-          stream: '{stream}'
-      - odl-parameter:
-          odl: '{odl}'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
-
-    scm:
-      - gerrit-trigger-scm:
-          credentials-id: 'jenkins-gerrit-credentials'
-          refspec: '$GERRIT_REFSPEC'
-          choosing-strategy: 'gerrit'
-
-    wrappers:
-      - fdio-infra-wrappers:
-          build-timeout: '{build-timeout}'
-
-    triggers:
-      - gerrit:
-          server-name: 'Primary'
-          trigger-on:
-            - comment-added-contains-event:
-                comment-contains-value: 'verify-csit'
-          projects:
-            - project-compare-type: 'ANT'
-              project-pattern: '{project}'
-              branches:
-                - branch-compare-type: 'ANT'
-                  branch-pattern: '**/{branch}'
-          skip-vote:
-            successful: true
-            failed: true
-            unstable: true
-            notbuilt: true
-
-    prebuilders:
-      - provide-maven-settings:
-          settings-file: 'honeycomb-settings'
-          global-settings-file: 'global-settings'
-      - shell:
-          !include-raw-escape: include-raw-hc2vpp-csit-verify-prebuild.sh
-
-    maven:
-      maven-name: 'mvn36'
-      root-pom: 'pom.xml'
-      # yamllint disable-line rule:line-length
-      goals: 'clean install -V -Dmaven.repo.local=/tmp/r -Dorg.ops4j.pax.url.mvn.localRepository=/tmp/r -DskipTests -Dcheckstyle.skip=true'
-      maven-opts: '-Xmx2048m -Dmaven.compile.fork=true'
-      settings: 'hc2vpp-settings'
-      settings-type: cfp
-      global-settings: 'global-settings'
-      global-settings-type: cfp
-
-    postbuilders:
-      - shell:
-          !include-raw-escape: include-raw-hc2vpp-csit-verify-odl.sh
-
-    publishers:
-      - email-notification:
-          email-prefix: '[hc2vpp]'
-
-      - robot-report:
-          output-path: 'csit/archive'
-
-      - lf-infra-publish
-
-- job-template:
-    name: 'hc2vpp-csit-perf-{stream}-{os}'
-
-    project-type: freestyle
-    node: '{os}-builder-4c-4g'
-    concurrent: false
-    archive-artifacts: '**/csit/archive/*.*'
-    latest-only: false
-
-
-    build-discarder:
-      daysToKeep: '{build-days-to-keep}'
-      numToKeep: '{build-num-to-keep}'
-      artifactDaysToKeep: '{build-artifact-days-to-keep}'
-      artifactNumToKeep: '{build-artifact-num-to-keep}'
-
-    parameters:
-      - project-parameter:
-          project: '{project}'
-      - os-parameter:
-          os: '{os}'
-      - stream-parameter:
-          stream: '{stream}'
-      - odl-parameter:
-          odl: '{odl}'
-      - string:
-          name: ARCHIVE_ARTIFACTS
-          default: '{archive-artifacts}'
-          description: Artifacts to archive to the logs server.
-    scm:
-      - gerrit-trigger-scm:
-          credentials-id: 'jenkins-gerrit-credentials'
-          refspec: '$GERRIT_REFSPEC'
-          choosing-strategy: 'gerrit'
-
-    wrappers:
-      - fdio-infra-wrappers:
-          build-timeout: '{build-timeout}'
-
-    triggers:
-      - timed: '@weekly'
-
-      - gerrit:
-          server-name: 'Primary'
-          trigger-on:
-            - comment-added-contains-event:
-                comment-contains-value: 'verify-perf'
-
-          projects:
-            - project-compare-type: 'ANT'
-              project-pattern: '{project}'
-              branches:
-                - branch-compare-type: 'ANT'
-                  branch-pattern: '**/{branch}'
-          skip-vote:
-            successful: false
-            failed: false
-            unstable: false
-            notbuilt: false
-
-    builders:
-      - shell:
-          !include-raw-escape: include-raw-hc2vpp-csit-perf.sh
-
-    publishers:
-      - robot-report:
-          output-path: 'csit/archive'
-
-      - lf-infra-publish
-
-- parameter:
-    name: odl-parameter
-    parameters:
-      - string:
-          name: ODL
-          default: '{odl}'
-          description: "ODL version parameter."

commit d34b75d0ffce0f940872404e2fd9ce4a6544f0da

Fix workarounds for archive paths
https://gerrit.fd.io/r/c/ci-management/+/33060
has introduced workarounds for jobs
which create Robot outputs outside /archives/.
It got merged without verifying the workarounds work correctly.

Fixes: 37a55a05cd6ac7ca15bbb99f5ddc9ca61874d5f8

Change-Id: I5e08545bd290fa917e85a375865a130c3f2ff996
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

jjb/scripts/vpp/csit-device.sh
@@ -38,4 +38,4 @@ fi
 popd
 csit_entry_dir="${WORKSPACE}/csit/resources/libraries/bash/entry"
 source "${csit_entry_dir}/with_oper_for_vpp.sh" "per_patch_device.sh"
-cp -R "${WORKSPACE}/csit_current/archives/"* "${WORKSPACE}/archives/" || true
+cp -R "${WORKSPACE}/csit_current" "${WORKSPACE}/archives/" || true

jjb/scripts/vpp/csit-perf.sh
@@ -38,5 +38,5 @@ fi
 popd
 csit_entry_dir="${WORKSPACE}/csit/resources/libraries/bash/entry"
 source "${csit_entry_dir}/with_oper_for_vpp.sh" "per_patch_perf.sh"
-cp -R "${WORKSPACE}/csit_current/"* "${WORKSPACE}/archives/" || true
-cp -R "${WORKSPACE}/csit_parent/"* "${WORKSPACE}/archives/" || true
+cp -R "${WORKSPACE}/csit_current" "${WORKSPACE}/archives/" || true
+cp -R "${WORKSPACE}/csit_parent" "${WORKSPACE}/archives/" || true