2f2a82b0844994412f8b26834fca030d3a5531cb
Dave Wallace <dwallacelf@gmail.com>
2021-10-06 18:16:17 -0400

    Convert VPP docs upload destination from nexus to amazon S3.
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>
    Change-Id: I54c46b32f95705ee0868a961f18fed8b0d4bba81

--- jjb/scripts/vpp/sphinx-docs.sh
+++ jjb/scripts/vpp/sphinx-docs.sh
@@ -17,6 +17,13 @@ echo "---> jjb/scripts/vpp/sphinx-docs.sh"
 
 set -euxo pipefail
 
+# TODO: Remove this file when stable/2106 and stable/2110 are deprecated
+vpp_release="$(${WORKSPACE}/build-root/scripts/version rpm-version)"
+if [[ "${vpp_release::2}" -ge "22" ]] ; then
+    echo "Sphinx-docs documentation is now generated in 'vpp-docs-*' job."
+    exit 0
+fi
+
 line="*************************************************************************"
 # Don't build anything if this is a merge job being run when
 # the git HEAD id is not the same as the Gerrit New Revision id.
@@ -26,51 +33,5 @@ if [[ ${JOB_NAME} == *merge* ]] && [ -n "${GERRIT_NEWREV:-}" ] &&
     exit 0
 fi
 
-DOCS_REPO_URL=${DOCS_REPO_URL:-"https://nexus.fd.io/content/sites/site"}
-PROJECT_PATH=${PROJECT_PATH:-"io/fd/vpp"}
-DOC_FILE=${DOC_FILE:-"vpp.docs.zip"}
-DOC_DIR=${DOC_DIR:-"./docs/_build/html"}
-SITE_DIR=${SITE_DIR:-"build-root/docs/deploy-site"}
-RESOURCES_DIR=${RESOURCES_DIR:-"${SITE_DIR}/src/site/resources"}
-MVN=${MVN:-"/opt/apache/maven/bin/mvn"}
-VERSION=${VERSION:-"$(./build-root/scripts/version rpm-version)"}
-
 make docs-venv
 CONFIRM=-y FORCE=--force-yes make docs
-
-if [[ ${JOB_NAME} == *merge* ]]; then
-  mkdir -p $(dirname ${RESOURCES_DIR})
-  mv -f ${DOC_DIR} ${RESOURCES_DIR}
-  cd ${SITE_DIR}
-  cat > pom.xml << EOF
-  <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-    <modelVersion>4.0.0</modelVersion>
-    <groupId>io.fd.vpp</groupId>
-    <artifactId>docs</artifactId>
-    <version>1.0.0</version>
-    <packaging>pom</packaging>
-
-    <properties>
-      <generateReports>false</generateReports>
-    </properties>
-
-    <build>
-      <extensions>
-        <extension>
-          <groupId>org.apache.maven.wagon</groupId>
-           <artifactId>wagon-webdav-jackrabbit</artifactId>
-           <version>2.10</version>
-        </extension>
-      </extensions>
-    </build>
-    <distributionManagement>
-      <site>
-        <id>fdio-site</id>
-        <url>dav:${DOCS_REPO_URL}/${PROJECT_PATH}/v${VERSION}</url>
-      </site>
-    </distributionManagement>
-  </project>
-EOF
-  ${MVN} -B site:site site:deploy -gs "${GLOBAL_SETTINGS_FILE}" -s "${SETTINGS_FILE}" -T 4C
-  cd -
-fi
