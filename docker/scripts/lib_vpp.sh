ea90c1b8f51c6d937e32f35309e8ed3eb0069771
Dave Wallace <dwallacelf@gmail.com>
2021-12-01 13:55:59 -0500

    Remove VPP master branch from ubuntu-18.04 executor.
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>
    Change-Id: I92aea8725dcd00f90c61025673792048e06934ca

--- docker/scripts/lib_vpp.sh
+++ docker/scripts/lib_vpp.sh
@@ -101,6 +101,6 @@ docker_build_setup_vpp() {
 declare -A VPP_BRANCHES
 VPP_BRANCHES["centos-8"]="stable/2106 stable/2110 master"
 VPP_BRANCHES["debian-10"]="stable/2106 stable/2110 master"
-VPP_BRANCHES["ubuntu-18.04"]="stable/2106 stable/2110 master"
+VPP_BRANCHES["ubuntu-18.04"]="stable/2106 stable/2110"
 VPP_BRANCHES["ubuntu-20.04"]="stable/2106 stable/2110 master"
 export VPP_BRANCHES
