8ae7512928d49aa7d325c0ae7121b7f3f4a754cf
Mauro Sardara <msardara@cisco.com>
2022-04-20 18:59:10 +0200

    fix(hicn/docs.sh): create SITE_DIR if it does not exist before moving html folder
    Signed-off-by: Mauro Sardara <msardara@cisco.com>
    Change-Id: I1cd578f1895f911e4b56d57c564759a6759de052

--- jjb/scripts/hicn/docs.sh
+++ jjb/scripts/hicn/docs.sh
@@ -2,13 +2,15 @@
 set -xe -o pipefail
 
 DOC_DIR="docs/build/html"
-SITE_DIR="build/doc/deploy-site"
+SITE_DIR_ROOT="build/doc"
+SITE_DIR="${SITE_DIR_ROOT}/deploy-site"
 
 echo "---> jjb/scripts/hicn/docs.sh"
 
 bash scripts/build-packages.sh sphinx
 
 if [[ "${JOB_NAME}" == *merge* ]]; then
+  mkdir -p "${SITE_DIR_ROOT}"
   mv -f "${DOC_DIR}" "${SITE_DIR}"
   find "${SITE_DIR}" -type f '(' -name '*.md5' -o -name '*.dot' -o -name '*.map' ')' -delete
 fi
