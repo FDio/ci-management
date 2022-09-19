30e5f48bd61ca09c624a7c2d483565663256e505
Dave Wallace <dwallacelf@gmail.com>
2022-09-19 13:15:07 -0400

    Update jenkins-job-builder version in bash test functions.
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>
    Change-Id: I52c7cc025a4c9bbfa6d686ed0afcec7a8e5d5061

--- extras/bash/sandbox_test_functions.sh
+++ extras/bash/sandbox_test_functions.sh
@@ -29,7 +29,7 @@
 
 jjb-sandbox-env()
 {
-    local jjb_version=${JJB_VERSION:-"3.5.0"}
+    local jjb_version=${JJB_VERSION:-"4.1.0"}
 
     if [ -z "$WS_ROOT" ] ; then
         echo "ERROR: WS_ROOT is not set!"
