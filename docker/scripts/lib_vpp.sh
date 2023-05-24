5003e784cdb09c75bfae163fcef613895648bb6a
Dave Wallace <dwallacelf@gmail.com>
2023-05-24 14:59:27 -0400

    docker: add support for vpp stable/2306 branch
    Change-Id: I7a6ffeb41abaf911406012c8a00219de8469b9fd
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- docker/scripts/lib_vpp.sh
+++ docker/scripts/lib_vpp.sh
@@ -96,7 +96,7 @@ docker_build_setup_vpp() {
 #       to create an enumerated set of jobs jobs that match the
 #       definitions here.
 declare -A VPP_BRANCHES
-VPP_BRANCHES["debian-11"]="stable/2210 stable/2302 master"
-VPP_BRANCHES["ubuntu-20.04"]="stable/2210 stable/2302 master"
-VPP_BRANCHES["ubuntu-22.04"]="stable/2210 stable/2302 master"
+VPP_BRANCHES["debian-11"]="stable/2210 stable/2302 stable/2306 master"
+VPP_BRANCHES["ubuntu-20.04"]="stable/2210 stable/2302 stable/2306 master"
+VPP_BRANCHES["ubuntu-22.04"]="stable/2210 stable/2302 stable/2306 master"
 export VPP_BRANCHES
