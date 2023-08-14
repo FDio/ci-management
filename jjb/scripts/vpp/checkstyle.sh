7b4321b0bb2ce96fc2dd64e975f1afb3bc7884a3
Dave Wallace <dwallacelf@gmail.com>
2023-08-14 17:38:45 -0400

    vpp checkstyle: verify dpdk/rdma-core matrix
    - If mlx_rdma_dpdk_matrix.txt file has been updated
      in the current changeset, verify the current
      rdma-core_version and dpdk_version exist in
      the matrix file.
    
    Change-Id: Ib7bf2969c48badb6795de440a858796c83ad1083
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- jjb/scripts/vpp/checkstyle.sh
+++ jjb/scripts/vpp/checkstyle.sh
@@ -15,6 +15,25 @@
 
 echo "---> jjb/scripts/vpp/checkstyle.sh"
 
+# If mlx_rdma_dpdk_matrix.txt file has been updated in the current changeset,
+# verify the current rdma-core_version and dpdk_version exist in the matrix
+# file
+LINE="*******************************************************************"
+BUILD_EXT_DIR="build/external"
+MATRIX_FILE="$BUILD_EXT_DIR/mlx_rdma_dpdk_matrix.txt"
+PKGS_DIR="$BUILD_EXT_DIR/packages"
+if git show --stat | grep -q "$MATRIX_FILE" ; then
+    RDMA_CORE_VERSION="$(grep rdma-core_version $PKGS_DIR/rdma-core.mk | grep -v '(' | mawk '{print $3}')"
+    DPDK_VERSION="$(grep dpdk_version $PKGS_DIR/dpdk.mk | grep -v '(' | mawk '{print $3}')"
+    CURRENT_MATRIX="rdma=$RDMA_CORE_VERSION dpdk=$DPDK_VERSION"
+    if grep -q "$CURRENT_MATRIX" "$MATRIX_FILE"; then
+        echo -e "$LINE\n* DPDK/RDMA-CORE matrix file update successfully verified\n$LINE"
+    else
+        echo -e "$LINE\n* ERROR: 'rdma=$RDMA_CORE_VERSION dpdk=$DPDK_VERSION' not found in $MATRIX_FILE!\n$LINE"
+        exit 1
+    fi
+fi
+
 if [ -n "$(grep -E '^checkstyle:' Makefile)" ]
 then
 	make checkstyle
