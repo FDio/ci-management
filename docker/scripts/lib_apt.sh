ed7a584dd5a2d25d41bf484b2f69480b92b8f139
Dave Wallace <dwallacelf@gmail.com>
2024-01-25 16:09:00 -0500

    Add stable/2402 branch to docker executor image scripts
    - Refactor csit_pip_cache function to use csit bash
      functions that activate the python virtualenv in
      production jobs.
    - Implement workaround for CSIT python cache miss
      apparently due to change of cache directory in
      pip 23.x and newer.
    
    Change-Id: Ib63aa654a35ad01fbfb5b7f593d3f8ffa6499b07
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- docker/scripts/lib_apt.sh
+++ docker/scripts/lib_apt.sh
@@ -178,26 +178,14 @@ RUN apt-get update -qq \\
              zlib1g-dev \\
   && curl -L https://packagecloud.io/fdio/master/gpgkey | apt-key add - \\
   && curl -s https://packagecloud.io/install/repositories/fdio/master/script.deb.sh | bash \\
-EOF
-
-    cat <<EOF >>"$DOCKERFILE"
   && rm -r /var/lib/apt/lists/*
 
 # Install terraform for CSIT
 #
-RUN wget https://releases.hashicorp.com/terraform/1.4.2/terraform_1.4.2_linux_$dpkg_arch.zip \\
-  && unzip terraform_1.4.2_linux_$dpkg_arch.zip \\
+RUN wget https://releases.hashicorp.com/terraform/1.7.3/terraform_1.7.3_linux_$dpkg_arch.zip \\
+  && unzip terraform_1.7.3_linux_$dpkg_arch.zip \\
   && mv terraform /usr/bin \\
-  && rm -f terraform_1.4.2_linux_$dpkg_arch.zip
-
-# Install Hugo and Go for CSIT
-RUN wget https://github.com/gohugoio/hugo/releases/download/v0.111.3/hugo_extended_0.111.3_linux-$dpkg_arch.deb \\
-  && dpkg -i hugo_extended_0.111.3_linux-$dpkg_arch.deb \\
-  && rm -f hugo_extended_0.111.3_linux-$dpkg_arch.deb \\
-  && wget https://go.dev/dl/go1.20.2.linux-$dpkg_arch.tar.gz \\
-  && rm -rf /usr/local/go \\
-  && tar -C /usr/local -xzf go1.20.2.linux-$dpkg_arch.tar.gz \\
-  && rm -f go1.20.2.linux-$dpkg_arch.tar.gz
+  && rm -f terraform_1.7.3_linux_$dpkg_arch.zip
 
 # Install packages for all project branches
 #
