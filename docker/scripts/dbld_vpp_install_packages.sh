03dfdaff6f8e194965b3dacc034e11f355ff0e5b
Dave Wallace <dwallacelf@gmail.com>
2024-04-11 21:28:31 -0400

    vpp-hst: add VPP HostStack Test framework jobs
    Change-Id: I10396c823fbeaa5e081e48332509e0c20b31b87c
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- docker/scripts/dbld_vpp_install_packages.sh
+++ docker/scripts/dbld_vpp_install_packages.sh
@@ -15,7 +15,7 @@
 
 set -euxo pipefail
 
-export CIMAN_DOCKER_SCRIPTS=${CIMAN_DOCKER_SCRIPTS:-"$(dirname $BASH_SOURCE)"}
+export CIMAN_DOCKER_SCRIPTS=${CIMAN_DOCKER_SCRIPTS:-"$(dirname "${BASH_SOURCE[0]}")"}
 . "$CIMAN_DOCKER_SCRIPTS/lib_vpp.sh"
 . "$CIMAN_DOCKER_SCRIPTS/lib_apt.sh"
 
@@ -23,18 +23,21 @@ must_be_run_in_docker_build
 
 echo_log
 if ! vpp_supported_executor_class "$FDIOTOOLS_EXECUTOR_CLASS" ; then
-    echo_log "VPP is not supported on executor class '$FDIOTOOLS_EXECUTOR_CLASS'. Skipping $(basename $0)..."
+    echo_log "VPP is not supported on executor class '$FDIOTOOLS_EXECUTOR_CLASS'. Skipping $(basename "$0")..."
     exit 0
 else
-    echo_log "Starting  $(basename $0)"
+    echo_log "Starting  $(basename "$0")"
 fi
 
 do_git_config vpp
 for branch in ${VPP_BRANCHES[$OS_NAME]} ; do
     do_git_branch "$branch"
 
+    # Install hs-test depndencies
+    install_hst_deps "$branch"
+
     # Install OS packages
-    make_vpp "install-dep" "$branch"
+    make_vpp "install-deps" "$branch"
 
     # Download, build, and cache external deps packages
     make_vpp "install-ext-deps" "$branch"
@@ -47,7 +50,7 @@ for branch in ${VPP_BRANCHES[$OS_NAME]} ; do
         exit 1
     fi
     if [ -f "$vpp_ext_deps_pkg" ] ; then
-        cp -f $vpp_ext_deps_pkg $DOCKER_DOWNLOADS_DIR
+        cp -f "$vpp_ext_deps_pkg" $DOCKER_DOWNLOADS_DIR
     else
         echo "ERROR: Missing VPP external deps package: '$vpp_ext_deps_pkg'"
         exit 1
@@ -76,4 +79,4 @@ for branch in ${VPP_BRANCHES[$OS_NAME]} ; do
     esac
 done
 
-echo_log -e "Completed $(basename $0)!\n\n=========="
+echo_log -e "Completed $(basename "$0")!\n\n=========="
