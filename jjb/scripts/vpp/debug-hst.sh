5f03aed3dbe6df8421014902d11a9213eb32b924
Dave Wallace <dwallacelf@gmail.com>
2024-04-11 21:28:31 -0400

    vpp-hst: add VPP HostStack Test framework jobs
    Change-Id: I10396c823fbeaa5e081e48332509e0c20b31b87c
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- jjb/scripts/vpp/debug-hst.sh
+++ jjb/scripts/vpp/debug-hst.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "---> jjb/scripts/vpp/debug-build.sh"
+echo "---> jjb/scripts/vpp/debug-hst.sh"
 
 set -euxo pipefail
 
@@ -22,17 +22,12 @@ OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
 OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
 OS_ARCH=$(uname -m)
 DRYRUN="${DRYRUN:-}"
-MAKE_PARALLEL_JOBS="${MAKE_PARALLEL_JOBS:-}"
 BUILD_RESULT="SUCCESSFULLY COMPLETED"
 BUILD_ERROR=""
 RETVAL="0"
+HST_DIR="./extras/hs-test"
 
-# run with ASAN on
-# disable ASAN for now in the debug build - it's broken with PAPI
-# in make test transitioning to unix sockets
-# export VPP_EXTRA_CMAKE_ARGS='-DVPP_ENABLE_SANITIZE_ADDR=ON'
-
-make_build_test_debug() {
+vpp_make_build_debug() {
     if ! make UNATTENDED=yes install-dep ; then
         BUILD_ERROR="FAILED 'make install-dep'"
         return
@@ -45,34 +40,23 @@ make_build_test_debug() {
         BUILD_ERROR="FAILED 'make build'"
         return
     fi
-    if grep -q "${OS_ID}-${OS_VERSION_ID}" <<< "${MAKE_TEST_OS}"; then
-        if [ -n "${MAKE_PARALLEL_JOBS}" ] ; then
-            TEST_JOBS="${MAKE_PARALLEL_JOBS}"
-            echo "Testing VPP with ${TEST_JOBS} cores."
-        else
-            TEST_JOBS="auto"
-            echo "Testing VPP with automatically calculated number of cores. " \
-                "See test logs for the exact number."
-        fi
-        if ! make UNATTENDED=yes COMPRESS_FAILED_TEST_LOGS=yes \
-                TEST_JOBS="$TEST_JOBS" test-debug ; then
-            BUILD_ERROR="FAILED 'make UNATTENDED=yes COMPRESS_FAILED_TEST_LOGS=yes TEST_JOBS=$TEST_JOBS test-debug'"
-            return
-        fi
-    else
-        echo "Skip running 'make test-debug' on ${OS_ID}-${OS_VERSION_ID}"
+}
+
+hst_test_debug() {
+    if ! make VERBOSE=true VPPSRC="$(pwd)" -C "$HST_DIR" build-debug test ; then
+        BUILD_ERROR="FAILED 'make -C $HST_DIR test' (debug)"
+        return
     fi
 }
 
-# clang is not working with ASAN right now - see change 27268
-# also, it does not work with gcc-7, we need gcc-8 at least
-# on ubuntu 20.04 executor the gcc is gcc9
 if [ "${DRYRUN,,}" != "true" ] ; then
-    make_build_test_debug
+    vpp_make_build_debug
+    hst_test_debug
 fi
 if [ -n "$BUILD_ERROR" ] ; then
     BUILD_RESULT="$BUILD_ERROR"
     RETVAL="1"
 fi
-echo -e "\n$line\n* VPP ${OS_ID^^}-${OS_VERSION_ID}-${OS_ARCH^^} DEBUG BUILD $BUILD_RESULT\n$line\n"
+echo -e "\n$line\n* VPP ${OS_ID^^}-${OS_VERSION_ID}-${OS_ARCH^^}" \
+        "BUILD $BUILD_RESULT\n$line\n"
 exit $RETVAL
