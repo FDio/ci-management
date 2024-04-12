4c677fa1aad94164e988270a645f45f0de1b3619
Dave Wallace <dwallacelf@gmail.com>
2024-04-11 21:28:31 -0400

    vpp-hst: add VPP HostStack Test framework jobs
    Change-Id: I10396c823fbeaa5e081e48332509e0c20b31b87c
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- jjb/scripts/vpp/debug-hst.sh
+++ jjb/scripts/vpp/debug-hst.sh
@@ -76,3 +76,75 @@ if [ -n "$BUILD_ERROR" ] ; then
 fi
 echo -e "\n$line\n* VPP ${OS_ID^^}-${OS_VERSION_ID}-${OS_ARCH^^} DEBUG BUILD $BUILD_RESULT\n$line\n"
 exit $RETVAL
+#!/bin/bash
+
+# Copyright (c) 2024 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "---> jjb/scripts/vpp/hst.sh"
+
+set -euxo pipefail
+
+line="*************************************************************************"
+OS_ID=$(grep '^ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_VERSION_ID=$(grep '^VERSION_ID=' /etc/os-release | cut -f2- -d= | sed -e 's/\"//g')
+OS_ARCH=$(uname -m)
+DRYRUN="${DRYRUN:-}"
+BUILD_RESULT="SUCCESSFULLY COMPLETED"
+BUILD_ERROR=""
+RETVAL="0"
+HST_DIR="./extras/hs-test"
+
+make_build() {
+    if ! make UNATTENDED=yes install-deps ; then
+        BUILD_ERROR="FAILED 'make install-dep'"
+        return
+    fi
+    if ! make UNATTENDED=yes install-ext-deps ; then
+        BUILD_ERROR="FAILED 'make install-ext-deps'"
+        return
+    fi
+    if [ -f extras/scripts/build_static_vppctl.sh ]; then
+        if ! extras/scripts/build_static_vppctl.sh ; then
+            BUILD_ERROR="FAILED 'extras/scripts/build_static_vppctl.sh'"
+            return
+        fi
+    fi
+    if ! make build; then
+        BUILD_ERROR="FAILED 'make build' (debug)"
+        return
+    fi
+    if ! make -C "$HST_DIR" install-deps build-debug ; then
+        BUILD_ERROR="FAILED 'make -C $HST_DIR install-deps build-debug'"
+        return
+    fi
+}
+
+hst_test_debug() {
+    if ! make VERBOSE=true VPPSRC="$(pwd)" -C "$HST_DIR" test ; then
+        BUILD_ERROR="FAILED 'make -C $HST_DIR test' (debug)"
+        return
+    fi
+}
+
+if [ "${DRYRUN,,}" != "true" ] ; then
+    make_build
+    hst_test_debug
+fi
+if [ -n "$BUILD_ERROR" ] ; then
+    BUILD_RESULT="$BUILD_ERROR"
+    RETVAL="1"
+fi
+echo -e "\n$line\n* VPP ${OS_ID^^}-${OS_VERSION_ID}-${OS_ARCH^^}" \
+        "BUILD $BUILD_RESULT\n$line\n"
+exit $RETVAL
