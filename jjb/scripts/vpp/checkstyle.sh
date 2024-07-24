812effd8014f2b8efc2b543b5afd568bdcbc8e8c
Dave Wallace <dwallacelf@gmail.com>
2024-07-24 18:16:33 -0400

    fix(vpp): install vpp dependencies in checkstyle job
    - ensure correct version of clang-format is installed
    
    Change-Id: Ieb944e8368076ca44bdc3a0060bfdd0252a71844
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- jjb/scripts/vpp/checkstyle.sh
+++ jjb/scripts/vpp/checkstyle.sh
@@ -34,9 +34,9 @@ if git show --stat | grep -q "$MATRIX_FILE" ; then
     fi
 fi
 
-if [ -n "$(grep -E '^checkstyle:' Makefile)" ]
+if grep -E '^checkstyle:' Makefile)
 then
-	make checkstyle
+    make install-deps checkstyle
 else
-        echo "Can't find checkstyle target in Makefile - skipping checkstyle"
+    echo "Can't find checkstyle target in Makefile - skipping checkstyle"
 fi
