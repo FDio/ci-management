bb70ef8c72a2c8eecc0d6353eb3176da3667d493
Naveen Joy <najoy@cisco.com>
2025-03-04 12:22:05 -0800

    chore(vpp): post 25.02 release clean up
    Change-Id: Ib1d5c74f2240fd914848f50e3d9a14bb25e48bc8
    Signed-off-by: Naveen Joy <najoy@cisco.com>

--- docker/scripts/lib_csit.sh
+++ docker/scripts/lib_csit.sh
@@ -34,8 +34,8 @@ export CIMAN_DOCKER_SCRIPTS="${CIMAN_DOCKER_SCRIPTS:-$(dirname ${BASH_SOURCE[0]}
 #       to create an enumerated set of jobs jobs that match the
 #       definitions here.
 declare -A CSIT_VPP_BRANCHES
-CSIT_VPP_BRANCHES["ubuntu-22.04"]="stable/2406 master"
-CSIT_VPP_BRANCHES["ubuntu-24.04"]="master"
+CSIT_VPP_BRANCHES["ubuntu-22.04"]="stable/2410 stable/2502 master"
+CSIT_VPP_BRANCHES["ubuntu-24.04"]="stable/2410 stable/2502 master"
 export CSIT_VPP_BRANCHES
 
 CSIT_SUPPORTED_EXECUTOR_CLASSES="builder csit_dut"
