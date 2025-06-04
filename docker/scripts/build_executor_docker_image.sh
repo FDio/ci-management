3ced40cff5349af6eee879c6a1cbbc1151d758be
Dave Wallace <dwallacelf@gmail.com>
2025-06-04 17:20:13 -0400

    chore: update docker builder scripts for stable/2506
    - remove ubuntu-22.04 from CSIT requirements
    - add hst build* targets to prepopulate docker cache
      with base docker images
    - update golang version to 1.24.4
    - update to latest docker-in-docker base images
    
    Change-Id: I309a1a00fecacd3151b0cd97e076f68fa93c4cc8
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

--- docker/scripts/build_executor_docker_image.sh
+++ docker/scripts/build_executor_docker_image.sh
@@ -129,7 +129,7 @@ for executor_os_name in $os_names ; do
         cat "$DOCKERFILE"
         echo -e "$line\n"
     else
-        docker build -t "$executor_docker_image" "$DOCKER_BUILD_DIR"
+        docker build --privileged -t "$executor_docker_image" "$DOCKER_BUILD_DIR"
         rm -f "$DOCKERFILE"
         if [ -n "$ci_tag" ] ; then
             ci_image="$repository:$ci_tag"
