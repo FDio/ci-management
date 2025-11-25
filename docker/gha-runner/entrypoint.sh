37364fa9f28607bfb9a5100a2d44ec8c9d0a276a
Dave Wallace <dwallacelf@gmail.com>
2025-11-24 21:06:48 -0500

    gha: add gha container generation to docker build scripts
    Change-Id: I896533f58c87db96b480aec6c9fe5e690358079b
    Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

---
+++ docker/gha-runner/entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -ex
+
+function deregister_runner {
+    echo "Deregistering runner"
+    "${DOCKER_GHA_RUNNER_DIR}"/config.sh remove --token "${token}"
+
+}
+
+function register_runner {
+    echo "Get Runner Registration Token"
+    token=$(
+        curl -sS -X POST -H "Authorization: token ${GITHUB_PAT}" "${GITHUB_API_URL}/actions/runners/registration-token" | jq -r .token
+    )
+
+    echo "Configuring runner"
+    CONFIGURED=false
+    "${DOCKER_GHA_RUNNER_DIR}"/config.sh \
+        --disableupdate \
+        --ephemeral \
+        --labels "${RUNNER_LABELS}" \
+        --name "${NOMAD_ALLOC_ID}" \
+        --replace \
+        --token "${token}" \
+        --unattended \
+        --url "${GITHUB_REPO_URL}"
+    CONFIGURED=true
+}
+
+export PATH="$PATH":"${DOCKER_GHA_RUNNER_DIR}"
+export RUNNER_ALLOW_RUNASROOT=1
+
+# Register the Github Runner
+register_runner
+
+# Ensure we deregister the Github runner
+trap 'deregister_runner' SIGINT SIGQUIT SIGTERM
+
+# Launch the Github Runner
+"${DOCKER_GHA_RUNNER_DIR}"/bin/Runner.Listener run
+
+# Deregister the Github runner
+deregister_runner
+
+exit 0
